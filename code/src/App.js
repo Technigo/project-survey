d13b896680a4730fbf64e983583abfb01754b71d
Clara Gyökeres <claragyokeres@Claras-Air.lan>
2019-11-25 20:34:43 +0100

    updated with textbox and styling

--- code/src/App.js
+++ code/src/App.js
@@ -2,23 +2,32 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom"
 
 export const App = () => {
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState('')
+  const [name, setName] = useState('')
 
   return (
-
-    <form>
-      <h1>WIN EXCLUSIVE TICKETS TO YOUR DREAM DESTINATION!</h1>
-      <h2>We are giving away one lucky winner for this unique opportunity. All you have to do is to fill out the form beneath and cross your fingers!</h2>
-      <select
-        onChange={event => setLocation(event.target.value)}
-        value={location}
+    <div className="myForm">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault()
+          console.log('Submit!')
+        }}
       >
-        <option value="">Where to?</option>
-        <option value="newYork">New York</option>
-        <option value="tokyo">Tokyo</option>
-        <option value="paris">Paris</option>
-      </select>
-    </form>
-  );
-};
-
+        <h1>WIN EXCLUSIVE TICKETS TO YOUR DREAM DESTINATION!</h1>
+        <h2>
+          We are giving away one lucky winner for this unique opportunity. All you have to do is to
+          fill out the form beneath and cross your fingers!
+        </h2>
+        <h2>Motivate why you should be one of the lucky winners: {name}</h2>
+        <input type="text" onChange={(event) => setName(event.target.value)} value={name} />
+        <h1> *** </h1>
+        <select onChange={(event) => setLocation(event.target.value)} value={location}>
+          <option value="">Where to?</option>
+          <option value="newYork">New York</option>
+          <option value="tokyo">Tokyo</option>
+          <option value="paris">Paris</option>
+        </select>
+      </form>
+    </div>
+  )
+}
