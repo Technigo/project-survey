669c2b8ab442e05e238a805fb3128dcd53b2ec7b
Petra B <petrabrinck@icloud.com>
2019-11-25 12:00:04 +0100

    first commit with usestate

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
+/* below from import */
+// export const App = () => {
+//   return (
+//     <div>
+//       Find me in src/app.js!
+//     </div>
+//   )
+// }
+
 
 export const App = () => {
+  const [temperature, setTemperature] = useState(20)
+
   return (
     <div>
-      Find me in src/app.js!
+      <h1>Current temperature: {temperature} degrees</h1>
+
+      <button onClick={() => setTemperature(-5)}>Freeze!</button>
+      <button onClick={() => setTemperature(30)}>Warm!</button>
+      <button onClick={() => setTemperature(120)}>Boil!</button>
     </div>
   )
 }
