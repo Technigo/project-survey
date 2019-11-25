d82baca85dcd76cf79a9d8c51072fbb6b08f02a3
nyblad <nyblad@hotmail.com>
2019-11-25 21:50:27 +0100

    Added button to reload page after summary

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 export const Summary = (props) => {
+  // Funtion to reload page ans start over - called in button onClick
+  const refreshPage = () => {
+    window.location.reload();
+  }
+
   return (
     <div className="summary">
       <h2>Thank you for your answers {props.name}!</h2>
       <p>1: {props.place}</p>
       <p>2: {props.fruit}</p>
+      <button onClick={refreshPage}>Restart</button>
     </div>
   )
 }
