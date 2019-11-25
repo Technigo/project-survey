0f1d4e0e14d78a562fd7f498e820a5f0fdbf36c4
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-11-25 12:00:03 +0100

    connection works with State

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+/*import React from 'react'*/
+import React, { useState } from 'react'
 
+/* Down below from Technigo
 export const App = () => {
   return (
     <div>
       Find me in src/app.js!
     </div>
+  
   )
-}
+} */
+
+export const App = () => {
+  const [temperature, setTemperature] = useState(20);
+
+  return (
+    <div>
+
+
+    <h1>Current temp> {temperature} degrees </h1>
+    
+    <button onClick={() => setTemperature(-5)}>Freeze!</button>
+
+    {temperature > 100 && <p>Steamy!</p>}
+    </div>
+  );
+}; 
--- code/src/index.js
+++ code/src/index.js
@@ -4,3 +4,19 @@ import './index.css'
 import { App } from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+/*const App = () => {
+    const [temperature, setTemperature] = useState(20);
+  
+    return (
+      <div>
+  
+  
+      <h1>Current temp> {temperature} degrees </h1>
+      
+      <button onClick={() => setTemperature(-5)}>Freeze!</button>
+  
+      {temperature > 100 && <p>Steamy!</p>}
+      </div>
+    );
+  };*/
