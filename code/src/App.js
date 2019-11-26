9e72fd27b594d83dfda71ae8ac67ae6e6b97949b
nyblad <nyblad@hotmail.com>
2019-11-26 22:07:48 +0100

    Added condition to only show form element when not submitted

--- code/src/App.js
+++ code/src/App.js
@@ -31,74 +31,77 @@ export const App = () => {
     <div className="app">
       <h1>Quiz time!</h1>
 
-      <form onSubmit={event => event.preventDefault()}>
+      {!submitted &&
+        <form onSubmit={event => event.preventDefault()}>
 
-        <div className="question">
-          <label>
-            <h2>Please enter your name to participate *</h2>
-            <input
-              type="text"
-              required
-              placeholder="Type your name here..."
-              onChange={event => setName(event.target.value)}
-              value={name}
-            />
-          </label>
-        </div>
-
-        <div className="question">
-          <label>
-            <h2>#1: What's your place?</h2>
-            <div className="select-main">
-              <select
-                onChange={event => setPlace(event.target.value)}
-                value={place}
-              >
-                <option value="">Choose your place</option>
-                <option value="Home">Home</option>
-                <option value="Away">Away</option>
-              </select>
-            </div>
-          </label>
-        </div>
-
-        <div className="question" role="radiogroup">
-          <h2>#2: What is the Minions favorite fruit?</h2>
-          {fruits.map((choice) => (
-            <label key={choice} className="radio-buttons">
+          <div className="question">
+            <label>
+              <h2>Please enter your name to participate *</h2>
               <input
-                type="radio"
-                value={choice}
-                onChange={(event) => setFruit(event.target.value)}
-                checked={fruit === choice}
+                type="text"
+                required
+                placeholder="Type your name here..."
+                onChange={event => setName(event.target.value)}
+                value={name}
               />
-              <div>{choice}</div>
             </label>
-          ))
-          }
-        </div>
+          </div>
 
-        <div className="question">
-          <label>
-            <h2>#3: How many % has been knocked down by imposter syndrome?</h2>
-            <input
-              className="slider"
-              type="range"
-              min={1}
-              max={100}
-              value={range}
-              onChange={(event) => setRange(event.target.value)}
-            />
-          </label>
-          <div className="show-range">{range}%</div>
-        </div>
+          <div className="question">
+            <label>
+              <h2>#1: What's your place?</h2>
+              <div className="select-main">
+                <select
+                  onChange={event => setPlace(event.target.value)}
+                  value={place}
+                >
+                  <option value="">Choose your place</option>
+                  <option value="Home">Home</option>
+                  <option value="Away">Away</option>
+                </select>
+              </div>
+            </label>
+          </div>
+
+          <div className="question" role="radiogroup">
+            <h2>#2: What is the Minions favorite fruit?</h2>
+            {fruits.map((choice) => (
+              <label key={choice} className="radio-buttons">
+                <input
+                  type="radio"
+                  value={choice}
+                  onChange={(event) => setFruit(event.target.value)}
+                  checked={fruit === choice}
+                />
+                <div>{choice}</div>
+              </label>
+            ))
+            }
+          </div>
+
+          <div className="question">
+            <label>
+              <h2>#3: How many % has been knocked down by imposter syndrome?</h2>
+              <input
+                className="slider"
+                type="range"
+                min={1}
+                max={100}
+                value={range}
+                onChange={(event) => setRange(event.target.value)}
+              />
+            </label>
+            <div className="show-range">{range}%</div>
+          </div>
 
 
-        <button type="submit" onClick={handleSubmit} disabled={isDisabled()}>Done</button>
-        {isDisabled() && <div className="name-required">* Please type your name before hitting Done *</div>}
-        {submitted && <Summary name={name} place={place} fruit={fruit} range={range} />}
+          <button type="submit" onClick={handleSubmit} disabled={isDisabled()}>Done</button>
+          {isDisabled() && <div className="name-required">* Please type your name before hitting Done *</div>}
 
-      </form>
+        </form>
+      }
+
+      {submitted && <Summary name={name} place={place} fruit={fruit} range={range} />}
 
       <footer>Technigo Bootcamp 2019 © Sofie Nyblad</footer>
 
