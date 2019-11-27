2621711a24e9057a79f55020733de090b2cfaafb
nyblad <nyblad@hotmail.com>
2019-11-27 22:18:05 +0100

    Updated question content again

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -10,9 +10,12 @@ export const Summary = (props) => {
   return (
     <div className="summary">
       <h2>Thank you {props.name}!</h2>
-      <p>#1: {props.location}</p>
-      <p>#2: {props.fruit}</p>
-      <p>#3: {props.range}%</p>
+      <p className="user-answer">#1: {props.location}</p>
+      <p className="correct-answer"> Vacayay of course! Or home sweet home 🏡</p>
+      <p className="user-answer">#2: {props.fruit}</p>
+      <p className="correct-answer">Minions crave for 🍌</p>
+      <p className="user-answer">#3: {props.range}</p>
+      <p className="correct-answer">Depending on legend Santa has 8 or 9 🦌</p>
       <button onClick={refreshPage}>Restart</button>
     </div>
   )
