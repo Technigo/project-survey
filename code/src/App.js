ee3a66de25845f43aefcb8ffa05684f43ff680c0
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-11-27 15:07:24 +0100

    trying to style submit text

--- code/src/App.js
+++ code/src/App.js
@@ -30,12 +30,12 @@ export const App = () => {
           
         <div className="formButton"> 
           
-          <button onClick={() => setReps(5)}>Just started and I love it!</button>
-          <button onClick={() => setReps(20)}>A couple of months and my box is the best!</button>
-          <button onClick={() => setReps(50)}>It's been a couple of years now and can't imagine a life without it!</button>
-          {reps == 50 && <p>Rockstar - totally agree!</p>} 
-          {reps == 5 && <p>Awesome - keep it up and keep that feeling!</p>} 
-          {reps == 20 && <p>Whoop, whoop - one of the best things with the sport..The community!</p>} 
+          <button onClick={() => setReps(2)}>Just started and I love it!</button>
+          <button onClick={() => setReps(5)}>A couple of months and my box is the best!</button>
+          <button onClick={() => setReps(24)}>It's been a couple of years now and can't imagine a life without it!</button>
+          {reps == 24 && <p>Rockstar - totally agree!</p>} 
+          {reps == 2 && <p>Awesome - keep it up and keep that feeling!</p>} 
+          {reps == 5 && <p>Whoop, whoop - one of the best things with the sport..The community!</p>} 
         </div>
 
         <p> Which movement was the most fun to get the hang of? </p>
--- code/src/Summary.js
+++ code/src/Summary.js
@@ -5,7 +5,7 @@ import "./Components/summary.css";
 export const Summary = (props) => {
     return <div className="summaryText">
         <p>Great job! Your future is strong and bright! This is your CrossFit profile!</p>
-        <p>You've been working hard during {props.reps} months! <br></br>Keep it up and you'll reach your goals! 
+        <p>You've been working hard during {props.reps} months, that's 2 years! <br></br>Keep it up and you'll reach your goals! 
         </p>
         <p>Your first memory being a true CrossFiter was when you hit your first: {props.movement}'s</p>
         <p>{props.icon} sends her regards and hope you'll meet to wod one day! :-) </p>
