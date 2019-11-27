66b4fc0c4459d537a6f78fba65b16b94345ee30a
Michel Elmoznino Laufer <mitchel_85@hotmail.com>
2019-11-27 15:13:17 +0100

    Created a textbox, a set of radio buttons, two scrollbars and a submit button

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Summary } from './Summary'
+
 
 export const App = () => {
+  const ageGroups=["0-18", "19-30", "31-40", "41+"]
+  const [name, setName] = useState("")
+  const [age, setAge] = useState("")
+  const [food, setFood] = useState("")
+  const [tvseries, setTvseries] = useState("")
+  
+  const [submitted, setSubmitted] = useState(false)
+  const [showText, setShowText] = useState(true)
+  const showSummary = () => {
+    setSubmitted(true);
+    setShowText(!showText);
+  }
+
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="menu-container">
+      
+    {showText && <div className="form-container">
+    <h1>Welcome to my survey!</h1>
+    <p>Please answer the following questions about yourself</p>
+    
+    <form 
+      onSubmit={event => event.preventDefault()}>
+      <p>Name</p>
+      <label>
+      <input 
+        type="text"
+        onChange={event => setName(event.target.value)}
+        value={name}
+      />
+      </label>
+
+    <br></br>
+
+      Age group:
+      {ageGroups.map(group => (
+        <label key={group}>
+          <input
+            type="radio"
+            value={group}
+            onChange={event => setAge(event.target.value)}
+            checked={age === group}
+          />
+          {group}
+        </label>
+      ))}
+
+    <br></br><br></br>
+      
+      <label>
+      <select
+        onChange={event => setFood(event.target.value)}
+        value={food}
+      >
+        <option value="">Select your favourite food</option>
+        <option value="hamburger">Hamburger</option>
+        <option value="sushi">Sushi</option>
+        <option value="pasta">Pasta</option>
+        <option value="pizza">Pizza</option>
+        <option value="pizza">Tacos</option>
+        <option value="other">Other</option>
+      </select>
+      </label>
+
+    <br></br>
+      
+      <label>
+      <select
+        onChange={event => setTvseries(event.target.value)}
+        value={tvseries}
+      >
+        <option value="">Select your favourite tv-series</option>
+        <option value="strangerthings">Stranger Things</option>
+        <option value="homeland">Homeland</option>
+        <option value="truedetective">True Detective</option>
+        <option value="biglittlelies">Big Little Lies</option>
+        <option value="thehandmaidstale">The Handmaids Tale</option>
+        <option value="breakingbad">Breaking Bad</option>
+        <option value="twinpeaks">Twin Peaks</option>
+        <option value="other">Other</option>
+      </select>
+      </label>
+
+    </form>
+
+    <button type="submit" onClick={() => showSummary()}>
+    Submit
+    </button>
+
+    </div> }
+
+    <br></br>
+
+    {submitted && <Summary name={name} age={age} food={food} tvseries={tvseries} />}
     </div>
   )
 }
---
+++ code/src/Summary.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+export const Summary = (props) => {
+  return (
+    <div className="summary">
+      <h2>Thank you for your answers {props.name}</h2>
+      <p>#1 {props.age} years</p>
+      <p>#2 {props.food}</p>
+      <p>#3 {props.tvseries}</p>
+    </div>
+  )
+}
