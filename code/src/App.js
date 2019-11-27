a7784fad39e7e43f0c14cf2bf60e33a5ec26e528
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-11-27 23:53:28 +0100

    submittext in the box / still need to hide

--- code/src/App.js
+++ code/src/App.js
@@ -18,31 +18,30 @@ export const App = () => {
   
   return (
     <section className="formContainer">
-    <div>
-       {submit && <Summary movement={movement} icon={icon} reps={reps}/>}
-    </div>
-
-   
-             
+    
       <form className="formInput" onSubmit={event => event.preventDefault()}>
-      <Header />
+        <Header />
 
-        <p> When did you start with CrossFit?  </p> 
-          
-        <div className="formButton"> 
-          
-          <button onClick={() => setReps(2)}>Just started and I love it!</button>
-          <button onClick={() => setReps(5)}>A couple of months and my box is the best!</button>
-          <button onClick={() => setReps(24)}>It's been a couple of years now and can't imagine a life without it!</button>
-          {reps == 24 && <p>Rockstar - totally agree!</p>} 
-          {reps == 2 && <p>Awesome - keep it up and keep that feeling!</p>} 
-          {reps == 5 && <p>Whoop, whoop - one of the best things with the sport..The community!</p>} 
-        </div>
+          <div>
+            {submit && <Summary movement={movement} icon={icon} reps={reps}/>}
+          </div>
 
-        <p> Which movement was the most fun to get the hang of? </p>
-      
-        <div>
-              
+          <p> When did you start with CrossFit?  </p> 
+          
+          <div className="formButton"> 
+            
+            <button onClick={() => setReps(2)}>Just started and I love it!</button>
+            <button onClick={() => setReps(5)}>A couple of months and my box is the best!</button>
+            <button onClick={() => setReps(24)}>It's been a couple of years now and can't imagine a life without it!</button>
+            {reps == 24 && <p>Rockstar - totally agree!</p>} 
+            {reps == 2 && <p>Awesome - keep it up and keep that feeling!</p>} 
+            {reps == 5 && <p>Whoop, whoop - one of the best things with the sport..The community!</p>} 
+          </div>
+
+          <p> Which movement was the most fun to get the hang of? </p>
+        
+          <div>
+                
           <select
             onChange={event => setMovement(event.target.value)}
             value={movement}
