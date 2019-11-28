5a68f268bd1b130cbff6ad5d3e69b7a896f665ea
Jenfi <jenniferfick@LTWHYRED39.Whyred.local>
2019-11-28 15:32:35 +0100

    header styled

--- code/src/App.js
+++ code/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import hero from './hero.png'
 
 import './App.css'
 import './index.css'
@@ -10,16 +11,21 @@ export const App = () => {
 
     <div>
       <header>
-        <div className="imgContainer">
-        </div>
-        {/* <img src="src/hero.png" /> */}
+        <img src={hero} height="400px" width="auto" />
         <h1>How well do you really know each other?</h1>
-        <h2>Take the survery now to find out!</h2>
-        <button onClick="">Join survey</button>
+        <button onClick="">Find out here</button>
       </header>
-
-
-      Find me in src/app.js!
+      <article>
+        <h1>All the questions</h1>
+        <h2>What is your name? - Text field</h2>
+        <h2>Question 2- What is your nickname of your partner? - Text field</h2>
+        <h2>Question 3 - Do you know your partner? - Radio button yes/no</h2>
+        <h2>Question 4 - What is your partner to you? Choose from the list. - Drop down menu</h2>
+        <button onClick="">Submit</button>
+        <h1>The result</h1>
+      </article>
+      <footer>
+      </footer>
     </div>
   )
 }
