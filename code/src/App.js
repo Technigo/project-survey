2843fb04a2aa864b5d5cf6d5d93498f17336c2d5
Johanna Luks Wærn <johanna@misswaern.se>
2019-11-28 11:41:10 +0100

    initial commit

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from "react"
+import "./index.css"
+
+const characters = ["The Grinch", "Rudolph", "Frosty the Snowman"]
 
 export const App = () => {
+  const [name, setName] = useState()
+  const [character, setCharacter] = useState("")
+  const [song, setSong] = useState("")
+
   return (
     <div>
-      Find me in src/app.js!
+      <header>
+        <h1>How much do you love Christmas?</h1>
+      </header>
+
+      <div className='survey'>
+        <form onSubmit={event => event.preventDefault()}>
+          <h2>Fill in your full name, please</h2>
+          <input
+            type='text'
+            onChange={event => setName(event.target.value)}
+            value={name}
+          />
+        </form>
+        <form onSubmit={event => event.preventDefault()}>
+          <h2>Fill in your Christmas nickname</h2>
+          <input
+            type='text'
+            onChange={event => setName(event.target.value)}
+            value={name}
+          />
+        </form>
+        <form>
+        <h2>Which Christmas Character would you be in another life?<h2>
+          {characters.map(group => (
+            <label key={group}>
+              <input
+                type='radio'
+                value={group}
+                onChange={event => setCharacter(event.target.value)}
+                checked={character === group}
+              />
+              {group}
+            </label>
+          ))}
+        </form>
+
+        <form>
+          <h2>Select your favourite x-mas song</h2>
+          <select onChange={event => setSong(event.target.value)} value={song}>
+            <option value=''>Select</option>
+            <option value='song1'>Song1</option>
+            <option value='song2'>Song2</option>
+            <option value='song3'>Song3</option>
+            <option value='song4'>Song4</option>
+            <option value='song5'>Song5</option>
+          </select>
+        </form>
+      </div>
     </div>
   )
 }
