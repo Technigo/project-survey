8f2ebf1c9385ccfe2be60235e56250fe41d3c977
AleksandraMuller <aleksandra.e.mueller@gmail.com>
2019-11-28 01:36:11 +0100

    added styling hero/layout/fonts

--- code/src/App.js
+++ code/src/App.js
@@ -38,77 +38,79 @@ export const App = () => {
 
   return (
     <main>
-      <h1>Customer Satsfaction Survey "The Coffee time"</h1>
-      {hideForm && (
-        <Summary
-          name={name}
-          time={time}
-          quality={quality}
-          experience={experience}
-        />
-      )}
-      {/* {!hideForm && ( */}
-      <form onSubmit={handleSubmit}>
-        {section === "firstQuestion" && !hideForm && (
-          <div className="firstQuestion">
-            <Questions value={"q1"} text={"Please fill in your name:"} />
-            <InputText
-              value={name}
-              onChange={event => setName(event.target.value)}
-            />
-            <Submit onClick={handleNext} text={"Next question"} />
-          </div>
+      <div className="hero-image"></div>
+      <div className="main-container">
+        <h1>Customer Satsfaction Survey "The Coffee time"</h1>
+        {hideForm && (
+          <Summary
+            name={name}
+            time={time}
+            quality={quality}
+            experience={experience}
+          />
         )}
-        {section === "secondQuestion" && !hideForm && (
-          <div className="secondQuestion">
-            <Questions
-              value={"q2"}
-              text={`Are you satisfied with the quality of our coffee place, ${name}?`}
-            />
-            <RadioButton
-              label="Yes"
-              value="yes"
-              checked={quality === "yes"}
-              onChange={handleRadioChange}
-            />
-            <RadioButton
-              label="No"
-              value="no"
-              checked={quality === "no"}
-              onChange={handleRadioChange}
-            />
-            <Submit onClick={handleNext} text={"Next question"} />
-          </div>
-        )}
-        {section === "thirdQuestion" && (
-          <div className="thirdQuestion">
-            <Questions
-              value={"q3"}
-              text={"How often do you visit our place?"}
-            />
-            <Select
-              value={time}
-              onChange={event => setTime(event.target.value)}
-            />
-            <Submit onClick={handleNext} text={"Next question"} />
-          </div>
-        )}
-        {section === "fourthQuestion" && !hideForm && (
-          <div className="fourthQuestion">
-            <Questions
-              value={"q4"}
-              text={
-                "Finally, how can we improve your experience? Do you have any ideas for us?"
-              }
-            />
-            <InputTextarea
-              value={experience}
-              onChange={event => setExperience(event.target.value)}
-            />
-            <Submit onClick={handleSubmit} text={"Submit"} />
-          </div>
-        )}
-      </form>
+        <form onSubmit={handleSubmit}>
+          {section === "firstQuestion" && !hideForm && (
+            <div className="questions firstQuestion">
+              <Questions value={"q1"} text={"Please fill in your name:"} />
+              <InputText
+                value={name}
+                onChange={event => setName(event.target.value)}
+              />
+              <Submit onClick={handleNext} text={"Next question"} />
+            </div>
+          )}
+          {section === "secondQuestion" && !hideForm && (
+            <div className="questions secondQuestion">
+              <Questions
+                value={"q2"}
+                text={`Are you satisfied with the quality of our coffee place, ${name}?`}
+              />
+              <RadioButton
+                label="Yes"
+                value="yes"
+                checked={quality === "yes"}
+                onChange={handleRadioChange}
+              />
+              <RadioButton
+                label="No"
+                value="no"
+                checked={quality === "no"}
+                onChange={handleRadioChange}
+              />
+              <Submit onClick={handleNext} text={"Next question"} />
+            </div>
+          )}
+          {section === "thirdQuestion" && (
+            <div className="questions thirdQuestion">
+              <Questions
+                value={"q3"}
+                text={"How often do you visit our place?"}
+              />
+              <Select
+                value={time}
+                onChange={event => setTime(event.target.value)}
+              />
+              <Submit onClick={handleNext} text={"Next question"} />
+            </div>
+          )}
+          {section === "fourthQuestion" && !hideForm && (
+            <div className="questions fourthQuestion">
+              <Questions
+                value={"q4"}
+                text={
+                  "Finally, how can we improve your experience? Do you have any ideas for us?"
+                }
+              />
+              <InputTextarea
+                value={experience}
+                onChange={event => setExperience(event.target.value)}
+              />
+              <Submit onClick={handleSubmit} text={"Submit"} />
+            </div>
+          )}
+        </form>
+      </div>
     </main>
   );
 };
--- code/src/Summary.js
+++ code/src/Summary.js
@@ -3,14 +3,14 @@ import React from "react";
 export const Summary = props => {
   return (
     <div className="summary">
-      <h3>
+      <h4>
         Thank you {props.name} for your time and participation in our survey!
-      </h3>
-      <p>Are you satified? You answer was {props.quality} </p>
-      <p>You visit our place {props.time} </p>
-      <p>
+      </h4>
+      <h4>Are you satified? You answer was {props.quality} </h4>
+      <h4>You visit our place {props.time} </h4>
+      <h4>
         And here some of your experience and ideas for us: {props.experience}
-      </p>
+      </h4>
     </div>
   );
 };
