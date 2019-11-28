65f38b38963c5ea3e652c3321ea9a04b6a5238ca
Jenfi <jenniferfick@LTWHYRED39.Whyred.local>
2019-11-28 14:52:49 +0100

    first commit

--- code/src/App.js
+++ code/src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import './App.css'
+import './index.css'
+import './header.css'
+
+
 export const App = () => {
   return (
+
     <div>
+      <header>
+        <div className="imgContainer">
+        </div>
+        {/* <img src="src/hero.png" /> */}
+        <h1>How well do you really know each other?</h1>
+        <h2>Take the survery now to find out!</h2>
+        <button onClick="">Join survey</button>
+      </header>
+
+
       Find me in src/app.js!
     </div>
   )
