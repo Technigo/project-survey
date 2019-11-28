0f4ebbe52ba81b5b184f70f161f2a4cc083c0c13
Johanna Luks Wærn <johanna@misswaern.se>
2019-11-28 15:25:53 +0100

    building forms

--- code/src/App.js
+++ code/src/App.js
@@ -5,14 +5,26 @@ const characters = ["The Grinch", "Rudolph", "Frosty the Snowman"]
 
 export const App = () => {
   const [name, setName] = useState()
+  const [nickname, setNickname] = useState()
   const [character, setCharacter] = useState("")
   const [song, setSong] = useState("")
+  const [showResult, setShowResult] = useState(false)
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    setShowResult(true)
+  }
 
   return (
     <div>
       <header>
         <h1>How much do you love Christmas?</h1>
       </header>
+
+      {showResult && <h1>EVERYONE LOVES CHRISTMAS!</h1>}
+
+      {!showResult && (
+        <form onSubmit={handleSubmit}>
 
       <div className='survey'>
         <form onSubmit={event => event.preventDefault()}>
@@ -23,18 +35,22 @@ export const App = () => {
             value={name}
           />
         </form>
+        <span>* * * * * * * * * * * * * * * * * * * * *</span>
+
         <form onSubmit={event => event.preventDefault()}>
-          <h2>Fill in your Christmas nickname</h2>
+          <h2>Fill in your Christmas nickname, please</h2>
           <input
             type='text'
-            onChange={event => setName(event.target.value)}
-            value={name}
+            onChange={event => setNickname(event.target.value)}
+            value={nickname}
           />
         </form>
+        <span>* * * * * * * * * * * * * * * * * * * * *</span>
+
         <form>
-        <h2>Which Christmas Character would you be in another life?<h2>
+          <h2>Which Christmas Character would you be in another life?</h2>
           {characters.map(group => (
-            <label key={group}>
+            <label className='radioButtons' key={group}>
               <input
                 type='radio'
                 value={group}
@@ -45,9 +61,10 @@ export const App = () => {
             </label>
           ))}
         </form>
+        <span>* * * * * * * * * * * * * * * * * * * * *</span>
 
         <form>
-          <h2>Select your favourite x-mas song</h2>
+          <h2>Select your favourite Christmas Song</h2>
           <select onChange={event => setSong(event.target.value)} value={song}>
             <option value=''>Select</option>
             <option value='song1'>Song1</option>
@@ -57,7 +74,12 @@ export const App = () => {
             <option value='song5'>Song5</option>
           </select>
         </form>
+        <span>* * * * * * * * * * * * * * * * * * * * *</span>
+        <button type='submit'>Submit my answers</button>
       </div>
+      <footer>
+        <p>Happy Holidays™</p>
+      </footer>
     </div>
   )
 }
