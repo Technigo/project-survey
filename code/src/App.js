a010d02041237547097e085132192dedf6c96da2
Clara Gyökeres <claragyokeres@Claras-Air.lan>
2019-11-29 18:06:52 +0100

    changed the summary

--- code/src/App.js
+++ code/src/App.js
@@ -1,18 +1,13 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom"
 import { Summary } from './Summary';
 
 export const App = () => {
   const [location, setLocation] = useState('')
-  const [name, setName] = useState('')
+  const [name, setName] = useState()
   const [ageGroup, setAgeGroup] = useState();
   const [submitted, setSubmitted] = useState(false)
-  const [showResult, setShowResult] = useState(false);
+  const [setShowResult] = useState(false);
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    setShowResult(true);
-  };
 
   return (
     <div>
@@ -31,16 +26,17 @@ export const App = () => {
 
             <div className="introduction">
               <h2> We know it can be awkward and tricky.
-                Don't worry, we won't have "the talk" with you or stress you in the cashier.
+                Don't worry, we won't have "the talk" with you or
+                stress you in the cashier.
                 Our condoms are free of charge and they're really good.
                 We'll even send them right home to your address.
                 Fill out the form and we'll take care of the boring part.
-          Thanks for wearing rubber.</h2></div>
+                Thanks for wearing rubber.</h2></div>
 
-            <div className="mailbox">
-              <h3><span>Your email: {name} ✍️</span></h3>
-              <input type="email" onChange={(event) => setName(event.target.value)} value={name} required />
-            </div>
+            <form className="mailbox">
+              <h3><span>Your email: {name} ✍️ </span></h3>
+              <input type="email" onChange={event => setName(event.target.value)} value={name} required />
+            </form>
 
             <div className="sizebox">
               <h3> <span> Size? </span></h3>
@@ -92,10 +88,6 @@ export const App = () => {
 
       <div className="summary">
         {submitted && <Summary name={name} location={location} ageGroup={ageGroup} />}</div>
-
-      <div className="footer">
-        <h1>RUBBER SOUL.</h1>
-      </div>
 
     </div>
   )
