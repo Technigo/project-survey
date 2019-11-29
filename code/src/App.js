ab2b3b058f0704d21ff8356198d06ab0de3f201b
Jenfi <jenniferfick@LTWHYRED39.Whyred.local>
2019-11-29 17:28:38 +0100

    removed survey when showing result

--- code/src/App.js
+++ code/src/App.js
@@ -12,11 +12,17 @@ export const App = () => {
   const [definition, setDefinition] = useState("");
   const [love, setLove] = useState(false);
   const [showResult, setShowResult] = useState(false);
+  // const [showSurvey, setShowSurvey] = userstate(false);
 
   const handleSubmit = (event) => {
     event.preventDefault()
     setShowResult(true)
   }
+
+  // const showSurvey = (event) => {
+  //   event.preventDefault()
+  //   setShowSurvey(true)
+  // }
 
   const renderMessage = () => {
     if (love === "Yes") {
@@ -27,114 +33,119 @@ export const App = () => {
     }
   }
 
-
   return (
-
     <div>
-      <header>
-        <img src={hero} height="400px" width="auto" />
-        <h1>What do you really think of your partner?</h1>
-        <button onClick="">Find out here!</button>
-      </header>
-      <article>
-        <h1>Fill in and find the thruth of your heart</h1>
 
 
+      {showResult && (<div className="result">
+        <h1>The result</h1>
+        <h2>Hi {name}!</h2>
+        <p>Do you really love your partner {nickName}? Ooooohhh {love}!</p>
+        <p>For you, {nickName} is your {definition}</p>
+        {renderMessage()}
+      </div>
+      )}
 
-        <form onSubmit={handleSubmit}>
-          <section className="questionWrapper">
+      {!showResult && (
+        <div>
+          <header>
+            <img src={hero} height="400px" width="auto" />
+            <h1>What do you really think of your partner?</h1>
+            <button onClick="">Find out here!</button>
+          </header>
+          <article>
+            <h1>Fill in and find the thruth of your heart</h1>
 
-            <div class="question">
-              <h2>Hi there! What's your name?</h2>
-              <input
-                type="text"
-                onChange={event => setName(event.target.value)}
-                value={name}
-              />
-            </div>
-            <div className="seperator"></div>
-            {/* </form> */}
-          </section>
+            <form onSubmit={handleSubmit}>
+              <section className="questionWrapper">
 
-          <section className="questionWrapper">
-            {/* <form> */}
-            <div class="question">
-              <h2>What's your nickname of your partner?</h2>
-              <input
-                type="text"
-                onChange={event => setnickName(event.target.value)}
-                value={nickName}
-              />
-            </div>
-            <div className="seperator"></div>
-            {/* </form> */}
-          </section>
+                <div class="question">
+                  <h2>Hi there! What's your name?</h2>
+                  <input
+                    type="text"
+                    onChange={event => setName(event.target.value)}
+                    value={name}
+                  />
+                </div>
+                <div className="seperator"></div>
+                {/* </form> */}
+              </section>
 
-          <section className="questionWrapper">
-            {/* <form> */}
-            <div class="questionRadio">
-              <h2>Do you love your partner?</h2>
-              <label>
-                <input
-                  className="radio"
-                  type="radio"
-                  value="Yes"
-                  onChange={event => setLove(event.target.value)}
-                  checked={love === "Yes"}
-                />
-                <div class="fakeCheckbox"></div>
-                Yes
-              </label>
-              <label>
-                <input
-                  className="radio"
-                  type="radio"
-                  value="No"
-                  onChange={event => setLove(event.target.value)}
-                  checked={love === "No"}
-                />
-                <div class="fakeCheckbox"></div>
-                No
-              </label>
-            </div>
-            <div className="seperator"></div>
-            {/* </form> */}
-          </section>
+              <section className="questionWrapper">
+                {/* <form> */}
+                <div class="question">
+                  <h2>What's your nickname of your partner?</h2>
+                  <input
+                    type="text"
+                    onChange={event => setnickName(event.target.value)}
+                    value={nickName}
+                  />
+                </div>
+                <div className="seperator"></div>
+                {/* </form> */}
+              </section>
 
-          <section className="questionWrapper">
-            {/* <form> */}
-            <div class="dropdown">
-              <h2>What is your partner to you?</h2>
-              <select
-                onChange={event => setDefinition(event.target.value)}
-                value={definition}>
-                <option value="">Choose here!</option>
-                <option value="best friend for life!">Best friend for life</option>
-                <option value="greatest love!">Greatest love</option>
-                <option value="friend with benefits! Baam baaam!">Friend with benefits</option>
-                <option value="goofy goof ball!">Goof ball</option>
-                <option value="inspiration in life!">Muse in life</option>
-              </select>
-            </div>
-            <div className="seperator"></div>
-          </section>
-          <section className="submit">
-          <button
-            type="submit"
-          >Submit
-        </button>
-        </section>
-        </form>
-          {showResult && (<div className="result">
-            <h1>The result</h1>
-            <h2>Hi {name}!</h2>
-            <p>Do you really love your partner {nickName}? Ooooohhh {love}!</p>
-            <p>For you, {nickName} is your {definition}</p>
-            // {renderMessage()}
-            </div>
-          )}
+              <section className="questionWrapper">
+                {/* <form> */}
+                <div class="questionRadio">
+                  <h2>Do you love your partner?</h2>
+                  <label>
+                    <input
+                      className="radio"
+                      type="radio"
+                      value="Yes"
+                      onChange={event => setLove(event.target.value)}
+                      checked={love === "Yes"}
+                    />
+                    <div class="fakeCheckbox"></div>
+                    Yes
+      </label>
+                  <label>
+                    <input
+                      className="radio"
+                      type="radio"
+                      value="No"
+                      onChange={event => setLove(event.target.value)}
+                      checked={love === "No"}
+                    />
+                    <div class="fakeCheckbox"></div>
+                    No
+      </label>
+                </div>
+                <div className="seperator"></div>
+                {/* </form> */}
+              </section>
 
-      </article>
+              <section className="questionWrapper">
+                {/* <form> */}
+                <div class="dropdown">
+                  <h2>What is your partner to you?</h2>
+                  <select
+                    onChange={event => setDefinition(event.target.value)}
+                    value={definition}>
+                    <option value="">Choose here!</option>
+                    <option value="best friend for life!">Best friend for life</option>
+                    <option value="greatest love!">Greatest love</option>
+                    <option value="friend with benefits! Baam baaam!">Friend with benefits</option>
+                    <option value="goofy goof ball!">Goof ball</option>
+                    <option value="inspiration in life!">Muse in life</option>
+                  </select>
+                </div>
+                <div className="seperator"></div>
+              </section>
+              <section className="submit">
+                <button
+                  type="submit"
+                >Submit
+</button>
+              </section>
+            </form>
+          </article>
+        </div>
+
+      )}
+
+
       <footer>
       </footer>
     </div>
