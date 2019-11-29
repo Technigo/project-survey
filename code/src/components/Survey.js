2f5c49bb3fdbf79fb60e2d728e67cedc3d56aa7f
JoacimNilsen <joacimnilsen1986@gmail.com>
2019-11-29 12:41:59 +0100

    Radio buttons and checkbox done

--- code/src/components/Survey.js
+++ code/src/components/Survey.js
@@ -3,9 +3,24 @@ import React, { useState } from 'react';
 export const Survey = () => {
 	const [question, setQuestion] = useState('name');
 	const [name, setName] = useState('');
-	const [friend, setFriend] = useState('');
+	const [activity, setActivity] = useState('');
+	const [food, setFood] = useState('');
+	const [drinks, setDrinks] = useState('');
+	const [fun, setFun] = useState(3);
 
 	const [submit, setSubmit] = useState(false);
+
+	// Arrays for radiobuttons and select
+	const leasure = ['Concert', 'Football game', 'Pub', 'Wrestlingmatch'];
+	const drink = ['Beer', 'Trocadero', 'Wine', 'Coffee', 'Water'];
+	const meal = ['Pizza', 'Poke Bowl', 'Sushi', 'Punkgryta'];
+	const funMeter = [
+		'we really want to do this?',
+		'We are gonna have a drag',
+		'Dont forget there is work tomorrow',
+		'Now we are talking, wohooo',
+		'We are going to have the best night ever!!!'
+	];
 
 	const preventDefault = event => {
 		event.preventDefault();
@@ -14,7 +29,11 @@ export const Survey = () => {
 
 	const nextQ = () => {
 		if (question === 'name') {
-			setQuestion('friend');
+			setQuestion('activity');
+		} else if (question === 'activity') {
+			setQuestion('food');
+		} else if (question === 'food') {
+			setQuestion('fun');
 		}
 	};
 
@@ -22,15 +41,85 @@ export const Survey = () => {
 		<div>
 			<form onSubmit={preventDefault}>
 				{question === 'name' && (
-					<div className='name'>
+					<div className='form'>
+						<p>What is your name?</p>
 						<label>
-							What is your name?
 							<input
 								type='text'
 								onChange={event => setName(event.target.value)}
 								value={name}
 							/>
 						</label>
+					</div>
+				)}
+
+				{question === 'activity' && (
+					<div className='form'>
+						<p>
+							Oh, hi {name}! I feel like doing something fun today. What do you
+							reckon?
+						</p>
+						{leasure.map(options => (
+							<label key={options} className='radio'>
+								<input
+									type='radio'
+									value={options}
+									onChange={event => setActivity(event.target.value)}
+									checked={activity === options}
+								/>
+								<span className='radio-label'>{options}</span>
+							</label>
+						))}
+					</div>
+				)}
+
+				{question === 'food' && (
+					<div className='form'>
+						<p>
+							Ok {name}, {activity} it is! But i am kind of hungry, we should
+							eat something before we go. Choose some drinks while you are at
+							it!
+						</p>
+						{meal.map(options => (
+							<label className='select'>
+								<input
+									key={options}
+									type='checkbox'
+									value={options}
+									onChange={event => setFood(event.target.value)}
+									checked={food === options}
+								/>
+								<span className='check-label'>{options}</span>
+							</label>
+						))}
+						{drink.map(options => (
+							<label className='select'>
+								<input
+									key={options}
+									type='checkbox'
+									value={options}
+									onChange={event => setDrinks(event.target.value)}
+									checked={drinks === options}
+								/>
+								<span className='check-label'>{options}</span>
+							</label>
+						))}
+					</div>
+				)}
+
+				{question === 'fun' && (
+					<div className='form'>
+						<p>Last but not least {name}, how fun are we going to have?</p>
+						<label>
+							<input
+								type='range'
+								min={1}
+								max={5}
+								value={fun}
+								onChange={event => setFun(event.target.value)}
+							/>
+						</label>
+						<div className='funMeter'>{fun}</div>
 					</div>
 				)}
 				<button type='button' onClick={nextQ}>
