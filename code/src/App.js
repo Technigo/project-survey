c620dc1a3d688955cc10b3ecdb129e9750c57bb8
sarah wolfson <swolfson23@gmail.com>
2019-11-29 13:56:13 +0800

    testing

--- code/src/App.js
+++ code/src/App.js
@@ -3,7 +3,11 @@ import React, { useState } from 'react'
 import { Summary } from 'Summary'
 
 
-const hairstyle = ["Golden horn", "Pretty pink", "Rainbow pony", "Giltter puff"]
+const hairstyle = [
+
+  "Golden horn", "Pretty pink", "Rainbow pony", "Giltter puff"
+
+]
 
 export const App = () => {
   const [name, setName] = useState(" ");
@@ -18,7 +22,7 @@ export const App = () => {
 
         <p>what's your unicorn name?</p>
         <input
-          className="formfield"
+          className="formField"
           type="text"
           onChange={event => setName(event.target.value)}
           value={name}
@@ -29,11 +33,13 @@ export const App = () => {
           onChange={event => setUnicorn(event.target.value)}
           value={unicorn}
         >
+
           <option value="">Chocie the kingdom.</option>
           <option value="Crouhan Kingdom">Crouhan Kingdom.</option>
           <option value="Shothia Dynasty">Shothia Dynasty.</option>
           <option value="Vedduilyra Dynasty">Vedduilyra Dynasty.</option>
           <option value="Crouhan Kingdom">Oquqarian Empire.</option>
+
 
         </select>
         <p>Select a funky Fresh hairstyle!</p>
@@ -48,11 +54,11 @@ export const App = () => {
             {uni}
           </label>
         ))}
-
+        <button
+          onClick={() => setSubmit(true)}
+        >Adopt me!</button>
       </form>
-      <button
-        onClick={() => setSubmit(true)}
-      >Adopt me!</button>
+
       {submit && <Summary name={name} unicorn={unicorn} hair={hair} />}
     </div>
   )
--- code/src/Summary.js
+++ code/src/Summary.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 export const Summary = (props) => {
 
-  return <div className="Summary">
-    <p>{props.name}is so happy that you have picked them!</p>
-    <p>{props.name} comes from {props.unicorn}</p>
-    <p>{props.name} is very happy with the hairstyle you have chosen,{props.hair}</p>
-  </div>
+  return (
+    <div className="Summary">
+      <p>{props.name} is so happy that you have picked them!</p>
+      <p>{props.name} comes from the {props.unicorn}.</p>
+      <p>{props.name} is very happy with the hairstyle you have chosen,{props.hair}</p>
+    </div>
+  )
 };
+
