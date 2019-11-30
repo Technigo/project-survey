859350445663e9de87748e93e721e0b2823cfbc3
Petra B <petrabrinck@icloud.com>
2019-11-30 21:30:39 +0100

    name changes and more css

--- code/src/App.js
+++ code/src/App.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react'
 import { Summary } from './Summary'
 
+
+const adventureGroup = ["Surfing", "Climbing", "Yoga Retreat", "Hiking"];
+const destinationGroup = ["Costa Rica", "Mount Everest", "Bali", "Machu Picchu",];
 const ageGroups = ["alone", "with family members", "with friends"];
-const destinationGroup = ["Costa Rica", "Mount Everest", "Machu Picchu"];
 
 export const App = () => {
   const [name, setName] = useState("");
+  const [adventures, setAdventures] = useState();
   const [destinations, setDestinations] = useState();
-  const [location, setLocation] = useState("");
+  // const [location, setLocation] = useState("");
+  const [mostImportant, setMostImportant] = useState("");
   const [ageGroup, setAgeGroup] = useState()
+  // const [companionGroup, setCompanionGroup] = useState()
   const [submitted, setSubmitted] = useState(false)
 
 
@@ -22,7 +27,7 @@ export const App = () => {
           <form onSubmit={event => event.preventDefault()}>
 
             <div className="heroHeader">
-              <h1>Plan your 2020 adventure now!</h1>
+              <h2>Plan your 2020 adventure now!</h2>
             </div>
 
 
@@ -36,7 +41,23 @@ export const App = () => {
               />
             </div>
 
-            <p>What kind of adventures are you up for?</p>
+            <p>What kind of adventure are you up for?</p>
+            <div className="Adventures">
+              {adventureGroup.map(group => (
+                <label key={group}>
+                  <input className="adventures"
+                    type="checkbox"
+                    value={group}
+                    onChange={event => setAdventures(event.target.value)}
+                    checked={adventures === group}
+                  />
+                  {group}
+
+                </label>
+              ))}
+            </div>
+
+            <p>Where would you like to go?</p>
             <div className="Destinations">
               {destinationGroup.map(group => (
                 <label key={group}>
@@ -54,21 +75,35 @@ export const App = () => {
 
 
 
-            <div className="nextDestinationBox">
+
+
+            <div className="mostImportant">
 
               <p>What’s the most important thing when you travel?</p>
               <select className="formDropdown"
-                onChange={event => setLocation(event.target.value)}
+                onChange={event => setMostImportant(event.target.value)}
                 value={name}
               >
                 <option value="">Choose here!</option>
+                <option value="low-impact tourism policy">to step out of the comfort zone </option>
                 <option value="low-impact tourism policy">low-impact tourism policy </option>
                 <option value="food and beverage">great food and beverage</option>
-                <option value="tourist attractions">tourist attractions</option>
+                <option value="tourist attractions">many tourist attractions</option>
               </select>
             </div>
 
 
+
+            {/* <div className="companion">
+              <p>How do you prefer to travel?</p>
+              {companyGroup.map(group => (
+                <label key={group}>
+                  <input className="radioBtn"
+                    type="radio"
+                    value={group}
+                    onChange={event => setCompanionGroup(event.target.value)}
+                    checked={companionGroup === group}
+                  /> */}
 
             <div className="ageBox">
               <p>How do you prefer to travel?</p>
@@ -95,12 +130,10 @@ export const App = () => {
         </div>
       )}
 
-      {submitted && <Summary name={name} destinations={destinations} location={location} ageGroup={ageGroup} />}
+      {submitted && <Summary name={name} adventures={adventures} destinations={destinations} mostImportant={mostImportant} ageGroup={ageGroup} />}
 
     </div>
 
   )
 }
-
-
 
--- code/src/Summary.js
+++ code/src/Summary.js
@@ -7,11 +7,12 @@ export const Summary = (props) => {
     return (
         <div className="Summary">
             <h2>Thank you for your answers {props.name}!</h2>
-            <p>Cool, you are up for {props.destinations} adventures!</p>
+            <p>Cool, you are up for {props.adventures} in {props.destinations}!</p>
+            {/* <p>{props.destinations}!</p> */}
 
-            <p>Most important to you is {props.location}</p>
-            <p>and you like to travel {props.ageGroup}</p>
-            <p>Have a nice trip {props.name}!</p>
+            <p>The most important thing to you is {props.mostImportant}</p>
+            <p>and you like to travel {props.ageGroup}.</p>
+            <p>Have a great adventure 2020 {props.name}!</p>
         </div>
     )
 }
