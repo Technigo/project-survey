5bdce87477aa03c7d6a50cf8aa3b9c9f93eff6fa
Petra B <petrabrinck@icloud.com>
2019-12-01 21:36:08 +0100

    still on it

--- code/src/Summary.js
+++ code/src/Summary.js
@@ -7,7 +7,7 @@ export const Summary = (props) => {
         <div className="Summary">
             <h2>Thanks for your answers {props.name}!</h2>
             <p>Cool, you are up for {props.adventures} in {props.destinations}.</p>
-            <p>The most important thing to you is {props.mostImportant}.</p>
+            <p>The most important thing to you is {props.mostImportant}</p>
             <p>and you like to travel {props.companionGroup}.</p>
             <p>Have a great 2020 adventure {props.name}!</p>
         </div>
