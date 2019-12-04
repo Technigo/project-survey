6650e49749aa6e09cf1cf2d931f27ab7fce5d0a0
Camilla <camillalofroth@hotmail.com>
2019-12-04 15:25:24 +0100

    finalish version6

--- code/src/summary.js
+++ code/src/summary.js
@@ -5,6 +5,6 @@ export const Summary = (props) => (
         <h1>Heres your result!</h1>
         <p>Hello {props.feeling}</p>
         <p>You are {props.happiness < 3 ? 'so so' : 'Super happy'} with our service</p>
-        <p> You choose {props.treatment}</p>
+        <p> You choose to do {props.treatment}</p>
         </div>
 )
