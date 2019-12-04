a7d4da388de09fca30fe3c3742f02fe0c608745a
nyblad <50693799+nyblad@users.noreply.github.com>
2019-12-04 20:21:51 +0100

    Update code/src/App.js
    Co-Authored-By: Erica  Glimsholt <ericaglimsholt@gmail.com>

--- code/src/App.js
+++ code/src/App.js
@@ -14,7 +14,7 @@ export const App = () => {
   const [submitted, setSubmitted] = useState(false)   //False because form is not submitted from start (initial value)
 
   //Array with values for mapping radio buttons in #3
-  const radioButtons = ["Apple", "Banana", "Minions?"]
+  const fruits = ["Apple", "Banana", "Minions?"]
   //Array with values for mapping checkboxes in #4
   const checkboxes = ["Yes", "No", "The slowest"]
 
@@ -168,4 +168,3 @@ export const App = () => {
 
   )
 }
-
