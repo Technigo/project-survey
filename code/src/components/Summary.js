25065a462d86ffbdc01f3fe0cac55c576e4a91cf
nyblad <nyblad@hotmail.com>
2019-12-13 21:14:06 +0100

    Added conditional operators for correct/wrong answers

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -11,13 +11,13 @@ export const Summary = (props) => {
     <div className="summary">
       <h2>Thank you {props.text}!</h2>
       <p className="user-answer">#1: {props.select}</p>
-      <p className="correct-answer"> Vacayay of course! Or home sweet home <span role="img" aria-label="house">🏡</span></p>
+      <p className="correct-answer"> {props.select === ("Home" || "Vacayay!") ? "Correct!" : "Wrooong!"} Vacayay of course. Or home sweet home <span role="img" aria-label="house">🏡</span></p>
       <p className="user-answer">#2: {props.radioButton}</p>
-      <p className="correct-answer">Minions crave for <span role="img" aria-label="banana">🍌</span></p>
+      <p className="correct-answer">{props.radioButton === "Banana" ? "Correct!" : "Wrooong!"} Minions crave for <span role="img" aria-label="banana">🍌</span></p>
       <p className="user-answer">#3: {props.checkbox}</p>
-      <p className="correct-answer">Yes, sloths are slow. The slowest.</p>
-      <p className="user-answer">#4: {props.range}</p>
-      <p className="correct-answer">Depending on legend Santa has 8 or 9 <span role="img" aria-label="deer">🦌</span></p>
+      <p className="correct-answer">{props.checkbox === ("Yes" || "The slowest") ? "Correct!" : "Wrooong!"} Sloths are slow. The slowest.</p>
+      <p className="user-answer">#4 : {props.range}</p>
+      <p className="correct-answer">{props.range === (8 || 9) ? "Correct!" : "Wrooong!"} Depending on legend Santa has 8 or 9 <span role="img" aria-label="deer">🦌</span></p>
       <button className="restart" onClick={refreshPage}>Again?</button>
     </div>
   )
