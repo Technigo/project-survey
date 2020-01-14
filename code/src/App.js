d5995e6e22b86c1b6c46be2d4edc789783a5dc92
Jennie Dalgren <jennie.dalgren@gmail.com>
2020-01-14 09:51:53 +0100

    changed the copy of the website

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react"
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
+  return <div>Find me in src/app.js! ökfds lkfds ljfds</div>
 }
