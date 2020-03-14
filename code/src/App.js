2297f740e6327dbf50b161e7488649d543220551
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-03-14 10:23:11 +0100

    added textinput component wih useState

--- code/src/App.js
+++ code/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Radiobuttons } from './components/Radiobuttons'
+import { Textinput } from './components/Textinput'
 
 export const App = () => {
   return (
     <>
       <Radiobuttons />
+      <Textinput />
     </>
   )
 }
---
+++ code/src/components/Textinput.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+
+export const Textinput = () => {
+
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+
+  return (
+    <>
+      <h2>Your contact details</h2>
+      <form action="">
+        <label htmlFor="firstName">First name:</label>
+        <input
+          type="text"
+          id="firstName"
+          onChange={event => setFirstName(event.target.value)}
+          value={firstName}
+        ></input>
+        <label htmlFor="lastName">Last name:</label>
+        <input
+          type="text"
+          id="lastName"
+          onChange={event => setLastName(event.target.value)}
+          value={lastName}
+        ></input>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          onChange={event => setEmail(event.target.value)}
+          value={email}
+        ></input>
+      </form>
+    </>
+  )
+}
