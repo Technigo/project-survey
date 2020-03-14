60f18d28e09093f434cd4957c93d1851752ecdf5
Frida Eriksson <Frida.Eriksson@silf.se>
2020-03-14 10:29:51 +0100

    added dropdown

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+/* eslint-disable linebreak-style */
+import React, { useState } from 'react'
 
 export const App = () => {
+  const [option, setOption] = useState("")
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <form>
+      <select onChange={event => setOption(event.target.value)} value={option}>
+        <option value="">Select option</option>
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+      </select>
+    </form>
   )
 }
