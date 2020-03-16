55df7336f3fb73008d5b7fc4802a579527331524
bealun <53275597+bealun@users.noreply.github.com>
2020-03-16 16:59:20 +0100

    changed survey

--- code/src/components/Result.js
+++ code/src/components/Result.js
@@ -7,7 +7,6 @@ export const Result = (props) => {
       <h3 className="textResult">Okey, {props.name}!</h3> 
       <h3>You'd like to go to {props.whichPlanet}</h3>
       <h3>with {props.bringWith}!</h3>
-
       <h4>Safe travels, my friend!</h4>
       
     </div>
--- code/src/components/Survey.js
+++ code/src/components/Survey.js
@@ -3,103 +3,111 @@ import { Result } from './Result'
 
 
 const company = ['BFF',
-'Your Pet',
-'Your Mother',
-'No one',
-'Your Bed',
-'Random Stranger',
-'Michael Scott',
-'Donald Trump']
+  'Your Pet',
+  'Your Mother',
+  'No one',
+  'Your Bed',
+  'Random Stranger',
+  'Michael Scott',
+  'Donald Trump']
 
 const planets = ['Mercury',
-'Venus',
-'Earth',
-'Mars',
-'Jupiter',
-'Saturn',
-'Uranus',
-'Neptune']
+  'Venus',
+  'Earth',
+  'Mars',
+  'Jupiter',
+  'Saturn',
+  'Uranus',
+  'Neptune']
 
 const Question = {
   Q1: "What's your name?",
   Q2: "Which planet would you like to visit?",
-  Q3: "Who would you bring?"}
+  Q3: "Who would you bring?"
+}
 
 export const Survey = () => {
 
   const [name, setName] = useState()
   const [bringWith, setBringWith] = useState()
   const [whichPlanet, setWhichPlanet] = useState()
-  const [submit, setSubmit] = useState(false)
-  
+  const [showSummary, setShowSummary] = useState(false)
+
   return (
     <div>
-    {!submit && (
-    <form className="user-form" 
-    onSubmit={event => event.preventDefault()}>
-    
-    <div className="completeSurvey">
+      {!showSummary && (
+        <form
+          className="user-form"
+          onSubmit={event =>
+            event.preventDefault()}>
 
-      <div className="nameInput">
-        <h2>{Question.Q1}</h2>
-        <input type="text" className="text-input"
-        onChange={event => setName(event.target.value)} 
-        value={name} placeholder="Your Name" tabIndex="0"required />
-      </div>
+          {/* .window.scrollTo(0, 0) */}
 
+          <div className="completeSurvey">
 
-      <div className="select-planet">
-        <h2>{Question.Q2}</h2>
-        <div className="planets">
-          {planets.map(planet => (
-            <label key={planet} className="radio-btn" tabIndex="0">
-              <input type="radio" 
-              value={planet}
-              onChange={event => setWhichPlanet(event.target.value)}
-              checked={whichPlanet === planet}
-              id={planet}
-              className="radio"
-              required/>
+            <div className="nameInput">
+              <h2>{Question.Q1}</h2>
 
-              <span className="check"></span>
-              {planet}
-
-            </label>
-          ))}
-        </div>
-      </div>
+              <input type="text" className="text-input"
+                onChange={event => setName(event.target.value)}
+                value={name} placeholder="Your Name" tabIndex="0" required />
+            </div>
 
 
-      <div className="question">
-        <h2>{Question.Q3}</h2>
+            <div className="select-planet">
+              <h2>{Question.Q2}</h2>
 
-        <div>
-        <select
-          onChange={event => setBringWith(event.target.value)}
-          value={bringWith} 
-          tabIndex="0"
-          checked={bringWith === company}
-          defaultValue="---">
-          
-          <option disabled>---</option>
-          {company.map(who => (
-            <option key={who} value={who} tabIndex="0" required>{who}</option>
-          ))}
-        </select>
-      </div>
+              <div className="planets">
+                {planets.map(planet => (
+                  <label key={planet} className="radio-btn" tabIndex="0">
+                    <input type="radio"
+                      value={planet}
+                      onChange={event => setWhichPlanet(event.target.value)}
+                      checked={whichPlanet === planet}
+                      id={planet}
+                      className="radio"
+                      required />
 
-      <div className="submit-btn">
-        <button onClick={() => setSubmit(true)}
-        className="submit" 
-        tabIndex="0">Done!</button>
-      </div>
-      </div>
-      
-        
-    </div>
-    </form>
-    )}
-    {submit && <Result name={name} whichPlanet={whichPlanet} bringWith={bringWith} />}
+                    <span className="check"></span>
+                    {planet}
+
+                  </label>
+                ))}
+              </div>
+            </div>
+
+
+            <div className="question">
+              <h2>{Question.Q3}</h2>
+
+              <div>
+                <select required
+                  onChange={event => setBringWith(event.target.value)}
+                  value={bringWith}
+                  tabIndex="0"
+                  checked={bringWith === company}
+                  defaultValue="---">
+
+                  <option disabled>---</option>
+                  {company.map(who => (
+                    <option key={who} value={who} tabIndex="0" required>{who}</option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="submit-btn">
+                <button onSubmit={() => setShowSummary(true)}
+                  className="submit"
+                  tabIndex="0">Done!</button>
+              </div>
+
+
+            </div>
+          </div>
+        </form>
+
+      )}
+      {showSummary && <Result name={name} whichPlanet={whichPlanet} bringWith={bringWith} />}
     </div>
   )
 }
