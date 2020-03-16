1d6fd410cf01dd784ca1420c7b40acf08a98c214
Ruterryter <50732755+Ruterryter@users.noreply.github.com>
2020-03-16 14:55:50 +0100

    some styling

--- code/src/App.js
+++ code/src/App.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react'
 import './app.css'
 
 
-const ageGroups = ['too young', '19-35', '35-65', '+65'];
+const ageGroup = ['too young', '19-35', '35-65', '+65'];
 
 export const App = () => {
-  const [ageGroup, setAgeGroup] = useState();
+  const [ageGroups, setAgeGroup] = useState("");
   const [hobby, setHobby] = useState("");
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [showSummary, setShowSummary] = useState(false);
+  const handleSubmit = event => {
+    event.preventDefault();
+    setShowSummary(true);
+  };
 
   return (
     <div className='form-container'>
@@ -18,14 +24,14 @@ export const App = () => {
       <div className='questions-container'>
         <div className="questions">
 
-          <form onSubmit={event => event.preventDefault()}>
+          <form onSubmit={handleSubmit}>
             <h2>What is your name?</h2>
             <input type='text'
               onChange={event => setName(event.target.value)}
               value={name}>
             </input>
             <h2>{`How old are you ${name}?`}</h2>
-            {ageGroups.map(group => (
+            {ageGroup.map(group => (
               <label key={group}>
                 <input type='radio'
                   value={group}
@@ -37,7 +43,7 @@ export const App = () => {
             ))}
 
             <h2> What is a perfect evenig according to you?</h2>
-            <select
+            <select className="hobby"
               onChange={event => setHobby(event.target.value)}
               value={hobby}>
               <option value="resturant"> Candlelight dinner in a cosy resturant</option>
@@ -47,14 +53,24 @@ export const App = () => {
               <option value="concert"> Headbangers ball concert</option>
             </select>
 
-            <h2> {`${name}`}, please type in your email address?</h2>
-            <input type="text" required></input>
+            <h2> {`${name}`}, please type in your email address</h2>
+            <input type="email" value={email} required onChange={event => setEmail(event.target.value)}></input>
 
             <h2>How exicited are you?!</h2>
 
             <button type="submit">Join the competion!</button>
-
           </form>
+
+          {showSummary && <section className="summary">
+            <h2>We got your details:</h2>
+            <p>Name: {name} </p>
+            <p> Age: {ageGroups}</p>
+            <p> You love: {hobby} </p>
+            <p>Your email address: {email}</p>
+            <p>If you are the lucky winner we will send you and email within 2 weeks with all the details
+              If you are not the winner we will send you some inspiration about fun things to do in the city.</p>
+          </section>
+          }
 
         </div>
       </div>
