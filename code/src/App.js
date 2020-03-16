4389a5a0724d99aca55a998029d7c8337baa5fd8
Ruterryter <50732755+Ruterryter@users.noreply.github.com>
2020-03-16 10:00:35 +0100

    some styling

--- code/src/App.js
+++ code/src/App.js
@@ -10,50 +10,54 @@ export const App = () => {
   const [name, setName] = useState("");
 
   return (
-    <div className='intro'>
+    <div className='form-container'>
       <h1>Win a very special evening for you and someone equally as special!</h1>
       <h3>Just fill in the form below to participate in the competion</h3>
       <p>... and you could be the lucky one winning a custom made evening for you and your friend.</p>
 
-      <form onSubmit={event => event.preventDefault()}>
-        <h2>What is your name?</h2>
-        <input type='text'
-          onChange={event => setName(event.target.value)}
-          value={name}>
-        </input>
-        <h2>{`How old are you ${name}?`}</h2>
-        {ageGroups.map(group => (
-          <label key={group}>
-            <input type='radio'
-              value={group}
-              onChange={event => setAgeGroup(event.target.value)}
-              checked={ageGroups === group}
-            />
-            {group}
-          </label>
-        ))}
+      <div className='questions-container'>
+        <div className="questions">
 
-        <h2> What is a perfect evenig according to you?</h2>
-        <select
-          onChange={event => setHobby(event.target.value)}
-          value={hobby}>
-          <option value="resturant"> Candlelight dinner in a cosy resturant</option>
-          <option value="netflix"> Netlfix and Chill</option>
-          <option value="dancing"> Dancing on my own</option>
-          <option value="party"> Party with my friends</option>
-          <option value="concert"> Headbangers ball concert</option>
-        </select>
+          <form onSubmit={event => event.preventDefault()}>
+            <h2>What is your name?</h2>
+            <input type='text'
+              onChange={event => setName(event.target.value)}
+              value={name}>
+            </input>
+            <h2>{`How old are you ${name}?`}</h2>
+            {ageGroups.map(group => (
+              <label key={group}>
+                <input type='radio'
+                  value={group}
+                  onChange={event => setAgeGroup(event.target.value)}
+                  checked={ageGroups === group}
+                />
+                {group}
+              </label>
+            ))}
 
-        <h2> {`${name}`}, please type in your email address?</h2>
-        <input type="text" required></input>
+            <h2> What is a perfect evenig according to you?</h2>
+            <select
+              onChange={event => setHobby(event.target.value)}
+              value={hobby}>
+              <option value="resturant"> Candlelight dinner in a cosy resturant</option>
+              <option value="netflix"> Netlfix and Chill</option>
+              <option value="dancing"> Dancing on my own</option>
+              <option value="party"> Party with my friends</option>
+              <option value="concert"> Headbangers ball concert</option>
+            </select>
 
-        <h2>How exicited are you?!</h2>
+            <h2> {`${name}`}, please type in your email address?</h2>
+            <input type="text" required></input>
 
-        <button type="submit">Join the competion!</button>
+            <h2>How exicited are you?!</h2>
 
-      </form>
+            <button type="submit">Join the competion!</button>
 
+          </form>
 
+        </div>
+      </div>
     </div>
   )
 }
