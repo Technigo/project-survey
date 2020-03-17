b7df45017a07ad5b7e80949898de48d6155f9fae
Elin Aarenstrup <55394675+elinaarenstrup@users.noreply.github.com>
2020-03-17 19:52:41 +0100

    Added some styling

--- code/src/App.js
+++ code/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import "./app.css";
 const drinkGroups = ["coffee", "tea", "water"];
 
 /* Statehooks */
@@ -19,67 +19,81 @@ export const App = () => {
   return (
     <div className="formContainer">
       <form onSubmit={handleSubmit}>
-        <label>
-          Hey you, what's your name?
-          {/* controlled form */}
-          <input
-            className="nameField"
-            type="text"
-            onChange={event =>
-              setName(event.target.value)
-            } /* invoke setName with event.target.value */
-            value={name} /* reflect current state */
-            required
-          />
-        </label>
-        {/* RADIOBUTTONS */}
-        What drink do you prefer to your "fika":
-        {drinkGroups.map(group => (
-          <label key={group}>
+        <section className="questionCard">
+          <label>
+            <p>Hey you, what's your name?</p>
+            {/* controlled form */}
             <input
-              type="radio"
-              value={group}
-              onChange={event => setDrinkgroup(event.target.value)}
-              checked={drinkGroup === group}
+              className="nameField"
+              type="text"
+              onChange={event =>
+                setName(event.target.value)
+              } /* invoke setName with event.target.value */
+              value={name} /* reflect current state */
+              required
             />
-            {group}
           </label>
-        ))}
-        <label>
-          What do you prefer to eat when you "fikar"?
-          <input
-            className="fikaField"
-            type="text"
-            onChange={event => setFika(event.target.value)}
-            value={fika}
-            required
-          />
-        </label>
-        <label>
-          {/* DROPDOWN */}
-          <p>Where do you want to "fika"?</p>
-          {/* use select instead of form on dropdown */}
-          <select
-            className="formDropdown"
-            onChange={event => setPlace(event.target.value)}
-            value={place}
-          >
-            <option value="">Choose one!</option> {/* Label, no value */}
-            <option value="at a café">at a café</option>
-            <option value="at home">at home</option>
-            <option value="outside in the sun">outside in the sun</option>
-          </select>
-        </label>
+        </section>
+        {/* RADIOBUTTONS */}
+        <section className="questionCard">
+          <p>What drink do you prefer to your "fika":</p>
+          {drinkGroups.map(group => (
+            <div className="radioDiv">
+              <label key={group}>
+                <input
+                  type="radio"
+                  value={group}
+                  onChange={event => setDrinkgroup(event.target.value)}
+                  checked={drinkGroup === group}
+                />
+                {group}
+              </label>
+            </div>
+          ))}
+        </section>
+
+        <section className="questionCard">
+          <label>
+            <p>What do you prefer to eat when you "fikar"?</p>
+            <input
+              className="fikaField"
+              type="text"
+              onChange={event => setFika(event.target.value)}
+              value={fika}
+              required
+            />
+          </label>
+        </section>
+
+        <section className="questionCard">
+          <label>
+            {/* DROPDOWN */}
+            <p>Where do you want to "fika"?</p>
+            {/* use select instead of form on dropdown */}
+            <select
+              className="formDropdown"
+              onChange={event => setPlace(event.target.value)}
+              value={place}
+            >
+              <option value="">Choose one!</option> {/* Label, no value */}
+              <option value="at a café">at a café</option>
+              <option value="at home">at home</option>
+              <option value="outside in the sun">outside in the sun</option>
+            </select>
+          </label>
+        </section>
+
         <button type="submit">Submit</button>
       </form>
 
       {summary && (
-        <section>
+        <section className="summarySection">
           {" "}
-          <h1>
-            Hey {name}! So you like to eat {fika} and drink {drinkGroup} {place}
-            . Sounds sweet! Let's go...
-          </h1>
+          <h2>Hey {name}! </h2>{" "}
+          <p>
+            So you like to eat {fika} and drink {drinkGroup} {place}. Sounds
+            sweet! Let's go...
+          </p>
         </section>
       )}
     </div>
