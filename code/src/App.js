9f39ee176c46ccb9beb8aec289b28545f180b019
palhamel <pal.hamel@gmail.com>
2020-03-17 10:49:29 +0100

    Change to all questions in app.js and showSummery with handleSubmit

--- code/src/App.js
+++ code/src/App.js
@@ -1,30 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { Agequest } from "./QuestRadio";
-import { Selectanimal } from "./QuestAnimal";
-import { Myname } from "QuestName";
+// import { Selectanimal } from "./QuestAnimal";
+// import { Myname } from "./QuestName";
+
+const ageGroups = ["18-25", "19-34", "35+"];
 
 export const App = () => {
 
+  
+  // const [ageGroup, setAgeGroup] = useState();
+  const [name, setName] = useState("");
+  const [ageGroup, setAgeGroup] = useState();
+  const [animal, setAnimal] = useState('');
+  const [showSummery, setShowSummery] = useState(false);
 
   const handleSubmit = event => {
     event.preventDefault();
+    setShowSummery(true);
   };
 
 
   return (
     <div>
-      <h1>My survey</h1>
+      <h1>My survey:</h1>
       <form onSubmit={handleSubmit}>
+        {/* <p>{name}</p> */}
 
-      <Agequest />
-      <Selectanimal />
-      <Myname />
-
-
-
+      {/* <Agequest /> */}
+      {/* <Selectanimal /> */}
+      {/* <Myname /> */}
 
 
+        <label>
+          <h3>What is your name?</h3>
+          <input
+          type="text"
+          value={name}
+          onChange={event => setName(event.target.value)}
+          required
+          placeholder="name here.."
+          // onKeyPress={(event) => event.key === "Enter" && event.preventDefault()}
+          />
+        </label>
+
+        <div>
+          <h3>How old are you?</h3>
+          {ageGroups.map(group => (
+            <label key={group}>
+              <input 
+                type="radio"
+                value={group}
+                onChange={event => setAgeGroup(event.target.value)}
+                checked={ageGroup === group}
+              />
+              {group}
+            </label>
+          ))}
+        </div>
+
+        
+
+        <label>
+          <h3>What is your favorite animal?</h3>
+          <select
+            onChange={event => setAnimal(event.target.value)}
+            value={animal}
+          >
+            <option value="">Select animal:</option>
+            <option value="guinea pig">Guinea pig</option>
+            <option value="guinea pig, again">Guinea pig, again</option>
+            <option value="guinea pig, of course!">Guinea pig, of course!</option>            
+          </select>
+        </label>
+
+
+        <button type="submit">submit</button>
       </form>
+
+
+      {showSummery && <section>
+        <h1>Hi {name} ! You are {ageGroup} years old and absolutley love {animal}'s!</h1>
+        
+      </section>}
+
+
+
     </div>
   );
 };
