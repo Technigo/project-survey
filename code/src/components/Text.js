3dce1064df65347401b49be9e1ddeaf137a28c78
Emelie Loheden <emelie_loheden@hotmail.com>
2020-03-17 22:20:29 +0100

    more text input in Text

--- code/src/components/Text.js
+++ code/src/components/Text.js
@@ -4,6 +4,7 @@ import './text.css'
 export const Text = () => {
 
   const [name, setName] = useState("");
+  const [alias, setAlias] = useState("");
   const handleSubmit = event => {
     event.preventDefault();
   }
@@ -15,6 +16,14 @@ export const Text = () => {
         <input type="text"
           onChange={event => setName(event.target.value)}
           value={name}
+        />
+      </label>
+
+      <label>
+        <h1>Whats your alias? </h1>
+        <input type="text"
+          onChange={event => setAlias(event.target.value)}
+          value={alias}
         />
       </label>
     </form>
