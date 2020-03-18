bdd066e47d525a47ab7e6c0faf931ff24a256b55
annesophiegendron <annesophie.gendron@gmail.com>
2020-03-18 13:52:36 +0100

    text correction

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -12,7 +12,7 @@ export const Summary = (props) => {
         <h3 className="summaryTitle">Alright {props.name}! Let's sum up: </h3>
         <p>We will see you there on {props.dayPreference}... That's great! </p>
         <p>We are glad to hear that you feel {props.feeling} about it.</p>
-        <p>Apparently, you would rather go for {props.foodPreference}... We assume that it was a tough decision to take!</p>
+        <p>You would rather go for {props.foodPreference}... We assume that it was a tough decision to take!</p>
         <p>And we will make sure that you do not find any odd ingredients ({props.allergy}) in your plate <span role="img" aria-label="black heart">🖤</span></p>
         <iframe title="happyGif" style={{ borderRadius: 100 }} src="https://giphy.com/embed/Lb4IZLmCfALhm" width="180" height="180" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
       </div>
