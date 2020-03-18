4a69ed873307b9f9b42ae73f210739a0a0edb868
Johanna Åstrand <johanna@johannas-mbp.mynet>
2020-03-18 21:55:15 +0100

    small copy improvement

--- code/src/Compontents/Summary.js
+++ code/src/Compontents/Summary.js
@@ -6,7 +6,7 @@ export const Summary = ({ name, tech, radioInput }) => {
     <section>
       <h1>Thank you {name}! <span aria-label='Party symbol' role='img'>🎉</span></h1>
       <p>We agree, {tech} is amazing!</p>
-      <p>So you would {radioInput} recommend your friends to start coding!</p>
+      <p>So you would {radioInput} recommend your friends to start coding? Challenge them!</p>
       <p>Could not agree more, you are an <span className='bold'>awesome developer</span> and you are doing great!</p>
     </section>
   )
