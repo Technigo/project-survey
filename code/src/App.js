0ef1e3d3c9f87c99b0370b1bdd2dce64a3e8914f
Ida Abrahamsson <Ida Abrahamsson>
2020-03-18 20:17:11 +0100

    Made a start will improve on it

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
+    const [name, setName] = useState("")
+
+    const handleSubmit = event => {
+        event.preventDefault()
+    }
+
+    return (
+        <div className="firstSurvey">
+            <header>
+                <h1>Washing them hands</h1>
+            </header>
+
+            <form>
+                <label>
+                    How many times do you wash?
+                <input
+                        type="radio"
+                        onChange={}
+                        value={}
+                    />
+                </label>
+            </form>
+
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Your favorite soap?
+            <input
+                        type="text"
+                        onChange={event => setName(event.target.value)}
+                        value={name}
+                    />
+                </label>
+            </form>
+        </div>
+
+    )
+
+
 }
