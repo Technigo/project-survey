c4c5a81f9a7d10fbda198c79382d6d877193e40e
annaelsamaria <hej@annagustafsson.se>
2020-03-18 19:56:10 +0100

    done with the project, added a map and shortened the css a bit.

--- code/src/App.js
+++ code/src/App.js
@@ -9,6 +9,7 @@ export const App = () => {
   const [smile, setSmile] = useState("");
   const [showSummary, setShowSummary] = useState(false);
 
+  const smileOptions = ["Yes", "No", "Maybe"]
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -24,7 +25,7 @@ export const App = () => {
             <h1>Reflections of your day</h1>
             <a href="#happy"><img className="arrow" src="../assets/arrow.png" alt="arrow"></img></a>
           </header>
-          <section className="happy-section" id="happy">
+          <section className="section-content happy" id="happy">
             <h2>How was your day?</h2>
             <select
               onChange={event => setHappy(event.target.value)}
@@ -39,7 +40,7 @@ export const App = () => {
             <button><a href="#proud">Next</a></button>
           </section>
 
-          <section className="proud-section" id="proud">
+          <section className="section-content proud" id="proud">
             <h2>What are you most proud of from today?</h2>
             <input className="proud-input"
               type="text"
@@ -50,47 +51,30 @@ export const App = () => {
             <button><a href="#smile">Next</a></button>
           </section>
 
-          <section className="smile-section" id="smile">
+          <section className="section-content smile" id="smile">
             <h2>Did you make someone smile?</h2>
             <div className="radio-buttons">
-              <label>
-                <input
-                  type="radio"
-                  value="Yes"
-                  className="radio-button"
-                  onChange={event => setSmile(event.target.value)}
-                  checked={smile === "Yes"}
-                />
-                <p>Yes</p>
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  value="No"
-                  className="radio-button"
-                  onChange={event => setSmile(event.target.value)}
-                  checked={smile === "No"}
-                />
-                <p>No</p>
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  value="Maybe"
-                  className="radio-button"
-                  onChange={event => setSmile(event.target.value)}
-                  checked={smile === "Maybe"}
-                  required
-                />
-                <p>Maybe</p>
-              </label>
+              {smileOptions.map(group => (
+                <label key={group}>
+                  <input
+                    type="radio"
+                    value={group}
+                    className="radio-button"
+                    required
+                    onChange={event => setSmile(event.target.value)}
+                    checked={smile === group}
+                  />
+                  {group}
+                </label>
+              ))}
             </div>
             <button>Submit</button>
           </section>
         </form>
       ) : (
           <Summary happy={happy} proud={proud} smile={smile} />
-        )}
-    </main>
+        )
+      }
+    </main >
   )
 }
