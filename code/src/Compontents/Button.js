0eea7fcdb58d130e7c72b3367055503b0d89cfc0
Johanna Åstrand <johanna@johannas-mbp.mynet>
2020-03-18 20:17:04 +0100

    added validation on section 2 and improved styling on small screens

--- code/src/Compontents/Button.js
+++ code/src/Compontents/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Button = ({ name, section, tech, radioInput }) => {
   return (
     <button type='submit' disabled=
-      {name === '' || (name !== '' && section === 1 && tech === 'Select')}
+      {name === '' || (name !== '' && section === 1 && tech === '') || (name !== '' && section === 2 && radioInput === '')}
     >Next</button>
   )
 }
