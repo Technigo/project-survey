4cda7aca93d568c14285793aa851e3e63aef2491
Josephine <josephine@Josephines-Air.lan>
2020-03-19 19:19:40 +0100

    added id to actions component

--- code/src/Actions.js
+++ code/src/Actions.js
@@ -5,26 +5,27 @@ export const Actions = props => {
 
   return (
     <form onSubmit={event => event.preventDefault()}>
-
       Name two things you normally do in order to try to feel better?
       <br></br>
-      <label>
-        1.
+      <div className="actions">
+        <label for="actions">
+          1.
           <input
-          className="inputfield"
-          type="text"
-          onChange={(event) => setAction1(event.target.value)}
-          value={action1} />
+            className="inputfield"
+            type="text"
+            id="actions"
+            onChange={(event) => setAction1(event.target.value)}
+            value={action1} />
 2.
           <input
-          className="inputfield"
-          type="text"
-          name=""
-          required
-          onChange={(event) => setAction2(event.target.value)}
-          value={action2} />
-      </label>
-
+            className="inputfield"
+            type="text"
+            id="actions"
+            required
+            onChange={(event) => setAction2(event.target.value)}
+            value={action2} />
+        </label>
+      </div>
     </form>
   );
 };
