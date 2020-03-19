aa2a4145b8fff51924b451949225bd780c7b4d97
Frida Eriksson <Frida.Eriksson@silf.se>
2020-03-19 20:44:31 +0100

    added favicon

--- code/src/index.js
+++ code/src/index.js
@@ -2,6 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { App } from './App'
-/* import { Footer } from './components/Footer' */
 
 ReactDOM.render(<App />, document.getElementById('root'))
