8d75f9b478206ca43a71fb93ef7e7cc4323082ba
Lisa Hammarstrand <lisa@Lisas-Air.lan>
2020-03-20 22:18:47 +0100

    Cleaned up code

--- code/src/Components/Description.js
+++ code/src/Components/Description.js
@@ -2,13 +2,13 @@ import React from 'react'
 import './description.css'
 
 export const Description = () => {
-    return (
-        <div className="description">
-            <h1>Code & hike</h1>
-            <h4>We're super excited that your interested in our code & hike weekend!</h4>
-            <p>Enjoy three days of inspiring coding sessions, where you build a project of your choice. Each afternoon we'll head out on a mountain trail to clear our heads and target the best coffee spot.</p><p>There are several destinations to choose from, so pick your favourite spot</p>
-        </div >
-    )
+  return (
+    <div className="description">
+      <h1>Code & hike</h1>
+      <h4>We're super excited that your interested in our code & hike weekend!</h4>
+      <p>Enjoy three days of inspiring coding sessions, where you build a project of your choice. Each afternoon we'll head out on a mountain trail to clear our heads and target the best coffee spot.</p><p>There are several destinations to choose from, so pick your favourite spot!</p>
+    </div >
+  )
 }
 
 
