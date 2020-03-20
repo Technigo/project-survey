4cac7fe629371e2d1760d9cd1a54367600585454
Your Name <you@example.com>
2020-03-20 11:02:58 +0100

    add some styling and adding text

--- code/src/Components/FavorQuestion.js
+++ code/src/Components/FavorQuestion.js
@@ -10,28 +10,28 @@ export const FavorQuestion = props => {
       <section className='question-container'>
         <p>What can you do for your neighbor?</p>
         <label>
-          Provide groceries
           <input
           type="checkbox"
           checked={provideGroceries}
           onChange={event => setProvideGroceries(event.target.checked)}
           />
+          Provide groceries
         </label>
         <label>
-          Meal delivery services
           <input
           type="checkbox"
           checked={provideMealDeliveryServices}
           onChange={event => setProvideMealDeliveryServices(event.target.checked)}
           />
+          Meal delivery services
         </label>
         <label>
-          Provide medication
           <input
           type="checkbox"
           checked={provideMedication}
           onChange={event => setProvideMedication(event.target.checked)}
           />
+          Provide medication
         </label>
       </section>
     )
