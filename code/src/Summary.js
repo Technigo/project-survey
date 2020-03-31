aef7903e8a83d3c6b0629f7bf4f153782ac93716
Kajsa Bootin <56888076+kajsabootin@users.noreply.github.com>
2020-03-31 09:52:41 +0200

    Update Summary.js
    uppdate summary -- > Summary

--- code/src/Summary.js
+++ code/src/Summary.js
@@ -5,7 +5,7 @@ export const Summary = (props) => {
 
     return (
 
-        <section className="summary">
+        <section className="Summary">
             <p> {props.pizza} tastes best with {props.soda} and {props.sauce}sauce.</p>
         </section>
     )
