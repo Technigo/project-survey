0e33459f8c7e2512885126cdd6ecea7e4dea5936
aasa72 <asa.andersson10@comhem.se>
2020-04-11 18:11:23 +0200

    basic project

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import './app.css'
 
 export const App = () => {
   const [name, setName] = useState('');
   const [age, setAge] = useState();
   const [exercise, setExercise] = useState('');
+  const [newsletter, setNewsletter] = useState('')
   const [showSummary, setShowSummary] = useState (false);
 
   const ageGroups = ["19-35", "36-50", "51-65"];
@@ -15,24 +17,20 @@ export const App = () => {
 
   return (
     <div id="surveyPage">
-      
-    {!showSummary &&
-    <form onSubmit = {handleSubmit}>
-      <h1>We want to know how active you are!</h1>
-      <label>
-        <p>What is your name?</p>
-        
-        <input
+      {!showSummary &&
+      <form onSubmit = {handleSubmit}>
+        <h1>We want to know how active you are!</h1>
+        <label>
+          <p>What is your name?</p>
+          <input
           type = "text"
           onChange = {(event) => setName (event.target.value)}
           value = {name}
-        />
-      </label>
+          />
+        </label>
       
-
-      <p>How old are you?</p>
-      
-      {ageGroups.map (group => (
+        <p>How old are you?</p>
+        {ageGroups.map (group => (
         <label key = {group}> 
           <input 
             type = "radio"
@@ -43,10 +41,9 @@ export const App = () => {
           {group}
         </label>
       ))}
-      
+
         <label>
-        <p>How often do you exercise?</p>
-        
+          <p>How often do you exercise?</p>
           <select
             onChange = {(event) => setExercise (event.target.value)}
             value = {exercise}>
@@ -58,19 +55,25 @@ export const App = () => {
               <option value="none">I don`t exercise!</option>
           </select>
         </label>
+
+        <label>
+          <p>Sign up for a motivating newsletter</p>
+          <input
+            type = "checkbox"
+            onChange = {(event) => setNewsletter (event.target.value)}
+            value = {newsletter}
+          />
+        </label>
         <br/>
+
         <button type="submit">Submit</button>
-    </form>}
+      </form>}
 
-    {showSummary && 
-    <section>
-      <h1>Hey {name}! </h1>
-      <p>You exercise {exercise} times a week.</p>
-
-    </section>}
-    </div>
-    
-
-    
+      {showSummary && 
+      <section>
+        <h1>Hey {name}! </h1>
+        <p>You exercise {exercise} times a week.</p>
+      </section>}
+    </div> 
   )
 }
