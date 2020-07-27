644749e8bc64457b45a3c0d2cbab75a444fe09a6
Lisas Sturegard <lisasturegard@hotmail.com>
2020-07-27 13:10:07 +0200

    Udated

--- code/src/components/HoardingPlaceEntry.js
+++ code/src/components/HoardingPlaceEntry.js
@@ -9,16 +9,12 @@ export const HoardingPlaceEntry = ({place, setPlace}) => {
         onChange={event => setPlace(event.target.value)}
         value={place} 
       >
-<<<<<<< HEAD
         <option value=''>Hoarding Place:</option>
         <option value='supermarket'>Supermarket</option>
-=======
-        <option value='' disabled>Hoarding Place:</option>
-        <option value="supermarket">Supermarket</option>
->>>>>>> 3c78cef7e913298ba7ad52c0414c27e9a5707cdc
         <option value='local store'>Local Store</option>
         <option value='online shop'>Online Shopping</option>
       </select>
     </div>
   )
 }
+
