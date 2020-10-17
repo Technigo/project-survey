afd0f26b439f9886d15af3a31eb3c073c9267137
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-10-17 13:10:06 +0200

    Validation for second question working

--- code/src/components/NumberPicker.js
+++ code/src/components/NumberPicker.js
@@ -5,23 +5,32 @@ export const NumberPicker = (props) => {
   const [numberOption, setNumberOption] = useState();
   const [visible, setVisible] = useState(false);
 
+  const checkInput = () => {
+    if(numberOption) {
+      return () => setVisible(true)
+    }
+  };
+
   return (
     <>
-      <div className="number-picker">
-        <label key={numberOption}>
-          Pick your favorite number (between 1 and 5):
-          <input
-            type="number"
-            name="quantity"
-            min="1"
-            max="5"
-            value={numberOption}
-            onChange={(event) => setNumberOption(event.target.value)}
-            required />
-        </label>
-        <button onClick={() => setVisible(true)}>2 of 3</button>
-      </div>
-      {(visible === true && numberOption) && <DayPicker finalColor={props.chosenColor} chosenNumber={numberOption}/>}
+      <form onSubmit={(event) => event.preventDefault()}>
+        <div className="number-picker">
+            <label key={numberOption}>
+            Pick your favorite number (between 1 and 5):
+            <input
+                type="number"
+                name="quantity"
+                min="1"
+                max="5"
+                value={numberOption}
+                onChange={(event) => setNumberOption(event.target.value)} 
+                required />
+            </label>
+            <button onClick={checkInput()}>2 of 3</button>
+        </div>
+
+        {(visible === true) && <DayPicker finalColor={props.chosenColor} chosenNumber={numberOption}/>}
+      </form>
     </>
   );
 };
