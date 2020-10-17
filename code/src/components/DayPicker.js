66178ef04559a2e2425df92cd927b1c92728848f
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-10-17 13:15:09 +0200

    Validation for all questions working, yay!

--- code/src/components/DayPicker.js
+++ code/src/components/DayPicker.js
@@ -5,22 +5,32 @@ export const DayPicker = (props) => {
   const [dayOption, setDayOption] = useState();
   const [visible, setVisible] = useState(false);
 
+  const checkInput = () => {
+    if(dayOption) {
+      return () => setVisible(true)
+    }
+  };
+
   return (
-    <div className="day-picker">
-      <label htmlFor="weekdays">Pick a day of the week:</label>
-        <select
-          id="weekdays"
-          onChange={(event) => setDayOption(event.target.value)}
-          value={dayOption} required>
-            <option value="">Select a day:</option>
-            <option value="Monday">Monday</option>
-            <option value="Tuesday">Tuesday</option>
-            <option value="Wednesday">Wednesday</option>
-        </select>
-        <button onClick={() => setVisible(true)}>Show me the result!</button>
+      <>
+    <form onSubmit={(event) => event.preventDefault()}>
+        <div className="day-picker">
+            <label htmlFor="weekdays">Pick a day of the week:</label>
+                <select
+                id="weekdays"
+                onChange={(event) => setDayOption(event.target.value)}
+                value={dayOption} required>
+                    <option value="">Select a day:</option>
+                    <option value="Monday">Monday</option>
+                    <option value="Tuesday">Tuesday</option>
+                    <option value="Wednesday">Wednesday</option>
+                </select>
+            <button onClick={checkInput()}>Show me the result!</button>
+        </div>
 
         {(visible === true && dayOption) && <FinalText resultColor={props.finalColor} resultNumber={props.chosenNumber} resultDay={dayOption}/>}
         
-    </div>
+    </form>
+    </>
   );
 };
