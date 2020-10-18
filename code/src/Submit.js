77e6d07ff9b01bd33c89a9bd3fb9463103cb18fe
emeliesv <emeliesv@live.se>
2020-10-18 20:57:45 +0200

    added comment

--- code/src/Submit.js
+++ code/src/Submit.js
@@ -2,5 +2,6 @@ import React, { useState } from 'react';
 
 export const Submit = () => {
     const [surveySubmit, setSurveySubmit] = useState;
+    /* When this button is pressed it should hide the input form and show a summary of submissions */
     return <button>Submit Survey</button>
 }
