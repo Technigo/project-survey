af62aaf11ec7028c21a91dc744b72e9027d66663
Andrea Osmar <johndoe@email.com>
2020-10-19 17:34:17 +0200

    created dropdown

---
+++ code/src/Components/Dropdown.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+export const Dropdown = (props) => {
+
+  const { option, setOption, } = props
+
+  return (
+    <>
+      <h2>Where would you like to go?</h2>
+      <select
+        name="options"
+        id="options"
+        onChange={event => setOption(event.target.value)}
+        value={option}
+      >
+        <option value="">Select an option</option>
+        <option value="Africa">Africa</option>
+        <option value="Antartica">Antartica</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="North America">North America</option>
+        <option value="Oceania">Oceania</option>
+        <option value="South America">South America</option>
+      </select>
+    
+    </>
+  )
+}
