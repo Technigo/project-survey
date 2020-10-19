d7a5c7672e768c29afd59fc899868064921fadad
Andrea Osmar <johndoe@email.com>
2020-10-19 17:20:04 +0200

    created welcome component

---
+++ code/src/Components/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Welcome = (props) => {
+
+  return (
+    <>
+      <h1>Welcome to our place </h1>
+      <p>Banana Pancake Banana Pancake Banana Pancake Banana Pancake</p>
+    </>
+  )
+}
