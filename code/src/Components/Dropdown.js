556c366fc3510cbac89c9143a57074d9b3401a61
Gabbi-89 <gabriellabolin@hotmail.com>
2020-10-19 13:16:09 +0200

    Option values fixed

--- code/src/Components/Dropdown.js
+++ code/src/Components/Dropdown.js
@@ -12,9 +12,9 @@ export const Dropdown = ({ yearsOfExperience, setYearsOfExperience }) => {
     >
       <option value="">4. Years of experience within Tech?</option>
       <option value="0-1 year">0 - 1 year</option>
-      <option value="0-1 year">2 - 5 years</option>
-      <option value="0-1 year">5 - 10 years</option>
-      <option value="0-1 year">10+ years</option>
+      <option value="2-5 years">2 - 5 years</option>
+      <option value="10 years">5 - 10 years</option>
+      <option value="10+ years">10+ years</option>
     </select>
   );
 };
