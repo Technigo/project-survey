ee439e9d60afa55c7353bd6671bd7a4fc6ba4e3b
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2020-10-19 15:56:29 +0200

    startet upp project added from, inputtype and some styling in styles.css

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, {useState} from "react";
+import "./styles.css";
+
 
 export const App = () => {
+
+const [name, setName] = useState ("");
+const [password, setPassword] = useState ("");
+const [age, setAge] = useState ("");
+
+const handleSubmit = event => {
+  event.preventDefault ();
+
+};
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="App">
+      <h1>Hey hey!</h1>
+      <form onSubmit={handleSubmit}>
+        <lable>
+          What is your name?
+          <input
+          type="text"
+          value={name}
+          OnChange={event => setName (event.target.value)}
+          required
+          />
+        </lable>
+        <lable>
+        What is your password?
+          <input
+          type="password"
+          value={password}
+          OnChange={event => setPassword (event.target.value)}
+          required
+          />
+        </lable>
+        <lable>
+          What is your age?
+          <input
+          type="age"
+          value={age}
+          OnChange={event => setAge (event.target.value)}
+          required
+          />
+        </lable>
+        <button type="Submit">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
