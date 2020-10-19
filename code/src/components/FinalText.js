95062a7ef8d61d39802141d702125b74d1a706ee
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-10-19 15:41:11 +0200

    Structure done and added a Restart button at the end, ready to start styling

--- code/src/components/FinalText.js
+++ code/src/components/FinalText.js
@@ -1,8 +1,15 @@
 import React from "react";
 import 'components-css/finaltext.css';
 
-export const FinalText = (props) => {
-return <p className="final-text" id="final-text">On your dream vacation you'll find yourself chillin' at the {props.resultDestination} with {props.resultNumber} of your best friends, drinking cocktails during the {props.resultDay}.</p>
-};
+const restart = () => {
+    document.location.href = "";
+  };
 
-/* Add refresh button to start survey all over */
+export const FinalText = (props) => {
+return (
+    <section className="final-text" >
+        <p id="final-text">On your dream vacation you'll find yourself chillin' at the {props.resultDestination} with {props.resultNumber} of your best friends, drinking cocktails during the {props.resultDay}.</p>
+        <button onClick={restart}>Refresh Page</button>
+    </section>
+    )
+};
