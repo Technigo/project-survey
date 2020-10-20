5c14b1b0518c67f2736a3fe0e24d47ba4a9b3665
Moa <63230455+mhammarback@users.noreply.github.com>
2020-10-20 10:33:55 +0200

    password deleted in survey

--- code/src/Summary.js
+++ code/src/Summary.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Summary = ({ name , setName ,  password , setPassword, ageGroup, setAgeGroup, location, setLocation }) => {
+export const Summary = ({ name , setName , ageGroup, setAgeGroup, location, setLocation }) => {
     return ( 
      <section className ="summary-wrapper"> 
     <p> You want to name your cat: {name} </p>   
