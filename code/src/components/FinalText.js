80340c2aaf598d1950c25cabee34f79ee14df5bb
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-10-20 13:30:52 +0200

    Styling of final text done, start with testing

--- code/src/components/FinalText.js
+++ code/src/components/FinalText.js
@@ -7,9 +7,13 @@ const restart = () => {
 
 export const FinalText = (props) => {
 return (
-    <section className="final-text" >
-        <p id="final-text">On your dream vacation you'll find yourself chillin' at the {props.resultDestination} with {props.resultNumber} of your best friends, drinking cocktails during the {props.resultDay}.</p>
-        <button onClick={restart}>Refresh Page</button>
+    <section className="final-text">
+        <div className="final-result-container" id="final-text">
+            <img className="final-img" src="assets/final-img.png" alt=""/>
+            <p className="dream-text">On your dream vacation </p>
+            <p className="result-text">...you'll find yourself chillin' at the {props.resultDestination} with {props.resultNumber} of your best friends, drinking cocktails during the {props.resultDay}.</p>
+            <button className="restart-button" onClick={restart}>Refresh Page</button>
+        </div>
     </section>
     )
 };
