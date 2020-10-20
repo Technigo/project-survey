9106ba796150717d06d858bdb1e4b26ff7ca246e
Ingela Löfgren <ingela.lofgren@gmail.com>
2020-10-20 10:12:52 +0200

    Third question added

--- code/src/App.js
+++ code/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Header } from 'components/Header'
 import { FirstQuestion } from './components/FirstQuestion'
 import { SecondQuestion } from './components/SecondQuestion'
+import { ThirdQuestion } from './components/ThirdQuestion'
 
 
 export const App = () => {
@@ -10,6 +11,7 @@ export const App = () => {
       <Header />
       <FirstQuestion />
       <SecondQuestion />
+      <ThirdQuestion />
     </div>
   );
 };
---
+++ code/src/components/ThirdQuestion.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useState } from 'react'
+
+export const ThirdQuestion = () => {
+    const [text, setText] = useState()
+
+
+return (
+    <form>
+        <h2>What was the main reason you wanted to to start at the Technigo bootcamp and become a frontend developer?</h2>
+        <input
+        type='text'
+        onChange={event => setText(event.target.value)}
+        value={text}
+        />
+
+    </form>
+    );
+
+}; 
