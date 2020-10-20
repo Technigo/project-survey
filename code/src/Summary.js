63d394f6f5acc5624ece8ed265972459e7130010
Moa <63230455+mhammarback@users.noreply.github.com>
2020-10-20 12:15:20 +0200

    restart button added in Summary

--- code/src/Summary.js
+++ code/src/Summary.js
@@ -1,11 +1,18 @@
 import React from "react";
 
+
+const restart = () => {
+    document.location.href = "";
+  };
+
 export const Summary = ({ name , setName , ageGroup, setAgeGroup, location, setLocation }) => {
     return ( 
      <section className ="summary-wrapper"> 
     <p> You want to name your cat: {name} </p>   
     <p> You preffer a cat that is a {ageGroup}  </p>
     <p> You want a cat with {location} fur! </p>
+
+    <button onClick={restart}>Restart Page</button>
     </section> 
     )
 }
