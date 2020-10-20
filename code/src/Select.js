61ea9f544dad3c3da8cdbc2d2c6f219b74d74144
Moa <63230455+mhammarback@users.noreply.github.com>
2020-10-20 12:38:47 +0200

    test 2 to increase Lighhouse test

--- code/src/Select.js
+++ code/src/Select.js
@@ -5,19 +5,18 @@ import React from 'react';
 const Select = ( {fur, setFur} ) => { 
     
     return ( 
-      <section className="fur-wrapper">
-    
-      
-        <label id="fur" name="fur"></label>
+      <section className="fur-wrapper">      
+        <label for="fur">
         <select name="Fur" 
             onChange = {event => setFur(event.target.value)}
             value = {fur} 
             required >
-              <option value = "" > Select Fur type:</option>
+              <option  value = "" > Select Fur type:</option>
               <option value = "short" > Short</option>
               <option value = "long" > Long</option>
               <option value = "naked" > Naked </option>
         </select>
+        </label>
       </section>
       
     )
