fa25c1b374058bcc94051d50ca445e5c6e9edd6d
Claudia <Claudia@Apples-Air.lan>
2020-10-20 23:39:37 +0200

    Added two forms

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react';
+import "./index.css";
 
 export const App = () => {
+   const [name, setName] = useState("");
+   const [password, setPassword] = useState("");
+
+   const handleSubmit = event => {
+     event.preventDefault();
+   };
+
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="App">
+      <h1>Happy Tuesday!</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          What's your name?
+          <input
+            type="text"
+            value={name}
+            onChange={event => setName(event.target.value)}
+            required
+            />
+        </label>
+        <label>
+          What's your Password? 
+          <input
+            type="password"
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+            required 
+          />
+        </label>
+      </form>
     </div>
   )
 }
