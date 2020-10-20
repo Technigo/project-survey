f84358e7b25bc97a928c8e026867bae1ddd522cd
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-10-20 18:29:17 +0200

    Started with accessibility testing: tabbing on Chrome

--- code/src/components/FinalText.js
+++ code/src/components/FinalText.js
@@ -10,8 +10,8 @@ return (
     <section className="final-text">
         <div className="final-result-container" id="final-text">
             <img className="final-img" src="assets/final-img.png" alt=""/>
-            <p className="dream-text">On your dream vacation </p>
-            <p className="result-text">...you'll find yourself chillin' at the {props.resultDestination} with {props.resultNumber} of your best friends, drinking cocktails during the {props.resultDay}.</p>
+            <p className="dream-text" tabIndex="0">On your dream vacation </p>
+            <p className="result-text" tabIndex="0">...you'll find yourself chillin' at the {props.resultDestination} with {props.resultNumber} of your best friends, drinking cocktails during the {props.resultDay}.</p>
             <button className="restart-button" onClick={restart}>Start again!</button>
         </div>
     </section>
