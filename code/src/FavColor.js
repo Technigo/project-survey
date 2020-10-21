5e97183fb78f4b2d544dd93764b1d16e2e408ba5
idsintehittapa <jblom15@gmail.com>
2020-10-21 19:35:50 +0200

    Added styling

---
+++ code/src/FavColor.js
@@ -0,0 +1,8 @@
+///import React from 'react'
+
+//const FavColor = props => {
+    //const { setFavColor, favcolor } = props
+    //return (
+    //)
+
+///export default FavColor 
--- code/src/Summary.js
+++ code/src/Summary.js
@@ -3,13 +3,15 @@ import React from "react";
 export const Summary = props => {
     const {text, teletubbies, sailorMoon, turtles} = props
     return (
-        <section>
-            <h1>This survey is under construction. Sadly, we cannot reveal your ... </h1>
-            <h2>But here are is a summary of the answers you gave!</h2>
-            <h3>Your text input is {text}</h3>
-            <h3>The select input is {teletubbies}</h3>
-            <h3>And, the sailor input is {sailorMoon}</h3>
-            <h3>And, the turtle input is {turtles}</h3>
+        <section className="summary-wrapper">
+            <h1>Error - sadly we cannot reveal your favourite color <span role="img" aria-label="disappointed face">&#128542;</span></h1>
+            <h2>Here are is a summary of the answers you gave!</h2>
+            <h3>You chose the {text} power ranger!</h3>
+            <h3>And {teletubbies}</h3>
+            <h3>And, {sailorMoon}, as the coolest Sailor Moon character</h3>
+            <h3>And, lastly {turtles}</h3>
+            <h3>Pretty cool though!</h3>
+            <h4>Ps. the most common favourite color is blue - so yours is probablu too <span role="img" aria-label="Face with stuck-out tongue and winking eye"></span>&#128540;</h4>
         </section>
     )
 }
