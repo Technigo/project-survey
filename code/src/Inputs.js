69cc5b15ad37c927a813bbc7eb69a0c69500f015
idsintehittapa <jblom15@gmail.com>
2020-10-21 10:38:41 +0200

    refacotring code, made button work and show summary of survey

--- code/src/Inputs.js
+++ code/src/Inputs.js
@@ -2,25 +2,43 @@ import React, { useState } from 'react'
 import Select from 'Select'
 import Text from './Text'
 import Radio from './Radio'
+import Summary from './Summary'
 
 
 export const Input = () => {
     const [name, setName] = useState('') // text 
     const [question, setQuestion] = useState('') // select
-    const [questionGroup, setQuestionGroup] = useState() // radiobuttons
-    return (
-        <section>
-        <form>
-            <Text 
-            id="test"
-            question="here is my question"
-            setText={setName}
-            value={name}
-            />
-            < Select setQuestion={setQuestion} question={question}/>
-            < Radio setQuestionGroup={setQuestionGroup} questionGroup={questionGroup} />
-        </form>
-        
-        </section>
-    )
-}
+    const [questionGroup, setQuestionGroup] = useState("") // radiobuttons
+    const [showSummary, setShowSummary] = useState(false)
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        setShowSummary(true)
+    }
+
+        return (
+            <main>
+                <h1> Title for my form</h1>
+                {!showSummary ? (
+                    <form onSubmit={handleSubmit}>
+                        <Text
+                            id="test"
+                            question="here is my question"
+                            setText={setName}
+                            value={name}
+                        />
+                        < Select setQuestion={setQuestion} question={question} />
+                        < Radio setQuestionGroup={setQuestionGroup} questionGroup={questionGroup} />
+                        <button disabled={!name || !question || !questionGroup} type="submit"> I'M DONE </button>
+                    </form>
+                ):
+                showSummary && (
+                    <Summary
+                        text={name}
+                        select={question}
+                        radio={questionGroup}
+                    />
+                )}
+            </main>
+        )
+    }
--- code/src/Summary.js
+++ code/src/Summary.js
@@ -1,15 +1,14 @@
 import React from "react";
 
 export const Summary = props => {
-    const {text, select, radio}
-    
+    const {text, select, radio} = props
     return (
         <section>
             <h1>This survey is under construction. Sadly, we cannot reveal your ... </h1>
             <h2>But here are is a summary of the answers you gave!</h2>
-            <h3>Your lala is {text}</h3>
-            <h3>The lala is {select}</h3>
-            <h3>And, your lala is {radio}</h3>
+            <h3>Your text input is {text}</h3>
+            <h3>The select input is {select}</h3>
+            <h3>And, the radio input is {radio}</h3>
         </section>
     )
 }
