85e82666a6c1e06ed443f5cb9b9d6c619d8dfb5e
caroline birgersson <caroline@birgerochblom.se>
2020-10-21 19:02:32 +0200

    finished blue level

--- code/src/App.js
+++ code/src/App.js
@@ -8,36 +8,33 @@ import { Header } from "./components/Header.js"
 export const App = () => {
   const [reflection, setReflection] = useState(""); // the thing we have in the paranthesis is the initial state. If we console log status it's gonna show the thing in the paranthesis. If it's gonna show strings you use "" if it's gonna show bolean you write true or false etc, if i's gonna show an array you write []. 
   const [reason, setReason] = useState("");
-  const [weather, setWeather] = useState("")
-  const [culture, setCulture] = useState ("")
-  const [showSummary, setShowSummary] = useState (false)
-
-  const reflectionOptions = ["Splendid",  "Good", "Ok", "Bad"]
+  const [weather, setWeather] = useState("");
+  const [culture, setCulture] = useState ("");
+  const [showSummary, setShowSummary] = useState (false);
 
   const handleSubmit = event => {
     event.preventDefault()
     setShowSummary(true)
   }
 
-
   return (
-    <main>
-      {!showSummary ? (
+    <main> 
+      {!showSummary && (
         <form onSubmit={handleSubmit}>
           <Header reflection= {reflection}/>
           <section className="reflection-section" id="reflection">
-            <h2 className="question">How is your day?</h2>
-            <img className="arrow" src="../public/assets/arrow.png"></img>
+            <h2>How is your day?</h2>
+            <img className="arrow" src="./assets/arrow.png"></img>
             <select
               value={reflection}
-              onChange ={event => setReflection(event.target.value)}
+              onChange ={event => setReflection (event.target.value)}
               required
             >
               <option value=""></option>
-              <option value="splendid">Splendid</option>
-              <option value="good">Good</option>
-              <option value="ok">OK</option>
-              <option value="bad">Bad</option>
+              <option value="a splendid day">Splendid</option>
+              <option value="a good day">Good</option>
+              <option value="an ok day">OK</option>
+              <option value="a bad day">Bad</option>
             </select>
             <button><a href="#reason">Next</a></button>
           </section>
@@ -56,12 +53,12 @@ export const App = () => {
 
           <section className="weather-section" id="weather">
             <h2>Today's weather</h2>
-            <div class="option-container">
+            <div className="option-container">
             <label className="option">
               Sunny
               <input 
                 type="radio"
-                value={weather}
+                value="sunny"
                 onChange={event => setWeather (event.target.value)}
               />
             </label>
@@ -69,7 +66,7 @@ export const App = () => {
               Cloudy
               <input
                 type="radio"
-                value={weather}
+                value="cloudy"
                 onChange={event => setWeather (event.target.value)}
               />
             </label>
@@ -77,7 +74,7 @@ export const App = () => {
               Rainy
               <input
                 type="radio"
-                value={weather}
+                value="rainy"
                 onChange={event => setWeather (event.target.value)}
               />
             </label>
@@ -85,7 +82,7 @@ export const App = () => {
               Windy
               <input
                 type="radio"
-                value={weather}
+                value="windy"
                 onChange={event => setWeather (event.target.value)}
               />
             </label>
@@ -95,21 +92,22 @@ export const App = () => {
 
           <section className="culture-section" id="culture">
             <label className="question">
-            Did you read or watch anything you want to share?
+            Have you read or watched anything?
               <input 
                 type="message"
                 value={culture}
                 onChange={event => setCulture (event.target.value)}
+                required
               />
             </label>
             <button type="submit" className="submit">Done for today!</button>
           </section>
       </form>
-      ) : (
-      <Summary reflection={reflection}/>
-        )
-      }
+      ) } 
+      
+      {showSummary && <Summary reflection={reflection} weather={weather}/>}
+    
     </main>
-  )
-}
 
+    );
+  };
--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -1,10 +1,12 @@
 import React from "react"
 import "./summary.css"
 
-export const Summary = ({reflection}) => { // {reflection} could also be replaced with props (without curly) but then you should write {props.reflection} instead.
+export const Summary = ({reflection, weather}) => { // {reflection} could also be replaced with props (without curly) but then you should write {props.reflection} instead.
   return (
-    <section>
-      <h1>Today was {reflection} for you. </h1>
+    <section className="summary-section">
+      <h1 className="summary-heading">Notes of the day</h1>
+      <p>Today was {reflection} for you. Maybe the {weather} weather had an impact on your mood. Hope tomorrow is even better!</p>
+      <button><a href="">Create new notes</a></button>
     </section>
   )
 }
