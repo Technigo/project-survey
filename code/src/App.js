3bc284e31acf9176376d181a5e0cd135d23543ae
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-22 09:27:12 +0200

    Updated App.js componenent file: 1. Removed the import for Summary component as this will be exported into the Form.js. 2. Edited the content inside the main tag so we only have the header and form component tag as I decided it was good to start splitting up different parts of my form into individual componenets.

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Form } from './Form.js'
-import { Summary } from './Summary.js'
 
 export const App = () => {
 
@@ -10,17 +9,11 @@ export const App = () => {
 
   return (
     <main>
-      <div>
+      <header>
         <h1>CRAFT BEER SURVEY</h1>
-        <p>Thanks for taking the time to take our craft been survey</p>
-      </div>
-      <div>        
-          <Form />
-      </div>
-      <button type="submit">SUBMIT</button> 
-      <div>
-        <Summary />      
-      </div>     
+      </header>
+      
+      <Form />      
     </main>
   );
 };
