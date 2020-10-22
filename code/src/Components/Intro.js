3ed9554a737a499abfecc3ad7ce31ff58c949f72
Andrea Osmar <johndoe@email.com>
2020-10-22 17:37:24 +0200

    changes in code

--- code/src/Components/Intro.js
+++ code/src/Components/Intro.js
@@ -7,9 +7,8 @@ export const Intro = (props) => {
   return (
     <>
       <h1 tabIndex="0">Welcome to Adventure World </h1>
-      <p tabIndex="0">Adventure world is a unique travel agency who customize unforgettable travels for couples. 
-      Complete our form and we will contact you with an offer for a vacation you cannot resist!</p>
-      
+      <p tabIndex="0">We are a unique travel agency who customize unforgettable experiences for couples. 
+      Complete our form and we will contact you with an offer for a adventure you can't resist!</p>
     </>
   )
 }
