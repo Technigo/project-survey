028dd8d33dcfe2fd79b00daba01d566356ea0a16
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-22 09:34:18 +0200

    Updated Form.js file: 1. Exported Summary.js, Age.js, Type.js and Price.js. 2. Created a handleSubmit function with a preventDefault that'll stop the form from re-rendering when the form is submitted. And setShowSummary to true. This means when the form is submitted the setShorSummary section will be visible with the input data from the user. 3. Added the useState() hook for each of my form components and the showSummary section of the page. 4. Created a condition using ternary operators. If showSummary is not true e.g. the submit button hasn't been clicked then the form will be visible, else the Summary section will be rendered to the page. 5. Inside of this we have the form tag with the different form components inside, a submit button inside the form and the Summary tag with the different props for each of the form components.

--- code/src/Form.js
+++ code/src/Form.js
@@ -1,18 +1,41 @@
 import React, {useState} from 'react'
+import { Summary } from './Summary'
 import { Age } from './Form components/Age.js'
-// import { Type } from './Form components/Type.js'
-
+import { Type } from './Form components/Type.js'
+import { Price } from './Form components/Price.js'
 
 export const Form = () => {
+  /* handleSubmit is using the preventDefault method to stop the 
+  page from reloading when the form is submitted when we click the button.
+  It's also setting the setShowSummary to true which means that the form will
+  be hidden and the summary will be show*/
+  const handleSubmit = event => {
+    event.preventDefault();
+    setShowSummary(true);
+  };
+
   const [age, setAge] = useState();
-  const [type, setType] = useState(false);
+  const [type, setType] = useState([]);
   const [price, setPrice] = useState();
   const [special, setSpecial] = useState();
+  const [showSummary, setShowSummary] = useState(false);
 
     return (
-      <form>
-          <Age age={age} setage={setAge}/>
-          {/* <Type /> */}
-      </form>      
+      /* Using ternary operators below to show and hide the form and summary - if (?) else (:) */
+      <>
+        {!showSummary ? (
+          <div> 
+            <form onSubmit={handleSubmit}>
+              <Age age={age} setage={setAge}/>
+              <Type type={type} setType={setType} />
+              <Price price={price} setPrice={setPrice} />
+
+              <button type="submit">SUBMIT</button> 
+            </form>
+          </div>
+        ) : (        
+          <Summary age={age} type={type} price={price} />
+        )}        
+     </>    
     );
 };
