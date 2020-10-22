db5c7970c2a5059ad0e2576b7ded6b61cdf739c7
LillanEriksson <nailsbylillan@hotmail.com>
2020-10-22 12:18:14 +0100

    Added label to range-slider

--- code/src/Question5.js
+++ code/src/Question5.js
@@ -6,15 +6,16 @@ export const Question5 = ({ userAnswer5, onAnswerChange }) => {
 			<label htmlFor="question5">
 				<h2>Question number 5</h2>
 				<p>How many episodes were made of the iconic tv-series X-files?</p>
+
+				<input
+					type="range"
+					name="question5"
+					min="210"
+					max="230"
+					onChange={(e) => onAnswerChange(e.target.value)}
+					value={userAnswer5}
+				/>
 			</label>
-			<input
-				type="range"
-				name="question5"
-				min="210"
-				max="230"
-				onChange={(e) => onAnswerChange(e.target.value)}
-				value={userAnswer5}
-			/>
 			<p>Count: {userAnswer5} </p>
 		</div>
 	);
