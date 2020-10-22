8ac91f335256c1c67f50ff93af9f16f9176311d8
Ingela Löfgren <ingela.lofgren@gmail.com>
2020-10-22 15:43:46 +0200

    Corrected a typo in header

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <header className='survey-header'>
             <h1 className='main-header' tabIndex="0">Survey Technigo students</h1>
-            <p className='info-text' tabIndex="0">We would like to know more about your background and why you want to become a frontend developer! Can you spare 3 minutes?</p>
+            <p className='info-text' tabIndex="0">We would like to know more about your background and why you want to become a frontend developer! Can you spare 2 minutes?</p>
         </header>   
     );  
 };
