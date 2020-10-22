37b6306d4875a5ceeabf4e1365a283ae5c5d0a6a
Therese Lindén <theresemlinden@gmail.com>
2020-10-22 16:02:35 +0200

    other question depending on selected value in dropdown

--- code/src/components/Form.js
+++ code/src/components/Form.js
@@ -14,19 +14,25 @@ export const Form = () => {
 	const [question, setQuestion] = useState(-1); //show first "question"
 	const [name, setName] = useState(''); //text
 	const [age, setAge] = useState(false); //radio
+	const [months, setMonths] = useState([]); //checkbox
 	const [season, setSeason] = useState(''); //drop-down
 	const [value, setValue] = useState(5); //range
 	const [email, setEmail] = useState(''); //email
 	const [submit, setSubmit] = useState(false); //since we don't want to show result before submitted.
-	//const [summary, setSummary] = useState(0); //show first "question"
 
 	const nextQuestion = () => setQuestion(question + 1); //to get next question in line (add +1 later)
 	const previousQuestion = () => setQuestion(question - 1);
-	//const nextSummary = () => setSummary(summary + 1);
+
 	const handleSubmit = event => {
 		event.preventDefault();
 		setSubmit(true); //show summary on submit
 		console.log('form submitted');
+	};
+
+	const handleMonthsChange = monthValue => {
+		months.includes(monthValue)
+			? setMonths(months.filter(item => item !== monthValue))
+			: setMonths([...months, monthValue]);
 	};
 
 	//array for radio btn
@@ -38,6 +44,52 @@ export const Form = () => {
 		'51-60 years',
 		'60 + years',
 	];
+
+	//array for checkbox
+	//const monthsArray = ['January', 'February', 'March', 'April'];
+	// Karro exempel
+	const seasonArray = [
+		{
+			season: 'Winter',
+			months: ['December', 'January', 'February'],
+		},
+		{
+			season: 'Spring',
+			months: ['March', 'April', 'May'],
+		},
+		{
+			season: 'Summer',
+			months: ['June', 'July', 'August'],
+		},
+		{
+			season: 'Fall',
+			months: ['September', 'October', 'November'],
+		},
+	];
+	console.log(seasonArray);
+	console.log(seasonArray.findIndex(item => item.season === 'Spring'));
+	console.log(
+		seasonArray[seasonArray.findIndex(item => item.season === 'Summer')].months
+	);
+	// {
+	// 	id: 'spring',
+	// 	month1: 'March',
+	// 	month2: 'April',
+	// 	month3: 'May',
+	// },
+	// {
+	// 	id: 'summer',
+	// 	month1: 'June',
+	// 	month2: 'July',
+	// 	month3: 'August',
+	// },
+	// {
+	// 	id: 'fall',
+	// 	month1: 'September',
+	// 	month2: 'October',
+	// 	month3: 'November',
+	// },
+	// ];
 
 	return (
 		<section className="form-wrapper">
@@ -77,7 +129,6 @@ export const Form = () => {
 						<article className="question-container">
 							<InputSelect
 								question="What's your favourit season?"
-								// id="season"
 								setSeason={setSeason}
 								value={season}
 							/>
@@ -92,8 +143,120 @@ export const Form = () => {
 							</div>
 						</article>
 					)}
+					{question === 2 && season === 'Winter' && (
+						<>
+							<InputCheckbox
+								question="Select which ones you like"
+								selectedMonths={months}
+								onMonthsChange={handleMonthsChange}
+								array={
+									seasonArray[
+										seasonArray.findIndex(item => item.season === 'Winter')
+									].months
+								}
+							/>
+							<div className="navigation">
+								<Button button="button" click={previousQuestion} text="Back" />
+								<Button
+									button="button"
+									disabled={!season}
+									click={nextQuestion}
+									text="Next"
+								/>
+							</div>
+						</>
+					)}
+					{question === 2 && season === 'Spring' && (
+						<>
+							<InputCheckbox
+								question="Select which ones you like"
+								selectedMonths={months}
+								onMonthsChange={handleMonthsChange}
+								array={
+									seasonArray[
+										seasonArray.findIndex(item => item.season === 'Spring')
+									].months
+								}
+							/>
+							<div className="navigation">
+								<Button button="button" click={previousQuestion} text="Back" />
+								<Button
+									button="button"
+									disabled={!season}
+									click={nextQuestion}
+									text="Next"
+								/>
+							</div>
+						</>
+					)}
+					{question === 2 && season === 'Summer' && (
+						<>
+							<InputCheckbox
+								question="Select which ones you like"
+								selectedMonths={months}
+								onMonthsChange={handleMonthsChange}
+								array={
+									seasonArray[
+										seasonArray.findIndex(item => item.season === 'Summer')
+									].months
+								}
+							/>
+							<div className="navigation">
+								<Button button="button" click={previousQuestion} text="Back" />
+								<Button
+									button="button"
+									disabled={!season}
+									click={nextQuestion}
+									text="Next"
+								/>
+							</div>
+						</>
+					)}
+					{question === 2 && season === 'Fall' && (
+						<>
+							<InputCheckbox
+								question="Select which ones you like"
+								selectedMonths={months}
+								onMonthsChange={handleMonthsChange}
+								array={
+									seasonArray[
+										seasonArray.findIndex(item => item.season === 'Fall')
+									].months
+								}
+							/>
+							<div className="navigation">
+								<Button button="button" click={previousQuestion} text="Back" />
+								<Button
+									button="button"
+									disabled={!season}
+									click={nextQuestion}
+									text="Next"
+								/>
+							</div>
+						</>
+					)}
 
-					{question === 2 && (
+					{/* {question === 2 && (
+						<article className="question-container">
+							<InputCheckbox
+								question="Select months"
+								selectedMonths={months}
+								onMonthsChange={handleMonthsChange}
+								array={monthsArray}
+							/>
+							<div className="navigation">
+								<Button button="button" click={previousQuestion} text="Back" />
+								<Button
+									button="button"
+									disabled={!season}
+									click={nextQuestion}
+									text="Next"
+								/>
+							</div>
+						</article>
+					)} */}
+
+					{question === 3 && (
 						<article className="question-container">
 							<InputRange
 								// id="range"
@@ -113,7 +276,7 @@ export const Form = () => {
 						</article>
 					)}
 
-					{question === 3 && (
+					{question === 4 && (
 						<article className="question-container">
 							<InputRadio
 								question={`How many ${season} have you experienced?`}
@@ -153,7 +316,7 @@ export const Form = () => {
 							</div>
 						</article>
 					)} */}
-					{question === 4 && (
+					{question === 5 && (
 						<article className="question-container">
 							<Summary
 								name={name}
@@ -185,7 +348,7 @@ export const Form = () => {
 							<ProgressBar
 								// progress={question}
 								progress={`${question}`}
-								maxProgress="4"
+								maxProgress="5"
 								minProgress="0"
 								// progressText={`${question}/5`}
 							/>
--- code/src/components/InputCheckbox.js
+++ code/src/components/InputCheckbox.js
@@ -1,30 +1,52 @@
 import React from 'react';
 
-export const InputCheckbox = ({ question, selectedMonths, onMonthsChange }) => {
+export const InputCheckbox = ({
+	question,
+	array,
+	selectedMonths,
+	onMonthsChange,
+}) => {
 	return (
-		<div>
+		<>
 			<h2>{question}</h2>
-			<label htmlFor="summerMonths">Summer</label>
-			<input
-				id="summerMonths"
-				type="checkbox"
-				checked={selectedMonths.includes('June')}
-				onChange={() => onMonthsChange('June')}
-			/>
-			<label htmlFor="summerMonths">Summer</label>
-			<input
-				id="summerMonths"
-				type="checkbox"
-				checked={selectedMonths.includes('July')}
-				onChange={() => onMonthsChange('July')}
-			/>
-			<label htmlFor="summerMonths">Summer</label>
-			<input
-				id="summerMonths"
-				type="checkbox"
-				checked={selectedMonths.includes('August')}
-				onChange={() => onMonthsChange('August')}
-			/>
-		</div>
+			{array.map(item => (
+				<label htmlFor={item}>
+					{item}
+					<input
+						id={item}
+						type="checkbox"
+						value={item}
+						checked={selectedMonths.includes()}
+						onChange={event => onMonthsChange(event.target.value)}
+					/>
+				</label>
+			))}
+		</>
 	);
 };
+
+{
+	/* <div>
+	<label htmlFor="colorRed">Red</label>
+	<input
+		id="colorRed"
+		type="checkbox"
+		checked={userColors.includes('red')}
+		onChange={() => onColorsChange('red')}
+	/>
+	<label htmlFor="colorGreen">Green</label>
+	<input
+		id="colorGreen"
+		type="checkbox"
+		checked={userColors.includes('green')}
+		onChange={() => onColorsChange('green')}
+	/>
+	<label htmlFor="colorBlue">Blue</label>
+	<input
+		id="colorBlue"
+		type="checkbox"
+		checked={userColors.includes('blue')}
+		onChange={() => onColorsChange('blue')}
+	/>
+</div>; */
+}
