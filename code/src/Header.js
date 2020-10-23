faf25a3738f9d752bd7a1792d1d167d113107740
Moa Blomkvist <moa@birgerochblom.se>
2020-10-24 01:28:07 +0200

    final style

--- code/src/Header.js
+++ code/src/Header.js
@@ -4,6 +4,7 @@ export const Header = () => {
   return (
     <header>
       <h1>Joakim 40 år</h1>
+      <img src="../celebrate.svg" alt="celebrate" className="celebrate"></img> 
     </header>
   )
 }
--- code/src/Invitation.js
+++ code/src/Invitation.js
@@ -7,7 +7,6 @@ export const Invitation = ({date, time, place}) => {
         <p>Det är inte så ofta vi har riktiga fester längre, därför känns det dumt att inte fira när tillfälle ges.
           Nu händer det, Joakim fyller 40 år och vi är lediga på nyår. Dessutom har vi barnvakt till ungarna och 
           en stuga med sängplatser, bastu och nybyggt orangeri att fylla.</p> 
-          <img src="../celebrate.svg" alt="celebrate" className="celebrate"></img> 
           <ul>
             <li>31 oktober</li>
             <li>Kl. 15.00</li>
