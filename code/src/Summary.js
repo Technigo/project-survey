26f3e63db5de64a42bfa8144bbc3597fefcf4024
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-23 22:19:49 +0200

    Updated Summary.js: 1. Updated the answer template.

--- code/src/Summary.js
+++ code/src/Summary.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Summary = ({age, type, price, special}) => {
+export const Summary = ({age, type, price, special, name}) => {
 
     /* Created a re-useable function which takes an argument. This argument is iterated through by way of the for loop. The iteration is then used in the if else statement to be able append either an "&"" or "," when the length of the array is compared to the index number of the array. This function is then called when we pass the type array in the return statement further down in the jsx area*/
     const arrayToString = (array) => {
@@ -20,24 +20,27 @@ export const Summary = ({age, type, price, special}) => {
 
     let sentance ="";
     if (type.length === 1) {
-        sentance = "Your favourite craft beer is "
+        sentance = "Your favourite Craft Beer Co. product is "
     } else {
-        sentance = "Your favourite craft beers are "
+        sentance = "Your favourite Craft Beer Co. products are "
     }   
 
     return (
         <>       
-            <h3>Your craft beer profile:</h3>
+            <h3>Thanks for taking part in our survey! You submitted:</h3>
                 <div>
-                    <h3>Age:</h3>
+                    <h3>Your name: </h3>
+                    <p>{name}</p>
+                    <h3>Age range: </h3>
                     <p>{age}</p>
                     <h3>{sentance}</h3>
                     <p>{arrayToString(type)}</p>            
-                    <h3>Price range</h3>
+                    <h3>Your desired price range is: </h3>
                     <p>{price}</p>
-                    <h3>Craft beer is No.1 to you because:</h3>
+                    <h3>Craft Beer Co. is No.1 to you because: </h3>
                     <p>{special}</p>
-                </div>             
+                </div> 
+
         </>
     );
 };
