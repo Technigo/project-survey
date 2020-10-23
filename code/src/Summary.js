451d7aaaf4f26ec817863cdf69850528c801755d
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-23 08:34:09 +0200

    Updated the Summary.js: 1. Created the Summary component. 2. Created a re-useable function to help inserting separators e.g. comma or ampersand if using an array. Used this for the type variable which is an array. 3. Created a if statement which allows for a certain sentance to be printed depending on how many craft beers the user inputs using the checkboxes. 4. Created the return for the JSX.

--- code/src/Summary.js
+++ code/src/Summary.js
@@ -1,14 +1,43 @@
-import React, {useState} from 'react'
+import React from 'react'
 
-export const Summary = ({age, type, price}) => {
+export const Summary = ({age, type, price, special}) => {
+
+    /* Created a re-useable function which takes an argument. This argument is iterated through by way of the for loop. The iteration is then used in the if else statement to be able append either an "&"" or "," when the length of the array is compared to the index number of the array. This function is then called when we pass the type array in the return statement further down in the jsx area*/
+    const arrayToString = (array) => {
+        let string = "";
+        for (let i = 0; i < array.length; i++) {
+            string += array[i];
+            if (array.length-1 === i) {
+                
+            } else if (array.length-2 === i) {
+                string += " & ";
+            } else {
+                string += ", "
+            }  
+        }
+        return string;
+    };
+
+    let sentance ="";
+    if (type.length === 1) {
+        sentance = "Your favourite craft beer is "
+    } else {
+        sentance = "Your favourite craft beers are "
+    }   
+
     return (
-        <section>       
-            <h2>Your response:</h2>
+        <>       
+            <h3>Your craft beer profile:</h3>
                 <div>
+                    <h3>Age:</h3>
                     <p>{age}</p>
-                    <p>{type}</p>
+                    <h3>{sentance}</h3>
+                    <p>{arrayToString(type)}</p>            
+                    <h3>Price range</h3>
                     <p>{price}</p>
+                    <h3>Craft beer is No.1 to you because:</h3>
+                    <p>{special}</p>
                 </div>             
-        </section>
+        </>
     );
 };
