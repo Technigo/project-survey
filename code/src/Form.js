ddc777dda2530c904d7347f4ca13ffeca8068e11
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-23 08:29:33 +0200

    Updated Form.js file: 1. Imported the Special component file. 2. Added noted for myself regarding the onTypeChange function. 3. Added the special useState hook. 4. Added a div for the for the form. 5. Updated the form component props. 6. Wrapped the Summary form component tag in a section. 7. Updated the Summary tag with the form components.

--- code/src/Form.js
+++ code/src/Form.js
@@ -3,6 +3,8 @@ import { Summary } from './Summary'
 import { Age } from './Form components/Age.js'
 import { Type } from './Form components/Type.js'
 import { Price } from './Form components/Price.js'
+import { Special } from './Form components/Special.js'
+
 
 export const Form = () => {
   /* handleSubmit is using the preventDefault method to stop the 
@@ -14,27 +16,45 @@ export const Form = () => {
     setShowSummary(true);
   };
 
+  /*includes() checks if an array contains a certain element
+    filter() creates an array filled with all elements that pass a test implemented by the function
+    ... spread operator - Allows for array elements to be made individual and spead out in the array.
+    ? if : else
+
+    Function onTypeChange checks if type variable contains whatever is in the kinds argument which is defined in the Type.js.
+    if true -> then setType function will create an array in the type variable and not include those that are in the typeValue function
+    else -> then setType will spread out all array elements in the type variable and add whatever values that are in the typeValue into the type array. 
+  */
+  const onTypeChange = kinds => {
+    type.includes(kinds)
+    ? setType ( type.filter(item => item !== kinds) ) 
+    : setType( [...type, kinds] );
+  };
+
   const [age, setAge] = useState();
   const [type, setType] = useState([]);
   const [price, setPrice] = useState();
-  const [special, setSpecial] = useState();
+  const [special, setSpecial] = useState("");
   const [showSummary, setShowSummary] = useState(false);
 
     return (
       /* Using ternary operators below to show and hide the form and summary - if (?) else (:) */
       <>
         {!showSummary ? (
-          <div> 
+          <> 
             <form onSubmit={handleSubmit}>
-              <Age age={age} setage={setAge}/>
-              <Type type={type} setType={setType} />
+              <Age age={age} setAge={setAge}/>
+              <Type type={type} setType={setType} onTypeChange={onTypeChange} />
               <Price price={price} setPrice={setPrice} />
+              <Special special={special} setSpecial={setSpecial} />
 
               <button type="submit">SUBMIT</button> 
             </form>
-          </div>
-        ) : (        
-          <Summary age={age} type={type} price={price} />
+          </>
+        ) : ( 
+          <section>
+            <Summary age={age} type={type} price={price} special={special}/>
+          </section>       
         )}        
      </>    
     );
