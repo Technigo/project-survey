a5eb81453a57d12099ddd1da0e3e7162f56cce38
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-23 22:09:42 +0200

    Updated App.js file: 1. Added useState to the import react. 2. Imported Welcome, Don't take survey components. 3. Imported Logo. 4. Added pageState state. 5. Added variable which and the Welcome component tag to this. Passed the setPageState into it in order for the two different buttons to change the state depending on the number they have assigned to them based on the if else condition. 6. Added a logo to go with the H1 and a navbar to the header.

--- code/src/App.js
+++ code/src/App.js
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Welcome } from './Welcome.js'
+import { DontTakeSurvey } from './DontTakeSurvey.js'
 import { Form } from './Form.js'
+import  Logo  from './Images/blue-bottle.png'
 
 export const App = () => {
 
-  // const handleSubmit = () => {
-  //   preventDefault()
-  // }
+  const [pageState, setPageState] = useState(0);
+
+  let page = <Welcome setPageState={setPageState} />; 
+  if (pageState === 1) {
+    page = <Form />;
+  } else if (pageState === 2) {    
+    page = <DontTakeSurvey />;
+  } else {
+
+  }
 
   return (
     <main>
       <header>
-        <h1>CRAFT BEER PROFILE</h1>
+        <div className="logo-container">
+          <h1>CRAFT BEER Co.</h1>
+          <img src={Logo} alt="bottle logo" className="logo-image" />
+        </div>
+        <div className="nav-container">
+          <nav>
+            <a href="#">HOME</a>
+            <a href="#">PRODUCTS</a>
+            <a href="#">OUR STORY</a>
+            <a href="#">CONTACT</a>
+            <a href="#">CART</a>
+          </nav>
+        </div>
       </header>
-      <section>
-        <Form />      
-      </section>      
+      { page }   
     </main>
   );
 };
+
+// To do:
+// 1. Link each form section so they are shown in order based on the user clicking on a button
+// 2. Create an bottle image for the background so it looks like the form sections are inside or placed just over and outside of it.
+// 3. Make background of the form slighly transparent.
+// 4. Accessibility validation. 
