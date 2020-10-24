d37a0f69622450654a14ebddc68029a7213250b2
Trang Tara <trangnusi@gmail.com>
2020-10-24 10:03:15 +0200

    test

--- code/src/Header.js
+++ code/src/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="header section" id="header">
       <div className="wrapper">
         <h1 className="header-title">Suzuki Customer Satisfaction Survey</h1>
-        <p className="header-message">Let us hear your voice so we can serve you better. Every opinion counts!</p>
+        <p tabIndex="0" className="header-message">Let us hear your voice so we can serve you better. Every opinion counts!</p>
         <a className="start-survey-link" href="#sectionCarModels">Start survey</a>
       </div>
     </header>
