9a3100947a636e61765accc18719b78fb2cf46eb
Hanna Skeppe <hannaskeppe@gmail.com>
2020-10-24 21:20:04 +0200

    Spelling.

--- code/src/Summary.js
+++ code/src/Summary.js
@@ -6,11 +6,14 @@ const Summary = ({heading, name, color, ageGroup, userHobbies}) => {
   return (
     <>
       <h2>{heading}</h2>
-      <p>Hi {name}! Thanks for answering the summary!</p>
+      <p>Hi {name}! Thanks for answering the survey!</p>
       <p>Your favorite color is {color}. I agree, that really is a beautiful color!</p>
       <p>Your age is somewhere around {ageGroup} years. Great age!</p>
-      <p>In your sparetime you like to do {userHobbies.join(', ')}. Sounds like fun hobbies!</p>
+      <p>In your sparetime you like to do {userHobbies.join('& ')}. Sounds like fun hobbies!</p>
     </>
   )
 }
 export default Summary
+
+//How would I put a comma between hobbies except for the last hobbie where I want an '&'?
+//Something similar to what we did in the music-releases-project, but how exactly?
