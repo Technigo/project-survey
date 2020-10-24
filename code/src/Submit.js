bb512b7a9ae952953f5b8d72236e7991dc9c671e
Max Forssén <max.forssen@mentimeter.com>
2020-10-24 21:21:14 +0200

    accessibility

--- code/src/Submit.js
+++ code/src/Submit.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 export const Submit = () => {
   return <div className="question">
-           <label for="submit" tabIndex="0">Submit</label>
-           <input type="submit" id="submit" tabIndex="0"/>
+           <input type="submit" title="submit" tabIndex="0"/>
          </div>
 }
