8d1b7c65e9b45514d78d4bee7bda247c40305f92
Trang Tara <trangnusi@gmail.com>
2020-10-24 09:25:57 +0200

    test accessibility

--- code/src/UserCarModels.js
+++ code/src/UserCarModels.js
@@ -15,7 +15,7 @@ const UserCarModels = ({ onCarsChange }) => {
   return (
     <div className="car-models" id="sectionCarModels">
       <label htmlFor="carModelSelect">Which Suzuki car(s) that you are possessing?</label>
-     <Select id="carModelSelect" 
+     <Select id="carModelSelect" aria-label="list"
       // defaultValue={[]}
       onChange={onCarsChange}
       options={selectCarModels}
