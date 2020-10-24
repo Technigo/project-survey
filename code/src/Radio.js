9a3c57a74b3aab0257886f7948205ff832d0b7f4
Max Forssén <max.forssen@mentimeter.com>
2020-10-24 18:09:13 +0200

    accessibility

--- code/src/Radio.js
+++ code/src/Radio.js
@@ -5,11 +5,11 @@ export const Radio = ({ handleRadioInput }) => {
       <p tabIndex="0">Favourite callsign</p>
       <div>
         <label for="mav" tabIndex="0">Maverick</label> 
-        <input type="radio" value="Maverick" name="callsign" id="mav" onChange={e => handleRadioInput(e.target.value)}/>
+        <input type="radio" value="Maverick" id="mav" onChange={e => handleRadioInput(e.target.value)}/>
         <label for="goose" tabIndex="0">Goose</label> 
-        <input type="radio" value="Goose" name="callsign" id="goose" onChange={e => handleRadioInput(e.target.value)}/>
+        <input type="radio" value="Goose" id="goose" onChange={e => handleRadioInput(e.target.value)}/>
         <label for="sundown" tabIndex="0">Sundown</label> 
-        <input type="radio" value="Sundown" name="callsign" id="sundown" onChange={e => handleRadioInput(e.target.value)}/>
+        <input type="radio" value="Sundown" id="sundown" onChange={e => handleRadioInput(e.target.value)}/>
       </div>
   </div>
 }
--- code/src/Submit.js
+++ code/src/Submit.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export const Submit = () => {
   return <div className="question">
-           <input type="submit" tabIndex="0"/>
+           <label for="submit" tabIndex="0">Submit</label>
+           <input type="submit" name="submit" tabIndex="0"/>
          </div>
 }
