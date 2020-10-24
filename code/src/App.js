d53af20eb4e2355744735f5f67d049acf9bbb125
Gabbi-89 <gabriellabolin@hotmail.com>
2020-10-24 13:38:01 +0200

    Update

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Form } from './components/Form'
+import { Form } from './components/Form';
 import { Footer } from './components/Footer';
 
 export const App = () => {
