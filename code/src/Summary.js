824e220abfa65c8ea8ceda5001e6c60b47eed56c
Mats Johansen <johansenmats89@gmail.com>
2020-10-24 12:32:54 +0200

    Changes in generating {name}

--- code/src/Summary.js
+++ code/src/Summary.js
@@ -5,7 +5,7 @@ export const Summary = ({ name, surfboard, wave }) => {
   return (
     <div className="summary">
       <p tabIndex="0">
-        Hi {name.charAt(0).toUpperCase()}! You prefer to surf a {surfboard.toLowerCase()} in {wave.toLowerCase()} waves!
+        Hi {name}! You prefer to surf a {surfboard.toLowerCase()} in {wave.toLowerCase()} waves!
       </p>
     </div>
   )
