14f9412a6087497cc9b05209baa6b064f7982bed
Gabbi-89 <gabriellabolin@hotmail.com>
2020-10-24 10:27:27 +0200

    Header changes

--- code/src/Components/Form.js
+++ code/src/Components/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Header } from 'components/Header';
+import { Header } from './Header';
 import { NameInput } from './NameInput';
 import { EmailInput } from './EmailInput';
 import { Radiobuttons } from './Radiobuttons';
