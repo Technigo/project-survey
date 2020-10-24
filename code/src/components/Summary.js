21e37d787e5dfebd12a9f9aa9f2b36220d055d20
Sandra Shumanteva Kaevska <sandra@shumantevi.mk>
2020-10-24 15:02:59 +0200

    summary changes

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -10,12 +10,12 @@ const Summary = ({
 }) => {
   return (
     <div className="container">
-      <h3>Your Name is: {firstName}</h3>
-      <h3>Your Last Name is: {lastName}</h3>
-      <h3>Your Gender is: {gender}</h3>
-      <h3>Your Occupation is: {occupation}</h3>
-      <h3>Your Age is: {age} </h3>
-      <h3>Your Informations are/is from: {information}</h3>
+      <h3>First Name: {firstName}</h3>
+      <h3>Last Name: {lastName}</h3>
+      <h3>Gender: {gender}</h3>
+      <h3>Occupation: {occupation}</h3>
+      <h3>Age: {age} </h3>
+      <h3>Where did you hear about us: {information}</h3>
     </div>
   );
 };
