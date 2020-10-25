4ae7f26f339c96e1d62a4bb82a47880878180b15
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-25 19:17:06 +0100

    Updated Form.js: 1. Removed the class name for the form tag. 2. Deleted a p tag. 3. Added arial-label to the buttons to give users using screen readers more help when going through the buttons.

--- code/src/Form.js
+++ code/src/Form.js
@@ -6,6 +6,7 @@ import { Price } from './Form components/Price.js'
 import { Special } from './Form components/Special.js'
 import { Summary } from './Summary'
 
+
 export const Form = () => {
   /* handleSubmit is using the preventDefault method to stop the 
   page from reloading when the form is submitted when we click the button.
@@ -29,35 +30,35 @@ export const Form = () => {
       /* Created the showFormQuestion state hook above so it can be used for the form components to be shown in a specific order when the button is clicked. Each of the comparison statements below compares if showFormQuestion is equals to that number then the specific form component should be shown. The showFormQuestion is incremented by 1 in the button below by way of the setShowFormQuestion function which allows for each of the form components to be shown depending on the number that showFormQuestion is storing. */
       <>
         {!showSummary ? (
-          <>
-            <form onSubmit={handleSubmit}>
+          <section>
+            <form onSubmit={handleSubmit} className="form-container"  >
               {showFormQuestion === 0 && (
                 <Name name={name} setName={setName} />
               )}                
               {showFormQuestion === 1 && (
-                <Age age={age} setAge={setAge}/>
+                <Age age={age} setAge={setAge} />
               )}
               {showFormQuestion === 2 && (
-                <Type type={type} setType={setType} />
+                <Type type={type} setType={setType} tabIndex="0"/>
               )}
               {showFormQuestion === 3 && (
-                <Price price={price} setPrice={setPrice} />
+                <Price price={price} setPrice={setPrice} tabIndex="0"/>
               )}
               {showFormQuestion === 4 && (
-                <Special special={special} setSpecial={setSpecial} />
+                <Special special={special} setSpecial={setSpecial} tabIndex="0"/>
               )}
 
               {/* If else statement which compares if the showFormQuestion is equals to 4 then the submit button will be show. Otherwise the next button will be shown */}
               {showFormQuestion === 4 ? (
                 <> 
-                  <p>Submit your answers!</p>               
-                  <button type="submit">SUBMIT</button>
+                <button type="submit" className="button yes-button" aria-label="Press submit to end the survey">SUBMIT</button>
                 </>
               ) : (
-                <button type="button" className="form-button" onClick={event => setShowFormQuestion(showFormQuestion +1)}>Next</button>
+                
+                <button type="button" className="button yes-button" aria-label="Press the button to go to the next question" onClick={event => setShowFormQuestion(showFormQuestion +1)}>NEXT</button>
               )}               
             </form>
-          </>
+          </section>
         ) : ( 
           <section>
             <Summary name={name} age={age} type={type} price={price} special={special} />
