21d3f13b4da4b3256a8670cfdfddf03b708abfa7
Mats Johansen <johansenmats89@gmail.com>
2020-10-25 13:31:13 +0100

    Fixed previous saving issues

--- code/src/InputRadio.js
+++ code/src/InputRadio.js
@@ -1,27 +1,33 @@
 import React from 'react';
 
 export const InputRadio = ({ label, id, array, state, setState }) => {
-  return (
-    <>
-      <p tabIndex="0">{label}</p>
+  return ( <
+    >
+    <
+    p tabIndex = "0" > { label } < /p>
 
-      {array.map(item => (
+    {
+      array.map(item => (
 
-        <div key={item} className="input-radio">
-          <input
-            type="radio"
-            id={item}
-            value={item}
-            checked={state === item}
-            onChange={event => setState(event.target.value)}
-            required
-            name={id}
-          />
-          <label htmlFor={item} className="radio-label">{item}</label>
-        </div>
+        <
+        div key = { item }
+        className = "input-radio" >
+        <
+        input type = "radio"
+        id = { item }
+        value = { item }
+        checked = { state === item }
+        onChange = { event => setState(event.target.value) }
+        required name = { id }
+        /> <
+        label htmlFor = { item }
+        className = "radio-label" > { item } < /label> <
+        /div>
 
-      ))}
-      
-    </>
-  )
-}
+      ))
+    }
+
+    <
+    />
+  );
+};
