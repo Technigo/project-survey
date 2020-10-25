3798046c4b34218da84c4bb30907fae19f9d5f8d
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-25 19:10:15 +0100

    Updated Welcome.js: 1. Deleted bottle image. 2. Edited the div with different class name, tabindex and deleted the second button.

--- code/src/Welcome.js
+++ code/src/Welcome.js
@@ -1,19 +1,15 @@
 import React from 'react'
-import  BottleImages  from './Images/mix-bottles-2.png'
+// import  BottleImage  from './Images/mix-bottles-2.png'
 
-export const Welcome= ({setPageState}) => {
+export const Welcome= ({setPageState, BottleImage}) => {
     return (
         <section>
-            <div className="create-profile-container">
-                <div className="create-profile-text">
-                    <h2>Craft Beer Co survey</h2>
-                    <p>Fill out the survey to help us improve our products!</p>
-                    <div className="button-container">
-                        <button type="button" className="profile-button yes-button" onClick={event => setPageState(false)}>Yes please!</button>
-                    </div>
+            <div className="welcome-container">
+                <h2 tabIndex="0">Craft Beer Co. survey</h2>
+                <p tabIndex="0">Fill out the survey to help us improve our products!</p>
+                <div>
+                    <button type="button" className="button yes-button" aria-label="Press enter to start the survey" onClick={event => setPageState(false)}>START SURVEY</button>
                 </div>
-                <img src={BottleImages} alt="bottle images" className="bottle-image"/>
-
             </div>
         </section>
     );
