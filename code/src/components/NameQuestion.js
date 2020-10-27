e96bbfa5946bec03f036e45453b7f05a9ef64a21
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-10-27 23:27:06 +0100

    Debug 2

--- code/src/components/NameQuestion.js
+++ code/src/components/NameQuestion.js
@@ -6,11 +6,12 @@ export const NameQuestion = ({ name, setName }) => {
     <div className='name-question' >
 			<h2 tabIndex='0'>What is your full name?</h2>
 			<label 
-				for='name'
 				tabIndex='0' 
 				onSubmit={event => event.preventDefault()}
+				value = 'Your Name'
 			>
 				<input
+					tabIndex='0' 
 					label="Enter Name here"
 					type='text'
 					className='name'
