f1d50b417a6d234910b4c0144586e836d5309eef
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-10-27 23:38:10 +0100

    Requested Changes made after PR

--- code/src/components/NameQuestion.js
+++ code/src/components/NameQuestion.js
@@ -3,23 +3,18 @@ import React from 'react'
 export const NameQuestion = ({ name, setName }) => {
 	
 	return (
-    <div className='name-question' >
+    <div className="name-question" >
 			<h2 tabIndex='0'>What is your full name?</h2>
-			<label 
-				tabIndex='0' 
-				onSubmit={event => event.preventDefault()}
-				value = 'Your Name'
-			>
-				<input
-					tabIndex='0' 
-					label="Enter Name here"
-					type='text'
+			<label tabIndex='0' onSubmit={event => event.preventDefault()}>
+				<input autoFocus
+					type="text"
 					className='name'
 					onChange={event => setName(event.target.value)}
 					value={name}
 					required minLength='2'
+					aria-label="Enter your name here"
 					/>
 			</label>
     </div>
-)
+    )
 }
