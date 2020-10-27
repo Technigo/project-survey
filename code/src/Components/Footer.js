9bc7f3358230141803879923afcb80a313e3313d
petrame <63057558+petrame@users.noreply.github.com>
2020-10-28 00:02:24 +0100

    add space to footer-text

--- code/src/Components/Footer.js
+++ code/src/Components/Footer.js
@@ -5,12 +5,12 @@ export const Footer = () => {
   return (
         <footer tabIndex="0">
           <p className="author">&copy;
-            <a href="https://github.com/petrame/">&nbsp;Petra Almgren</a>
+            <a href="https://github.com/petrame/">Petra Almgren&nbsp;</a>
             for Technigo bootcamp 2020
           </p>
           <p className="cred">Images from 
-            <a href="https://www.deviantart.com/unidcolor">&nbsp;UnidColor</a>
-            &nbsp;and
+            <a href="https://www.deviantart.com/unidcolor">&nbsp;UnidColor&nbsp;</a>
+            and
             <a href="https://www.iconarchive.com/artist/ergosign.html" >&nbsp;Ergosign</a>.
           </p>
         </footer>
