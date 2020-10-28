05f87307cac2b5e6fb6cfacbd84c1475d4c030f7
Lili <lilihorvath@github.com>
2020-10-28 14:57:17 +0100

    Fixing some indentatio adn grammar

--- code/src/Components/Input.js
+++ code/src/Components/Input.js
@@ -4,7 +4,7 @@ export const Input = ({ name, setName}) => {
     return (
         <label>
             <h1> Hi Sweetie, are you ready to choose your favorite dessert? </h1>
-            <h2> First of all, I need to know, is your name sweet enough ti start this survey?</h2>
+            <h2> First of all, I need to know, is your name sweet enough to start this survey?</h2>
             <input className='text-input'
                 type='text'
                 onChange={event => setName(event.target.value)}
