18d413ecc1c83eab5571c7c9910ad5ec891ae569
Trang Tara <trangnusi@gmail.com>
2020-10-29 11:24:08 +0100

    tabindex 0 works

--- code/src/Summary.js
+++ code/src/Summary.js
@@ -16,30 +16,30 @@ const Summary = ({
     const carModels = userCarModels.map(({ label }) => label);
 
     return (
-        <div className="summary" id="sectionSummary" tabIndex="0">
-            <div className="wrapper">
-                <h2 className="summary-title">Thank you for your feedback!</h2>
-                <p className="summary-sub-title">Below is your answer to the survey:</p>
+        <div className="summary" id="sectionSummary" >
+            <div className="wrapper" tabIndex="0">
+                <h2 taIndex="0" className="summary-title">Thank you for your feedback!</h2>
+                <p tabIndex="0" className="summary-sub-title">Below is your answer to the survey:</p>
                 <div className="summary-message-container">
-                    <p className="summary-message">Your Name: {userName}</p>
+                    <p tabIndex="0" className="summary-message">Your Name: {userName}</p>
                 </div>
                 <div className="summary-message-container">
-                    <p className="summary-message">Your Age: {userAge}</p>
+                    <p tabIndex="0" className="summary-message">Your Age: {userAge}</p>
                 </div>
                 <div className="summary-message-container">
-                    <p className="summary-message">Your Email: {email}</p>
+                    <p tabIndex="0" className="summary-message">Your Email: {email}</p>
                 </div>
                 <div className="summary-message-container">
-                    <p className="summary-message">The Suzuki car models you have: {carModels.join(', ')}</p>
+                    <p tabIndex="0" className="summary-message">The Suzuki car models you have: {carModels.join(', ')}</p>
                 </div>
                 <div className="summary-message-container">
-                    <p className="summary-message">Reasons you bought Suzuki car(s): {reasonsToBuy.join(', ')}</p>
+                    <p tabIndex="0" className="summary-message">Reasons you bought Suzuki car(s): {reasonsToBuy.join(', ')}</p>
                 </div>
                 <div className="summary-message-container">
-                    <p className="summary-message">Your opinion about the Aftersales Services of Suzuki is: {userRating}</p>
+                    <p tabIndex="0" className="summary-message">Your opinion about the Aftersales Services of Suzuki is: {userRating}</p>
                 </div>
                 <div className="summary-message-container">
-                    <p className="summary-message">Your message: {textMessage}</p>
+                    <p tabIndex="0" className="summary-message">Your message: {textMessage}</p>
                 </div>
                 <div className="summary-buttons">
                     <EditAnswerButton
