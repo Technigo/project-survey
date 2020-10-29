afff947e117acff9599438614985ce6ecf27b700
Trang Tara <trangnusi@gmail.com>
2020-10-29 10:52:50 +0100

    improve codes for the ReasonsToBuy component

--- code/src/ReasonsToBuy.js
+++ code/src/ReasonsToBuy.js
@@ -1,95 +1,72 @@
 import React from 'react';
 
-const GOOD_GUARANTEE_VALUE = "good guarantee policy";
+const reasons = [
+    {
+        id:"goodQuality",
+        checked:"goodQuality",
+        label:"Good Quality",
+    },
+    {
+        id:"goodPrice",
+        checked:"goodPrice",
+        label:"Good Price",
+    },
+    {
+        id:"goodDesign",
+        checked:"goodDesign",
+        label:"Good Design",
+    },
+    {
+        id:"goodAfterSalesServices",
+        checked:"goodAfterSalesServices",
+        label:"Good Aftersales Services",
+    },
+    {
+        id:"goodGuaranteePolicy",
+        checked:"goodGuaranteePolicy",
+        label:"Good Guarantee Policy",
+    },
+    {
+        id:"goodInitiatives",
+        checked:"goodInitiatives",
+        label:"Good Initiatives",
+    },
+    {
+        id:"lowMaintenanceCost",
+        checked:"lowMaintenanceCost",
+        label:"Low Maintenance Cost",
+    },
+    {
+        id:"practicability",
+        checked:"practicability",
+        label:"Practicability",
+    },
+
+
+]
 
 const ReasonsToBuy = ({ userReasons, onReasonsChange }) => {
   return (
-    <div className="section-reasons-to-buy" id="sectionReasonsToBuy">
-        <p tabIndex="0">What are the reasons that made you decide to own (a) Suzuki car(s)?</p>
-        <div className="reasons-to-buy-container">
-            <article className="reasons-input-element">
-                <input tabIndex="0"
-                    id="goodQuality"
-                    className="check-box-button"
-                    type="checkbox"
-                    checked={userReasons.includes("good quality")}
-                    onChange={() => onReasonsChange("good quality")}
-                />
-                <label htmlFor="goodQuality">Good Quality</label>
-            </article>
-            <article className="reasons-input-element">
-                <input tabIndex="0"
-                    id="goodPrice"
-                    className="check-box-button"
-                    type="checkbox"
-                    checked={userReasons.includes("good price")}
-                    onChange={() => onReasonsChange("good price")}
-                />
-                <label htmlFor="goodPrice">Good Price</label>
-            </article>
-            <article className="reasons-input-element">
-                <input tabIndex="0"
-                    id="goodDesign"
-                    className="check-box-button"
-                    type="checkbox"
-                    checked={userReasons.includes("good design")}
-                    onChange={() => onReasonsChange("good design")}
-                 />
-                <label htmlFor="goodDesign">Good Design</label>
-            </article>
-            <article className="reasons-input-element">
-                <input tabIndex="0"
-                    id="goodAfterSalesServices"
-                    className="check-box-button"
-                    type="checkbox"
-                    checked={userReasons.includes("good aftersales services")}
-                    onChange={() => onReasonsChange("good aftersales services")}
-                />
-                <label htmlFor="goodAfterSalesServices">Good Aftersales Services</label>
-            </article>
-            <article className="reasons-input-element">
-                <input tabIndex="0"
-                    id="goodInitiatives"
-                    className="check-box-button"
-                    type="checkbox"
-                    checked={userReasons.includes("good initiatives")}
-                    onChange={() => onReasonsChange("good initiatives")}
-                />
-                <label htmlFor="goodInitiatives">Good Initiatives</label>
-            </article>
-            <article className="reasons-input-element">
-                <input tabIndex="0"
-                    id="goodGuaranteePolicy"
-                    className="check-box-button"
-                    type="checkbox"
-                    checked={userReasons.includes(GOOD_GUARANTEE_VALUE)}
-                    onChange={() => onReasonsChange(GOOD_GUARANTEE_VALUE)}
-                />
-                <label htmlFor="goodGuaranteePolicy">Good Guarantee Policy</label>
-            </article>
-            <article className="reasons-input-element">
-                <input tabIndex="0"
-                    id="lowMaintenanceCost"
-                    className="check-box-button"
-                    type="checkbox"
-                    checked={userReasons.includes("low maintenance cost")}
-                    onChange={() => onReasonsChange("low maintenance cost")}
-                />
-                <label htmlFor="lowMaintenanceCost">Low Maintenance Cost</label>
-            </article>
-            <article className="reasons-input-element">
-                <input tabIndex="0"
-                    id="practicability"
-                    className="check-box-button"
-                    type="checkbox"
-                    checked={userReasons.includes("Practicability")}
-                    onChange={() => onReasonsChange("Practicability")}
-                />
-                <label htmlFor="practicability">Practicability</label>
-            </article>
-            
+        <div className="section-reasons-to-buy" id="sectionReasonsToBuy">
+            <p tabIndex="0">What are the reasons that made you decide to own (a) Suzuki car(s)?</p>
+            {reasons.map((reason) => {
+                return(
+                    <div className="reasons-to-buy-container" key={reason.id}>
+                        <article className="reasons-input-element">
+                            <input 
+                                tabIndex="0"
+                                id={reason.id}
+                                className="check-box-button"
+                                type="checkbox"
+                                checked={userReasons.includes(reason.label)}
+                                onChange={() => onReasonsChange(reason.label)}
+                            />
+                            <label htmlFor={reason.id}>{reason.label}</label>
+                        </article>
+                    </div>
+                )
+            })}
         </div>
-    </div>
     );
 }
  
