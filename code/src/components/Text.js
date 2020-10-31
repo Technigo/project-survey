944ab15a55833c198819dec12fb922ff58358363
Jamie Cook <jamie.p.cook@outlook.com>
2020-10-31 11:47:09 +0100

    changes made as per code review

--- code/src/components/Text.js
+++ code/src/components/Text.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import '../style/input-box.css';
+import "../style/input-box.css";
 
 export const Text = ({ name, setName, askQuestion, handleNameChange }) => {
-
-  return(
+  return (
     <div>
-      <label htmlFor="userName"><h2>{askQuestion}</h2></label>
-        <input
-          id="userName"
-          className="input-text"
-          onKeyPress={event => {event.key === 'Enter' && event.preventDefault();}}
-          type="text"
-          onChange={handleNameChange}
-          value={name}
-          placeholder="Enter name here..."
-          required
-          minLength="2"
-        />
+      <label htmlFor="userName">
+        <h2>{askQuestion}</h2>
+      </label>
+      <input
+        id="userName"
+        className="input-text"
+        onKeyPress={event => {
+          event.key === "Enter" && event.preventDefault();
+        }}
+        type="text"
+        onChange={handleNameChange}
+        value={name}
+        placeholder="Enter name here..."
+        required
+      />
     </div>
-  
   );
 };
