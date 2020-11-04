19065d37e689ec5d4210dc7b2ae271c552c60430
Therese <64402455+Thessan@users.noreply.github.com>
2020-11-04 08:40:13 +0100

    fixed some indentation and 0px to 0 in CSS

--- code/src/Survey.js
+++ code/src/Survey.js
@@ -36,76 +36,84 @@ return (
     <form onSubmit= {handleSubmit}>
         <div className="questions" tabIndex="0">
             <div className="dropdown">
-            <h3>Where did you participate?</h3>
-                <select value={location} onChange={event => setLocation(event.target.value)} aria-label="Dropdown menu to choose the city where you participated">
-                <option value="">Choose city...</option>
-                <option value="gothenburg">Gothenburg</option>
-                <option value="stockholm">Stockholm</option>
-                <option value="umea">Umea</option>
-                </select>
+                <h3>Where did you participate?</h3>
+                    <select value={location} onChange={event => setLocation(event.target.value)} aria-label="Dropdown menu to choose the city where you participated">
+                        <option value="">Choose city...</option>
+                        <option value="gothenburg">Gothenburg</option>
+                        <option value="stockholm">Stockholm</option>
+                        <option value="umea">Umea</option>
+                    </select>
             </div>
 
         <div className="line"></div>
                 
-            <div className="radio" tabIndex="0">
+        <div className="radio" tabIndex="0">
             <h3>What baking experience did you participate in?</h3>
                 <label htmlFor="candyCanes">
-                <input
-                type="radio"
-                value="candyCanes" 
-                name="radioButton"  //by giving all radio buttons the same name, only one button can be/stay clicked at once
-                onChange={event => setCandyCanes(event.target.value)}
-                checked={radioButton1 === 'candyCanes'}/>
-                Candy Canes
+                    <input
+                        type="radio"
+                        value="candyCanes" 
+                        name="radioButton"  //by giving all radio buttons the same name, only one button can be/stay clicked at once
+                        onChange={event => setCandyCanes(event.target.value)}
+                        checked={radioButton1 === 'candyCanes'}/>
+                            Candy Canes
                 </label>
             </div>
 
             <div className="radio" tabIndex="0">
                 <label htmlFor="gingerBreadHouses">
-                <input 
-                type="radio"
-                value="gingerBreadHouses" 
-                name="radioButton"
-                onChange={event => setGingerBreadHouses(event.target.value)}
-                checked={radioButton2 === 'gingerBreadHouses'}/>
-                Ginger Bread Houses
+                    <input 
+                        type="radio"
+                        value="gingerBreadHouses" 
+                        name="radioButton"
+                        onChange={event => setGingerBreadHouses(event.target.value)}
+                        checked={radioButton2 === 'gingerBreadHouses'}/>
+                            Ginger Bread Houses
                 </label>
             </div>
 
             <div className="radio" tabIndex="0">
                 <label htmlFor="christmasChocolates">
-                <input
-                type="radio"
-                value="christmasChocolates"
-                name="radioButton"
-                onChange={event => setChristmasChocolates(event.target.value)}
-                checked={radioButton3 === 'christmasChocolates'}/>
-                Christmas Chocolates
+                    <input
+                        type="radio"
+                        value="christmasChocolates"
+                        name="radioButton"
+                        onChange={event => setChristmasChocolates(event.target.value)}
+                        checked={radioButton3 === 'christmasChocolates'}/>
+                            Christmas Chocolates
                 </label>
             </div>
 
         <div className="line"></div>
                 
             <div className="feedback" tabIndex="0">
-            <h3>Feedback</h3>
-                <label htmlFor="positiveFeedback">What did you find most positive about the baking experience?</label>
-                <input type="text"
-                id="positiveFeedback"
-                onChange={event => setPositiveFeedback(event.target.value)}/>
+                <h3>Feedback</h3>
+                    <label htmlFor="positiveFeedback">
+                        What did you find most positive about the baking experience?
+                    </label>
+                        <input type="text"
+                            id="positiveFeedback"
+                            onChange={event => setPositiveFeedback(event.target.value)}/>
             </div>
 
             <div className="feedback">
-                <label htmlFor="improveFeedback">Is there something you think we could improve on?</label>
-                <input type="text"
-                id="improveFeedback" 
-                onChange={event => setImproveFeedback(event.target.value)}/>
+                <label htmlFor="improveFeedback">
+                    Is there something you think we could improve on?
+                </label>
+                    <input type="text"
+                        id="improveFeedback" 
+                        onChange={event => setImproveFeedback(event.target.value)}/>
             </div>
 
         <div className="line"></div>  
         </div>
 
             <div className="submitButton" tabIndex="0">
-            <button type="submit" onClick={(event) => handleSubmit()} value="Submit"> Send your answer </button>
+                <button type="submit"
+                        value="Submit"
+                        onClick={(event) => handleSubmit()}>
+                            Send your answer
+                </button>
     </div>      
         </form>
         )} 
