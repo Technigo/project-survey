48d1b52b0aa423734a75b310be55f9895e1723a3
Therese <64402455+Thessan@users.noreply.github.com>
2020-11-04 20:55:45 +0100

    deleted unused useState in App and Summary

--- code/src/App.js
+++ code/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Form from './Survey'; 
 
 export const App = () => {
--- code/src/Summary.js
+++ code/src/Summary.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const locationResponse = location => {
     if (location === 'gothenburg') {
