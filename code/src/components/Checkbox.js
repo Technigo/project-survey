8ac9cb0bd07b429f0882601b731da8d4a93bc703
Rebkova <rebkova@gmail.com>
2020-11-11 13:58:45 +0100

    removed htmlFor attribute from the label tag and the closing tag for the input tag and made it self closing. All in the Checkbox component

--- code/src/components/Checkbox.js
+++ code/src/components/Checkbox.js
@@ -8,38 +8,32 @@ const Checkbox = ({ isBoxChecked, onCheckBoxChange }) => {
   return (
     <section className="section-container">
       <h2 tabIndex="0">I would like a price estimation for:</h2>
-      <label htmlFor="moving">
+      <label >
         <input
           tabIndex="0"
           type="checkbox"
-          id="moving"
           onChange={() => onCheckBoxChange("moving")}
           checked={isBoxChecked.includes("moving")}
-        >
-        </input>
+        />
         Moving
       </label>
-      <label htmlFor="packing">
+      <label>
         <input
           tabIndex="0"
           type="checkbox"
-          id="packing"
           onChange={() => onCheckBoxChange("packing")}
           checked={isBoxChecked.includes("packing")}
-        >
-        </input>
+        />
         Packing
       </label>
-      <label htmlFor="cleaning">
+      <label>
         <input
           tabIndex="0"
           type="checkbox"
-          id="cleaning"
           // onChange={(event) => onCheckBoxChange(event.target.checked)} - what is the difference?
           onChange={() => onCheckBoxChange("cleaning")}
           checked={isBoxChecked.includes("cleaning")}
-        >
-        </input>
+        />
         Cleaning
       </label>
     </section >
