d9ed0fe5e022a9b21ecde9191f2e92ec41403cb7
Gabbi-89 <gabriellabolin@hotmail.com>
2020-12-07 15:00:37 +0100

    Updated footer

--- code/src/Components/Footer.js
+++ code/src/Components/Footer.js
@@ -9,13 +9,13 @@ export const Footer = () => {
       <p tabIndex="0">This is a project created by Gabriella Bolin during Technigo's Bootcamp 20/21.</p>
       <p tabIndex="0">The answers will not be sent to me. </p>
       <p tabIndex="0">If you want to contact me please reach me on 
-        <span className="email-underlined">gabriellabolin@gmail.com </span> 
+        <span className="email-underlined"> gabriellabolin@gmail.com </span> 
       or 
       <a href="https://www.linkedin.com/in/gabriella-bolin-b718544a/" 
             aria-label="Link to LinkedIn"
             target="_blank" 
             rel="noopener noreferrer">
-             @LinkedIn.
+              @LinkedIn.
           </a>
       </p>
     </footer>
