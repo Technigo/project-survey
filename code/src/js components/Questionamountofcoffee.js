3e6120cde7de0454caa0c570f173c2ed5229ac75
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2020-12-14 11:17:55 +0100

    label improved

--- code/src/js components/Questionamountofcoffee.js
+++ code/src/js components/Questionamountofcoffee.js
@@ -7,33 +7,33 @@ return(
     <p tabIndex='0' >How many cups of coffee do you drink per week?</p>
       <label 
         tabIndex='0' 
-        htmlFor="answerAmount">1-3
+        htmlFor="1-3">1-3
       </label>
       <input 
         type="radio"
         onChange = {event => onAmountChange(event.target.value)}
-        id="answerAmount"
+        id="1-3"
         value = "1-3"
         name="cups"
         checked = {answeramount === "1-3"}
         required
         aria-label="Select the amount of cups of coffee you drink per week"
       />
-      <label htmlFor="answerAmount">4-7</label>
+      <label htmlFor="4-6">4-6</label>
         <input 
           type="radio"
           onChange = {event => onAmountChange(event.target.value)}
           value = "4-6"
-          id="answerAmount"
+          id="4-6"
           name="cups"
           checked = {answeramount === "4-6"}
         />
-      <label htmlFor="answerAmount">7-10</label>
+      <label htmlFor="7-10">7-10</label>
         <input 
           type="radio"
           onChange = {event => onAmountChange(event.target.value)}
           value = "7-10"
-          id="answerAmount"
+          id="7-10"
           name="cups"
           checked = {answeramount === "7-10"}
         />
