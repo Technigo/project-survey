80165a27e1a60877dabcedb66596e9a79b7c502e
Rasmus Persson <rrasmuspersson@gmail.com>
2021-03-13 07:17:51 +0100

    cleaned up the boiler plate

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-export const App = () => {
+const App = () => {
   return (
     <div>
       Find me in src/app.js!
     </div>
-  )
-}
+  );
+};
+
+export default App;
