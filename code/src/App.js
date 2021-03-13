3dab18bdf6e096985ba12d1293eb1fcfa1ab53ea
Rasmus Persson <rrasmuspersson@gmail.com>
2021-03-13 08:37:22 +0100

    added a header component

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./components/Header"
 
 const App = () => {
   return (
     <div>
       Find me in src/app.js!
+      <Header/>
     </div>
   );
 };
---
+++ code/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <h2>Amazing Survey</h2>
+    </div>
+  );
+};
+
+export default Header;
