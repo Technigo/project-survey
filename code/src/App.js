b3d581bbe3c84e3b1a3fcd089137e68f4d1d1562
liaback <felicia.liaback@gmail.com>
2021-03-15 18:49:47 +0100

    structure component folder

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from './Components/Header'
+import Survey from './Components/Survey'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+      <Header />
+      <Survey />
+    </>
   )
 }
---
+++ code/src/Components/Survey.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Survey = () => {
+    return (
+        <p>input comes here...</p>
+    )
+
+}
+
+export default Survey
