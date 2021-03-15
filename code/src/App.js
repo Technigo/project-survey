9b827d050291974512527795f5834395fc66efcc
Tina <tina.juren@gmail.com>
2021-03-15 11:16:46 +0100

    created a header component and added header styling

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from './Components/Header.js'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <main>
+      <article className="survey-page">
+          <Header headline="Airline Website Online Survey"/>
+      </article>
+    </main>
   )
 }
---
+++ code/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = (props) => {
+    return (
+        <header className="survey-page-header">
+            <h1 className="survey-title">{props.headline}</h1>
+        </header>
+    )
+}
+
+export default Header
