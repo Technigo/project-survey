f9055c9e1dd0c8ac61c65fc220db07527af91d05
Mir Albertioni <fethullah.dincer@gmail.com>
2021-03-16 12:09:52 +0100

    embedded images to radio buttons

--- code/src/components/Image.js
+++ code/src/components/Image.js
@@ -1,18 +1,12 @@
 import React, {useState} from 'react'
-import Radio from './Radio'
 
 
-const ImageSelect = (GroupValue) => {
-    const images = ["./assets/jacket2.jpg","./assets/jacket3.jpg","./assets/jacket4.jpg"]
-
-    const trying = {GroupValue,images}
-    console.log(trying)
+const ImageSelect = (image) => {
+    console.log(image)
 
     return (
-         <img className="teste2" src={GroupValue.images} />
-
+         <img className="teste2" src={image.image} />
         )
-
 }
 
 export default ImageSelect
--- code/src/components/Radio.js
+++ code/src/components/Radio.js
@@ -1,36 +1,31 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Image from './Image'
 
-const ageGroups = ["0-18","19-30","30+"]
+const ageGroups = ["0-18", "19-30", "30+"]
 
 const Radio = () => {
-    const [ageGroup, setageGroup] = useState();
-    const images = ["./assets/jacket2.jpg","./assets/jacket3.jpg","./assets/jacket4.jpg"]
-    const images2 = [0["./assets/jacket2.jpg"],1["./assets/jacket3.jpg"],2["./assets/jacket4.jpg"]]
-    const ima = "./assets/jacket3.jpg"
-    console.log(images)
+  const [ageGroup, setageGroup] = useState();
+  const images = ["./assets/jacket2.jpg", "./assets/jacket3.jpg", "./assets/jacket4.jpg"]
 
-    return (
-        <label>
-        <p>Age Group:</p>
+  return (
+    <label>
+      <p>Age Group:</p>
 
 
-        {ageGroups.map(group, => (
-              <label key={group}>
-                        <label >
-              <Image GroupValue={group} className="teste" />
-              </label>
-              <input
-                type="radio"
-                value={group}
-                onChange={event => setageGroup(event.target.value)}
-                checked={ageGroup === group}
-              />
-              {group}
-            </label>
-      ))}
+      {ageGroups.map((group, imageIndex) => (
+        <label key={group}>
+          <Image image={images[imageIndex]} />
+          <input
+            type="radio"
+            value={group}
+            onChange={event => setageGroup(event.target.value)}
+            checked={ageGroup === group}
+          />
+          {group}
         </label>
-    )
+      ))}
+    </label>
+  )
 }
 
 export default Radio
