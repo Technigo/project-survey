0fb5601c45ce7c0371cf445abe61708efdd96082
Laima Duhovnaja <laima@laimas-mbp.mynet>
2021-03-17 17:08:30 +0100

    background mobile changed

--- code/src/Header.js
+++ code/src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div className='header-wrapper'>
-            <h1 className='header-text'>Welcome to <span className='friend-match'>Friend Match</span></h1>
+            <h1 className='header-text'>Welcome to <span className='friend-match'>Friend Match!</span></h1>
             <p className='header-paragraph'>Here the friendship starts. Get matched to a new friend and enjoy the new life experience together!</p>
         </div>
     )
