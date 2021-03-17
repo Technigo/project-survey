5519040faeccd6484439d680b502fe8d5edebf88
Frivridna <54737644+Frivridna@users.noreply.github.com>
2021-03-17 16:48:14 +0100

    small change in Summary text + add comments to self

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-
 //change stageArtCategory to be the function??
+//stageArtCategory needs a join()'' ?? javascript ----> check it. It's an array
 const Summary = ({ inhabitants, ageCategory, stageArtCategory, proposal, professions }) => {
   return (
     <article className="summary-question-container">
       <ul className="summary-user-answers" tabIndex="0">
         <li>Size of city: {inhabitants}</li>
         <li>Age: {ageCategory}</li>
-        <li>Post Covid-19 stage art?: {stageArtCategory}</li>
+        <li>Post Covid-19 stage art wishes: {stageArtCategory}</li> 
         <li>Your thoughts: {proposal}</li>
-        <li>Profession: {professions}</li>
+        <li>Your overlapping professions: {professions}</li>
       </ul>
       <button
         type="button"
