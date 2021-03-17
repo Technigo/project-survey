83354ac823da70056e2107eab38e151ad868f9b6
“yennie <yennie.leek@hotmail.se>
2021-03-17 18:09:02 +0100

    added background pic

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -6,9 +6,8 @@ return (
   <div className='header-box'>
     <h1>Ice Ice Baby!</h1>
     <div className='icecream-image'>
-      <img src='./assets/icecream.jpg' alt='ice-creams'></img>
-    </div>
     
+    </div>
   </div>
 )
 }
