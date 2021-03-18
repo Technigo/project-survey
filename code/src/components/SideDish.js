9a4f1ae29722c6197b967d61a66b3ba83b3803dd
esteficodes <estefaniaquevedolusby@gmail.com>
2021-03-18 11:11:28 +0100

    dish2 works

--- code/src/components/SideDish.js
+++ code/src/components/SideDish.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SideDish = () => {
+    const [dish2, setDish2] = useState('');
+
+    const onDish2Change= (e) => {
+        console.dir(e);
+        setDish2(e.target.value);
+    }
 
     return (
         <form>
           <div>
-            <label>And now let's choose you a side dish</label>
-            <select>
+            <label htmlFor="dish2">And now let's choose you a side dish</label>
+            <select onChange={onDish2Change} id="dish2" value={dish2} >
               <option>French fries</option>
               <option>Beans and onion salad</option>
               <option>Leafy greens</option>
@@ -16,4 +22,5 @@ const SideDish = () => {
         </form>
     );
 };
+
 export default SideDish;
