6cd8c7c29176a74f71dde623901a0bd5f63d955d
Ylva Landoff Lindberg <ylvalandofflindberg@Ylvas-MBP-2.lan>
2021-03-18 10:08:06 +0100

    styled respons container

--- code/src/Components/Respons.js
+++ code/src/Components/Respons.js
@@ -8,15 +8,26 @@ import React from "react";
 export const Respons = ( {name, eyeColor, sizeOfNose, smile, hasMustache} ) => {
 
     return (
-        <>
-        <div id="summary"> 
-        <p>Name: {name}</p>
-        <p>Nose: {sizeOfNose}</p>
-        <p>Eyecolor: {eyeColor}</p>
-        <p>Smile: {smile}</p>
-        <p>Has a mustache: {hasMustache}</p>
+     
+        <div id="summary" className="respons-container"> 
+        <div className="respons-border">
+            <div className="face-pics-container">
+                <p>Nose: {sizeOfNose} <img src="/Components/assets/small_nose_2.jpg"/> </p>
+                <p>Eyecolor: {eyeColor}</p>
+                <p>Smile: {smile}</p>
+                <p>Has a mustache: {hasMustache}</p>
+                  
+            </div>
+        <div className="name-container">
+                    <p>{name}</p>
+                    </div>
+        
+       
+        
+       
         </div>
-        </>
+        </div>
+    
     )
 }
 
