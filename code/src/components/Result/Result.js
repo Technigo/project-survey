8e706ea50d00f4af4facb46aea07eebf24d1ba63
Sara Carlstein <carlstein.sara@gmail.com>
2021-03-18 17:00:52 +0100

    new stuff

--- code/src/components/Result/Result.js
+++ code/src/components/Result/Result.js
@@ -4,8 +4,8 @@ import './Result.css'
 const Result = ({snack, GenreGroup, name}) => {
     return (
         <div className="form-result">
-            <h2 className="result-header">Turn your cozy mood on</h2>
-                <p className="result-text">Get your blanket ready and invite <span>{name}</span> for a virtual movie night you can watch a <span>{GenreGroup}</span>, whilst snacking on some <span>{snack}</span></p>
+            <h2 className="result-header">Bring your coziness level up </h2>
+                <p className="result-text">Get your blanket ready and invite <span>{name}</span> for a movie night <span>💝</span>you can watch a <span>{GenreGroup}</span>, whilst snacking on some <span>{snack}</span></p>
                 <img className="robot-image"src="./assets/robot.png"/>
         </div>
     )
