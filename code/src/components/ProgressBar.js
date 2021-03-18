92f64830412e06f19930ead9def23a9981170554
sirihemsley <siri.hemsley@gmail.com>
2021-03-18 11:50:53 +0100

    Fixed the progress bar and added a animation effect

--- code/src/components/ProgressBar.js
+++ code/src/components/ProgressBar.js
@@ -3,12 +3,12 @@ import React from 'react'
 const ProgressBar = ( {section} ) => {
 
   let progress = section * 20
-  let percentage = progress / 100
-
+  let width = ( progress / 100 ) * 200
+  
   return (
     <div className="progress-container">
     <div className="progress-bar">
-      <div style={{width: `${percentage * 200}}px`}} className="progress-fill">
+      <div style={{width: `${width}px`}} className="progress-fill">
         <p>{progress} %</p>
       </div>
     </div>
