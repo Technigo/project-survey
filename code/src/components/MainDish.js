201bf419629c6084fa6216f7820b70b1d464f0e9
esteficodes <estefaniaquevedolusby@gmail.com>
2021-03-18 11:04:04 +0100

    main dish options works

--- code/src/components/MainDish.js
+++ code/src/components/MainDish.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const MainDish = () => {
+    const [dish, setDish] = useState('');
+
+    const onDishChange= (e) => {
+        console.dir(e);
+        setDish(e.target.value);
+    }
 
     return (
         <form>
           <div>
-            <label>Select your favorite main dish</label>
-            <select>
+            <label htmlFor="dish">Select your favorite main dish</label>
+            <select onChange={onDishChange} id="dish" value={dish} >
+            <option disabled>Pick one</option>
               <option>Pasta</option>
               <option>Meat</option>
               <option>Seitan</option>
--- code/src/components/Welcome.js
+++ code/src/components/Welcome.js
@@ -21,7 +21,7 @@ const Welcome = () => {
              value={name}
              onChange={onNameChange}
             />
-          </div>
+        </div>
         </>
     );
 };
