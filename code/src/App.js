6bf51d8652301b5289f035f8271f4e4f15c62f61
NoemiSalome <noemifrischknecht@macbook-pro.home>
2021-03-18 22:43:20 +0100

    ready to push set up of components

--- code/src/App.js
+++ code/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Input from './components/Input';
 import Radio from './components/Radio';
 import Select from './components/Select';
+import Submit from './components/Submit';
 
 export const App = () => {
 
@@ -11,6 +12,7 @@ return (
       <Input />
       <Radio />
       <Select />
+      <Submit />
     </>
   )
 
