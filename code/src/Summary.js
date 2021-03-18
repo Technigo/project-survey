d57250961a7ccfdc8c2b8c90eef6ce9e38e430df
Laima Duhovnaja <laima@laimas-mbp.mynet>
2021-03-18 14:14:09 +0100

    restart button added

--- code/src/Summary.js
+++ code/src/Summary.js
@@ -7,6 +7,12 @@ const Summary = ( {name, age, location} ) => {
             <h1 className='summary-heading'>Thank you for the information {name}!</h1>
             <p className='summary-paragraph'>You will be soon matched with the person that is {age} years old and lives in {location}. Don't forget to daily check your inbox.</p>
         </div>
+        <button
+            className = 'restart-btn'
+            type="button"
+            onClick={() => window.location.reload()}>
+            Restart
+        </button>
         </>
     )
 }
