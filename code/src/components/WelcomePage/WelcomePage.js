80c1b1c35d7657d355542122cda0e3911b066e70
Sara Carlstein <carlstein.sara@gmail.com>
2021-03-19 13:13:45 +0100

    styling

--- code/src/components/WelcomePage/WelcomePage.js
+++ code/src/components/WelcomePage/WelcomePage.js
@@ -4,7 +4,7 @@ import './WelcomePage.css'
 const WelcomePage = () =>{
     return(
         <div className="form-welcome">
-            <h2 className="welcome-heading">Your Movie night picker</h2>
+            <h2 className="welcome-heading">Your Movie night robot</h2>
                 <p>Get your cozy mood on and find out what you will be doing tonight</p>
         </div>
     )
