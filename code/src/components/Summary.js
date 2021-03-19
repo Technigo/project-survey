4be45d3ddff7464df6ea22684dc0ff558891257d
jess-stack <72085164+jess-stack@users.noreply.github.com>
2021-03-19 11:04:35 +0100

    Some style and size changes

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -7,10 +7,10 @@ export const Summary = ({name, country, travelType, satisfaction, amenities}) =>
   return (
     <section className="summary-container">
       <div class="circle"></div>
-      <h1 className="summary-title">{name},</h1>
+      <h1 className="summary-title">Thank you {name}!</h1>
       <h2 className="summary-text"> So you <i class="fas fa-plane"></i> all the way from {country} for {travelType.toLowerCase()}. {satisfaction >= 5 ? "So glad to hear that you are satisfied with your stay and that" : "So sad to hear that you're not satisfied with your stay but at least good that "} you enjoyed our {amenities.toLowerCase()}!
       </h2>
-      <button type="button" class="button" onClick={() => window.location.reload()}>SUBMIT A NEW FORM</button>
+      <button type="button" class="button restart-button" onClick={() => window.location.reload()}>SUBMIT A NEW FORM</button>
     </section>
   )
 }
