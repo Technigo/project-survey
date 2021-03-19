8e66faf966103342b32b8e3c2398614b838b8de8
“[ida <[ida.t.sylven@gmail.com]>
2021-03-19 17:14:28 +0100

    Styling of header section

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
+import './Header.css'
+
 const Header = () => {
   return (
 
   <header className="header">
-    <h1 className="main-heading" tabIndex="0">STOCKHOLM LIVE MUSIC SURVEY
+    <h1 className="stockholm-heading" tabIndex="0">
+      STOCKHOLM 
     </h1>
-      <h2 tabIndex="0"> Hey there gig goer! 
+    <h1 className="live-music-heading" tabIndex="0">
+    LIVE MUSIC
+    </h1>
+    <h1 className="survey-heading" tabIndex="0">
+    SURVEY
+    </h1>
+      <h2 className="greeting" tabIndex="0"> Hey there music lover! 👋 
       </h2>
-      <h3 tabIndex="0">Yes, we all miss the live music shenanigans like crazy. In the meantime, 
+      <h3 className="survey-info" tabIndex="0">Yes, we all miss the live gig shenanigans like crazy. In the meantime, 
       let´s take the opportunity to evalute your Stockholm live music scene preferences!
       </h3> 
   </header>
