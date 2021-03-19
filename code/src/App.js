38dfb3c6d6cb2cbf193714fbecc244747430c318
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-03-19 20:32:08 +0100

    Removed header warning

--- code/src/App.js
+++ code/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
 
-
-import Header from './components/Header'
 import Form from './components/Form'
 
 
 export const App = () => {
   return (
     <div className="content-wrapper">
-      {/* <Header/> */}
       <div className="image-container"> 
         <img src="./assets/picture.svg" className="image" alt="Beach and sunshine"/>
         <img src="./assets/picture1.svg" className="image" alt="Beach and sunshine"/>
