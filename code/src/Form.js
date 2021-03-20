c7c060a4229d103dda7b4dfb3fec315794506ad7
unknown <lindgren.anna.m@gmail.com>
2021-03-20 20:17:26 +0100

    added if condition to show form or summary and removed unused func

--- code/src/Form.js
+++ code/src/Form.js
@@ -8,6 +8,7 @@ import {FrequencyRadioOption } from './components/FrequencyRadioOption'
 import { SubmitButton } from './components/SubmitButton'
 import { Summary }  from './components/Summary'
 
+//Array 
 const frequencies = ['Daily', 'Weekly', 'Monthly', 'Never']
 
 export const Form = () => {
@@ -17,65 +18,50 @@ export const Form = () => {
     const [frequency, setFrequency] = useState('')
     const [showSummary, setShowSummary] = useState(false)
 
-    //Function to prevent the page to rerender itself
+    //Function to prevent the page to rerender itself and as soon as button clicked the Summary should show.
     const handleSubmit = (e) => {
         e.preventDefault()
         setShowSummary(true)
     }
 
-    const isFormComplete = () => {
-
-        //Fail fast way
-        //If "this" is not a valid value, return false
-        if (name === ''){
-            return false
-        }
-        if (brand === ''){
-            return false
-        }
-        if (frequency === ''){
-            return false
-        }
-        return true
+    //if showSummary is false - show Form. Else 
+    if(!showSummary){
+        return(
+            <>
+                <form onSubmit={handleSubmit}>
+                    <FormHeading 
+                        formTitle='We are all ears.'
+                        formText='We want to know what your sneaker deal is. Are you yay or nay? Take this 5 minute survey to help us understand your needs. You can tell us how you really feel!'
+                    />
+                    <NameInput 
+                        name={name} 
+                        setName={setName}
+                    />
+                    <BrandSelectOption 
+                            brand={brand} 
+                            setBrand={setBrand} 
+                    />
+                    <div>
+                        <p>How Often Do You Wear Sneakers?</p>
+                        {
+                            frequencies.map((frequency) => {
+                                return <FrequencyRadioOption
+                                        key={frequency}
+                                        frequencyValue={frequency} 
+                                        setFrequency={setFrequency}
+                                        />
+                            })
+                        }
+                    </div>
+                    <SubmitButton handleSubmit={handleSubmit} />
+                </form>
+            </>
+        )
+    } else {
+        return (
+        <Summary name={name} brand={brand} frequency={frequency} />
+        )
     }
-
-    if (isFormComplete()) {
-        console.log('this is true')
-    }
-
-    return(
-        
-        <>
-            <form onSubmit={handleSubmit}>
-                <FormHeading 
-                    formTitle='We are all ears.'
-                    formText='We want to know what your sneaker deal is. Are you yay or nay? Take this 5 minute survey to help us understand your needs. You can tell us how you really feel!'
-                />
-                <NameInput 
-                    name={name} 
-                    setName={setName}
-                />
-                <BrandSelectOption 
-                        brand={brand} 
-                        setBrand={setBrand} 
-                />
-                <div>
-                    <p>How Often Do You Wear Sneakers?</p>
-                    {
-                        frequencies.map((frequency) => {
-                            return <FrequencyRadioOption
-                                    key={frequency}
-                                    frequencyValue={frequency} 
-                                    setFrequency={setFrequency}
-                                    />
-                        })
-                    }
-                </div>
-                <SubmitButton />
-            </form>
-            <Summary name={name} brand={brand} frequency={frequency}/>
-        </>
-    )
 }
 
 
