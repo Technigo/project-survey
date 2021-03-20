a3b6da425d032da40e0f7457cc7cb3bbeda8d0f9
Anna Honkonen <ak.honkonen@gmail.com>
2021-03-21 00:07:42 +0200

    added company logo and styled buttons and changed colors.

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,7 +1,15 @@
 import React from "react";
+import companyLogo from "../images/punttis.jpg";
 
 const Header = () => {
-  return <h1 className="header-title">WEEK CHALLENGE REGISTRATION</h1>;
+  return (
+    <>
+      <div className="header">
+        <img src={companyLogo} />
+        <h1 className="header-title">WEEK CHALLENGE REGISTRATION</h1>
+      </div>
+    </>
+  );
 };
 
 export default Header;
