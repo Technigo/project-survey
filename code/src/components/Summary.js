af6285e3258a140402508777c1a0d5c815c6c7d0
Marielle Ohlsson <ohlssonmarielle@gmail.com>
2021-03-21 19:09:00 +0100

    fixed misspelling

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -10,7 +10,7 @@ const Summary = (props) => {
                 <p>The latest book you read was: {props.bookname}</p>
                 <p>Wow! After you had finished {props.bookname}, you felt {props.feeling}!</p>
                 <p>This is how many times you have read the book: {props.numberOfBook}</p>
-                <p>If someone would ask you if they should read {props.bookname}, your answer will be: {props.recomendation}!</p>
+                <p>If someone would ask you if they should read {props.bookname}, your answer will be: {props.recommendation}!</p>
                 <p>Thank you for your time and have a great day!</p>
             </section>
         </div>
