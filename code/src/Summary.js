a4506128650da2352e6898fe1ab22208253bf106
Åsa Hildebrand <asahildebrand@yahoo.com>
2021-03-21 10:22:15 +0100

    Changed background

--- code/src/Summary.js
+++ code/src/Summary.js
@@ -4,13 +4,16 @@ export const Summary = ({ name, checked, value, email }) => {
   
   return (
     <section className="summary-section">
-      <div className="thankyou">
+      <div className="greet">
         <h1 tabIndex="0">Thank you for your time! We will send an e-mail to {email} if you are the lucky one to win!</h1>
         <h2 tabIndex="0">Summary of your answers:</h2>
+      </div>
+      <div className="summary">
+        
         <p tabIndex="0">Your favorite cat is {checked} and {value}.</p>
         <p tabIndex="0">Your favorite cat name ever is {name}!</p>
       </div>
-      <div className="summary">
+      <div className="ending">
       <p>We couldn't agree with you more!</p>
       </div>
     </section>
