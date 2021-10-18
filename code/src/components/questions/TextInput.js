c0e89beb1546c50d1edb863475c9f8d46d8897c0
Gustav Frid <gustav.frid@gmail.com>
2021-10-18 20:24:02 +0200

    code cleanup in text input

--- code/src/components/questions/TextInput.js
+++ code/src/components/questions/TextInput.js
@@ -3,35 +3,28 @@ import React, { useState } from 'react'
 export const TextInput = props => {
   const { title, fields } = props.data
   const [state, setState] = useState({})
-  const testObject = { ...state }
-  console.log(testObject)
 
-  const handleChange = event => {
-    console.log(event.target)
-    console.log('first log: ')
-    console.log('the tag id:', event.target.id, ',the value:', event.target.value)
-    // setState(event.target.value)
-    setState({ ...state, [event.target.id]: event.target.value })
-    console.log('new state:', state)
+  const handleChange = e => {
+    const { id, value } = e.target
+    setState({ ...state, [id]: value })
   }
-  console.log('redering textinput component')
+
   return (
     <>
       <h2>{title}</h2>
       {fields.map(field => {
+        const { id, label, placeholder } = field
         return (
-          <label key={field.id}>
-            {field.label}
+          <label key={id}>
+            {label}
             <input
               type='text'
-              id={field.id}
-              placeholder={field.placeholder}
-              value={state[field.id]}
-              // [field.label]
+              id={id}
+              placeholder={placeholder}
+              value={state[id]}
               onChange={handleChange}
             />
-            <p>Input:{state[field.id]}</p>
-            {/* [field.label] */}
+            <p>Input:{state[id]}</p>
           </label>
         )
       })}
