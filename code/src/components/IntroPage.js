6af8f29f8eb358cb043d7aedf000fce47a066a2e
Anna Dimitrakopoulos <dimitrakopoulos.anna@gmail.com>
2021-10-18 16:09:06 +0200

    started styling, added dropdown form

--- code/src/components/IntroPage.js
+++ code/src/components/IntroPage.js
@@ -4,7 +4,7 @@ const IntroPage = ({ onStepChange }) => {
     return (
         <div>
             <h1 className="welcome-message">Welcome to my survey!</h1>
-            <button type="button" onClick={onStepChange}>Start Survey!</button>
+            <button className="start-survey-button" type="button" onClick={onStepChange}>Start Survey!</button>
         </div>
     )
 
--- code/src/components/QuestionTwo.js
+++ code/src/components/QuestionTwo.js
@@ -4,9 +4,11 @@ const QuestionTwo = ({ questionTwoInput, onQuestionTwoInputChange, onStepChange 
     return (
         <div>
             <form>
-                <label htmlFor="questionTwoInput">Is cereal soup?</label>
-                <input id="questionTwoInput" type="text" value={questionTwoInput} onChange={onQuestionTwoInputChange}>
-                </input>
+                <select onChange={onQuestionTwoInputChange} value={questionTwoInput}>
+                    <option value=''>Is cereal soup?</option>
+                    <option value='yes'>Of course it is!</option>
+                    <option value='no'>Absolutely not!</option>
+                </select>
                 <button onClick={onStepChange}>Next question!</button>
             </form>
         </div>
