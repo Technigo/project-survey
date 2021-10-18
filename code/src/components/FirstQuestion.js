fc46cd1b3c26efc555204421dfb77bc350fa431b
jakobxlindstrom <jake@lindstroem.se>
2021-10-18 22:30:45 +0200

    initial commit

---
+++ code/src/components/FirstQuestion.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const FirstQuestion = () => {
+  return <div></div>
+}
+
+export default FirstQuestion
---
+++ code/src/components/SecondQuestion.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SecondQuestion = () => {
+  return <div></div>
+}
+
+export default SecondQuestion
