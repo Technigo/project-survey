199f9bfe88e3e0e146501bd891fc5ae3b3d7f120
Gustav Frid <gustav.frid@gmail.com>
2021-10-21 14:30:17 +0200

    Added questions in data

--- code/src/components/Button.js
+++ code/src/components/Button.js
@@ -3,10 +3,19 @@ import DownArrow from 'assets/icons/down-arrow.svg'
 import UpArrow from 'assets/icons/up-arrow.svg'
 import 'components/button.css'
 
-export const Button = ({ disabled, onClick, text, type }) => (
-  <button className={'button'} disabled={disabled} onClick={onClick}>
-    {type === 'down' && <img className='button-icon' src={DownArrow} alt='down-arrow' />}
-    {type === 'up' && <img className='button-icon' src={UpArrow} alt='up-arrow' />}
-    {text}
-  </button>
-)
+export const Button = ({ disabled, onClick, text, type }) => {
+  const handleKeyDown = e => {
+    if (type === 'down' && (e.key === 'Enter' || e.keyCode === '39')) {
+      onClick()
+    }
+  }
+  return (
+    <button className={'button'} disabled={disabled} onClick={onClick}>
+      {type === 'down' && (
+        <img className='button-icon' src={DownArrow} alt='down-arrow' onKeyDown={handleKeyDown} />
+      )}
+      {type === 'up' && <img className='button-icon' src={UpArrow} alt='up-arrow' />}
+      {text}
+    </button>
+  )
+}
