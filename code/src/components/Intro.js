435f71117019899da5c135aec4ce0660f0b9d23b
Bruna Santos Araujo <brunasantosaraujo@Brunas-Air.lan>
2021-10-21 12:03:49 +0200

    alt on the images

--- code/src/components/Intro.js
+++ code/src/components/Intro.js
@@ -3,7 +3,7 @@ import React from "react";
 const Intro = ({ onStepChange }) => {
   return (
     <div>
-      <img src="./pictures/eat.jpg" />
+      <img src="./pictures/eat.jpg" alt="neon sign saying eat" />
       <p>
         Please answer the following questions regarding restaurants in Sweden
       </p>
