8d90e12d6aee8951d77f2dba51eaa7b5ef96f5d4
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2021-10-21 14:49:25 +0200

    Resized hero-image

--- code/src/components/Slider.js
+++ code/src/components/Slider.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import 'emoji-slider';
 
 
 const Slider = ({ happiness, setHappiness }) => {
 return (
     <>
     <p className="quote"><strong>"Rememeber! A balanced diet is a cookie in each hand!"</strong></p>
-<emoji-slider emoji="🍪" onChange={(event) => setHappiness(event)}></emoji-slider>
+{/* <emoji-slider emoji="🍪" onChange={(event) => setHappiness(event)}></emoji-slider> */}
 </>
 
 );
