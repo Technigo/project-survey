6aba3d41750cc7de5c898f77d059fc9931cd1d8f
silvertejp89 <kriss.stockhaus@gmail.com>
2021-10-21 16:51:34 +0200

    Changed "if" in form

---
+++ code/src/components/Form.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import FirstQuestion from "./FirstQuestion";
+import SecondQuestion from "./SecondQuestion";
+import Overview from "./Overview";
+
+const Form = () => {
+  const [nameInput, setNameInput] = useState("");
+  const [surnameInput, setSurnameInput] = useState("");
+  const [step, setStep] = useState(1);
+
+  const onNameInputChange = (event) => {
+    setNameInput(event.target.value);
+  };
+
+  const onSurnameInputChange = (event) => {
+    setSurnameInput(event.target.value);
+  };
+
+  const onStepChange = () => {
+    setStep(step + 1);
+  };
+
+  return (
+    <div>
+      {step === 1 && (
+        <FirstQuestion
+          nameInput={nameInput}
+          onNameInputChange={onNameInputChange}
+          onStepChange={onStepChange}
+        />
+      )}
+      {step === 2 && (
+        <SecondQuestion
+          surname={surnameInput}
+          onSurnameInputChange={onSurnameInputChange}
+          onStepChange={onStepChange}
+        />
+      )}
+      {step === 3 && (
+        <Overview nameInput={nameInput} surnameInput={surnameInput} />
+      )}
+    </div>
+  );
+};
+
+export default Form;
