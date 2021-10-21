0f8d9b2c1c036fb57d92edacb3ef8a7415ae38b9
Maria Thomasson <81348201+MariaThomasson@users.noreply.github.com>
2021-10-21 19:21:23 +0200

    Bug fix 2.0

--- code/src/App.js
+++ code/src/App.js
@@ -1,22 +1,21 @@
 
 import React, { useState } from 'react'
-import { Summary } from "./Summary";
+import { Summary } from './Summary';
 import './app.css'
 
-
 export const App = () => {
-  const [drink, setDrink] = useState("");
-  const [snack, setSnack] = useState("");
-  const [party, setParty] = useState("");
+  const [drink, setDrink] = useState('');
+  const [snack, setSnack] = useState('');
+  const [party, setParty] = useState('');
   const [showSummary, setShowSummary] = useState(false);
 
-  const partyOptions = ["Yes", "No", "Maybe"]
+  const partyOptions = ['Yes', 'No', 'Maybe']
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     setShowSummary(true);
   };
- 
+
   return (
     <main>
       {!showSummary ? (
@@ -24,59 +23,56 @@ export const App = () => {
           <header id="header">
             <h3>Glögg</h3>
             <h1>What´s your favorite?</h1>
-            <a href="#drink"><img className="glogg" src="../assets/gloggdown.png" alt="glogg"></img></a>
+            <a href="#drink"><img className="glogg" src="../assets/gloggdown.png" alt="glogg" /></a>
           </header>
           <section className="section-content drink" id="drink">
             <h2>How do you like your glögg?</h2>
             <select
-              onChange={event => setDrink(event.target.value)}
+              onChange={(event) => setDrink(event.target.value)}
               value={drink}
-              required
-            >
+              required>
               <option value="">Select answer</option>
               <option value="to drink it strong">With alcohol</option>
               <option value="to have a weak one">Without alcohol</option>
               <option value="to not drink glögg, you drink svagdricka?">Just Saft</option>
             </select>
-            <button><a href="#snack">Next</a></button>
+            <button type="button"><a href="#snack">Next</a></button>
           </section>
 
           <section className="section-content snack" id="snack">
             <h2>What do you add in your glögg?</h2>
             <h4>For example: Raisins, almond, cinnamon?</h4>
-            <input className="snack-input"
+            <input
+              className="snack-input"
               type="text"
-              onChange={event => setSnack(event.target.value)}
+              onChange={(event) => setSnack(event.target.value)}
               value={snack}
-              required
-            />
-            <button><a href="#party">Next</a></button>
+              required />
+            <button type="button"><a href="#party">Next</a></button>
           </section>
 
           <section className="section-content party" id="party">
             <h2>Are you having a glöggmingel?</h2>
             <div className="radio-buttons">
-              {partyOptions.map(group => (
-                <label key={group}>
+              {partyOptions.map((group) => (
+                <label htmlFor="text" key={group}>
                   <input
                     type="radio"
                     value={group}
                     className="radio-button"
                     required
-                    onChange={event => setParty(event.target.value)}
-                    checked={party === group}
-                  />
+                    onChange={(event) => setParty(event.target.value)}
+                    checked={party === group} />
                   {group}
                 </label>
               ))}
             </div>
-            <button>Submit</button>
+            <button type="submit">Submit</button>
           </section>
         </form>
       ) : (
-          <Summary drink={drink} snack={snack} party={party} />
-        )
-      }
-    </main >
+        <Summary drink={drink} snack={snack} party={party} />
+      )}
+    </main>
   )
 }
--- code/src/Summary.js
+++ code/src/Summary.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './summary.css'
 
-
 export const Summary = (props) => {
   const { snack } = props
   const { drink } = props
@@ -10,12 +9,12 @@ export const Summary = (props) => {
   return (
     <section className="summary-section">
       <h2>Summary of your favorite glögg</h2>
-      <p>You like {drink} and you add {snack}. 
-      {party === "Yes" && <p>If you are having a glöggmingel, you need to invite me, what time should I come?</p>}
-      {party === "No" && <p>No glöggmingel? You can come to mine.</p>}
-      {party === "Maybe" && <p>Maybe, maybe not a glöggmingel.. You need to drink some Glögg and think about it.</p>}
+      <p>You like {drink} and you add {snack}.
+        {party === 'Yes' && <p>If you are having a glöggmingel, you need to invite me, what time should I come?</p>}
+        {party === 'No' && <p>No glöggmingel? You can come to mine.</p>}
+        {party === 'Maybe' && <p>Maybe, maybe not a glöggmingel.. You need to drink some Glögg and think about it.</p>}
       </p>
-      <button><a href="return">Return</a></button>
+      <button type="button"><a href="return">Return</a></button>
     </section>
   )
 }
