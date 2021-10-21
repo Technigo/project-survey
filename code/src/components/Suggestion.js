89d1b39bb1afdce4211b55f539fd6f83f4ef4df3
Lovisa Carling <lovisa.carling@gmail.com>
2021-10-21 11:36:49 +0200

    changed suggestion statements

--- code/src/components/Suggestion.js
+++ code/src/components/Suggestion.js
@@ -5,114 +5,25 @@ import { Strategy } from "./Strategy"
 import { Survival } from "./Survival"
 
 export const Suggestion = ({moodInput, socialInput, viewInput}) => {
+
     return (
-    <>
     <div className="suggestion-container">
     <h2>You should play:</h2>
-    
-    {moodInput === 'Chill' && socialInput === 'Forever alone' && viewInput === 'From my own perspective' &&
-    (<div>
-        <FirstPersonShooter/>
-    </div>)}
 
-    {moodInput === 'Chill' && socialInput === 'A few peeps is enough' && viewInput === 'From my own perspective' &&
-    (<div>
-        <FirstPersonShooter/>
-    </div>)}
+    {viewInput === 'From my own perspective' && (
+        (moodInput === 'Chill' || moodInput === 'Overthinking') ? <FirstPersonShooter/> : <Survival/>
+    )}
 
-    {moodInput === 'Chill' && socialInput === 'Love to be in a group' && viewInput === 'From my own perspective' &&
-    (<div>
-        <FirstPersonShooter/>
-    </div>)}
-
-    {moodInput === 'Hyped' && socialInput === 'Forever alone' && viewInput === 'From my own perspective' &&
-    (<div>
-        <Survival/>
-    </div>)}
-
-    {moodInput === 'Hyped' && socialInput === 'A few peeps is enough' && viewInput === 'From my own perspective' &&
-    (<div>
-        <Survival/>
-    </div>)}
-
-    {moodInput === 'Hyped' && socialInput === 'Love to be in a group' && viewInput === 'From my own perspective' &&
-    (<div>
-        <Survival/>
-    </div>)}
-
-    {moodInput === 'Overthinking' && socialInput === 'Forever alone' && viewInput === 'From my own perspective' &&
-    (<div>
-        <FirstPersonShooter/>
-    </div>)}
-
-    {moodInput === 'Overthinking' && socialInput === 'A few peeps is enough' && viewInput === 'From my own perspective' &&
-    (<div>
-        <FirstPersonShooter/>
-    </div>)}
-
-    {moodInput === 'Overthinking' && socialInput === 'Love to be in a group' && viewInput === 'From my own perspective' &&
-    (<div>
-        <FirstPersonShooter/>
-    </div>)}
-
-    {moodInput === 'Overthinking' && socialInput === 'Forever alone' && viewInput === 'Watching from the outside' &&
-    (<div>
+    {viewInput === 'Watching from the outside' && (
         <Strategy/>
-    </div>)}
+    )}
 
-    {moodInput === 'Overthinking' && socialInput === 'Love to be in a group' && viewInput === 'Watching from the outside' &&
-    (<div>
-        <Strategy/>
-    </div>)}
-
-    {moodInput === 'Overthinking' && socialInput === 'A few peeps is enough' && viewInput === 'Watching from the outside' &&
-    (<div>
-        <Strategy/>
-    </div>)}
-
-    {moodInput === 'Chill' && socialInput === 'A few peeps is enough' && viewInput === 'Watching from the outside' &&
-    (<div>
-        <Strategy/>
-    </div>)}
-
-    {moodInput === 'Chill' && socialInput === 'Forever alone' && viewInput === 'Watching from the outside' &&
-    (<div>
-        <Strategy/>
-    </div>)}
-
-    {moodInput === 'Hyped' && socialInput === 'A few peeps is enough' && viewInput === 'Watching from the outside' &&
-    (<div>
-        <Strategy/>
-    </div>)}
-
-    {moodInput === 'Hyped' && socialInput === 'Love to be in a group' && viewInput === 'Watching from the outside' &&
-    (<div>
-        <Strategy/>
-    </div>)}
-
-    {moodInput === 'Hyped' && socialInput === 'Love to be in a group' && viewInput === 'By controlling others' &&
-    (<div>
+    {viewInput === 'By controlling others' && (
         <Moba/>
-    </div>)}
-    
-    {moodInput === 'Chill' && socialInput === 'A few peeps is enough' && viewInput === 'By controlling others' &&
-    (<div>
-        <Moba/>
-    </div>)}
-
-    {moodInput === 'Hyped' && socialInput === 'A few peeps is enough' && viewInput === 'By controlling others' &&
-    (<div>
-        <Moba/>
-    </div>)}
-
-    {moodInput === '' && socialInput === '' && viewInput === '' &&
-    (<div>
-      Hello
-    </div>)}
-
+    )}
     </div>
-    </>)
-}
+)}
+
         
     
 
