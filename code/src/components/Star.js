0cd8ed4274574f6753845996727226ba29499955
Helena Wiklund <helena.wiklund@live.se>
2021-10-22 14:55:42 +0200

    sizee

--- code/src/components/Star.js
+++ code/src/components/Star.js
@@ -15,8 +15,8 @@ const Star = (props) => {
         onClick={changeGrade}
       />
       <svg
-        width="35"
-        height="35"
+        width="40"
+        height="40"
         viewBox="0 0 24 24"
         fill="none"
         stroke="#393939"
