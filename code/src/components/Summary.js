889c68b29e706179416326b5e0edb5e1d39d03ab
Elin Elmvik Diczfalusy <elinelmvikdiczfalusy@Elins-iMac.local>
2021-10-22 10:25:47 +0200

    added background-color behind text and updated picture

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -1,20 +1,23 @@
 import React from "react";
+import "./Summary.css";
 
 const Summary = ({ nameInput, interestInput, ageInput, moodInput }) => {
   return (
     <>
       <h2 className="page-header">Summary</h2>
-      <p>
-        Welcome {nameInput} to be exactly what you feel like. Life with kids and
-        coding is not always the best combination
-      </p>
-      <p>
-        It is nice that you dont hate {interestInput} as much as the other, way
-        to go on looking on the bright side of life.{" "}
-      </p>
-      <p>
-        Take your hour to {ageInput} and {moodInput}
-      </p>
+      <article className="summary-text">
+        <p>
+          Welcome {nameInput} to be exactly what you feel like. Life with kids
+          and coding is not always the best combination
+        </p>
+        <p>
+          It is nice that you dont hate {interestInput} as much as the other,
+          way to go on looking on the bright side of life.{" "}
+        </p>
+        <p>
+          Take your hour to {ageInput} and {moodInput}
+        </p>
+      </article>
 
       <button
         onClick={() => window.location.reload(false)}
