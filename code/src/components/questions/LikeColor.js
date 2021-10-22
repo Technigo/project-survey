50fd2df0035cd4b634e03346da9f24e4aca0f03d
Birgit Nehrwein <nehrwein@posteo.de>
2021-10-22 18:54:54 +0200

    added Read.me

--- code/src/components/questions/LikeColor.js
+++ code/src/components/questions/LikeColor.js
@@ -29,7 +29,7 @@ const likeColor = ({
       
       <button 
         disabled={color === ''}
-        onClick={onStepChange}>Next question <span><b>>></b></span>
+        onClick={onStepChange}>Next question <span><b>'>>'</b></span>
       </button>
     </form>
   )
