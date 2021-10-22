3eb819682d8d9ab32270a29e40df8ecd68d823ff
Harry Bäcklin <83465217+Groovedharry@users.noreply.github.com>
2021-10-22 18:58:17 +0200

    the last things

--- code/src/components/Overview.js
+++ code/src/components/Overview.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './overview.css'
 const Overview = ({ nameInput, surnameInput, weakness, strength }) => {
     return(
-        <section className="summary">
+        <form className="summary">
         <div class="underline-values">
         <h1 className="values">My values from Form</h1>
         </div>
@@ -10,7 +10,10 @@ const Overview = ({ nameInput, surnameInput, weakness, strength }) => {
         <p className="surname"><span className="highlightme">Surname</span> : {surnameInput}</p>
         <p className="your-weakness">You believe your <span className="highlightme">weakness</span> is {weakness}</p>
         <p className="your-strength">You believe your <span className="highlightme">strength</span> is {strength} </p>
-        </section>
+        <div className="encourage">
+        <h3>Don't let this discourage you, work on your weaknesses and perfect your strengths, we have a long road ahead of us and we will all reach our goal togheter!</h3>
+        </div>
+        </form>
 
     );
 };
