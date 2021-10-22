1a4ce923a60a5abb96787c03be3f3ce36c07803e
camekman <ekman.camilla@gmail.com>
2021-10-22 11:15:52 +0200

    added more conditional styling in overview

--- code/src/components/Overview.js
+++ code/src/components/Overview.js
@@ -12,21 +12,49 @@ const Overview = ({
     <section className="summary-container">
       <h2 className="summary-header">Summary</h2>
       <div className="summary-text">
-        <p>Name: {nameInput}</p>
-        <p>Surname: {surnameInput}</p>
-        <p>Age: {ageGroup}</p>
         <p>
-          Do you believe that life exists outside of this planet?{" "}
-          {experienceInput}%
+          Your name:
+          <p style={{ color: "rgb(226, 224, 224)" }}>{nameInput}</p>
         </p>
-        <p>Have you ever experienced something out of this world? </p>
-        <p
-          style={{
-            color:
-              believesInput === "Yes" ? "blueviolet" : "rgb(226, 224, 224)",
-          }}
-        >
-          {believesInput}
+        <p>
+          Your surname:
+          <p style={{ color: "rgb(226, 224, 224)" }}>{surnameInput}</p>
+        </p>
+        <p>
+          Your age:
+          <p
+            style={{
+              color: ageGroup === "30+" ? "darkviolet" : "rgb(226, 224, 224)",
+            }}
+          >
+            {ageGroup}
+          </p>
+        </p>
+        <p>
+          Do you believe that life exists outside of this planet?
+          <p
+            style={{
+              color:
+                experienceInput === "100"
+                  ? "cornflowerblue"
+                  : "rgb(226, 224, 224)",
+            }}
+          >
+            {experienceInput}%
+          </p>
+        </p>
+        <p>
+          Have you ever experienced something out of this world?
+          <p
+            style={{
+              color:
+                believesInput === "Yes"
+                  ? "cornflowerblue"
+                  : "rgb(226, 224, 224)",
+            }}
+          >
+            {believesInput}
+          </p>
         </p>
       </div>
     </section>
