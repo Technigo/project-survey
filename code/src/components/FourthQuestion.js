f2b53e711e857421716dacfdf566dc4b46e6731d
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2021-10-22 08:45:56 +0200

    stylind added for most element on mobile

--- code/src/components/FourthQuestion.js
+++ code/src/components/FourthQuestion.js
@@ -12,25 +12,29 @@ export const FourthQuestion = ({
       <form onSubmit={onStepChange} className="content-container">
         <h2> Är du nöjd med utdeling av din post?</h2>
         <div className="options-spacing">
-          <label htmlFor="SatisfactionYes" className="margin">
+          <label htmlFor="SatisfactionYes" className="radio-button-container">
             Ja
             <input
               id="SatisfactionYes"
               type="radio"
               value="Ja"
+              name="radio"
               onChange={onSatisfactionInputChange}
               checked={satisfactionInput === "Ja"}
             />
+            <span className="radio-button"></span>
           </label>
-          <label htmlFor="SatisfactionNo" className="margin">
+          <label htmlFor="SatisfactionNo" className="radio-button-container">
             Nej
             <input
               id="SatisfactionNo"
               type="radio"
               value="Nej"
+              name="radio"
               onChange={onSatisfactionInputChange}
               checked={satisfactionInput === "Nej"}
             />
+            <span className="radio-button"></span>
           </label>
         </div>
         <button type="submit">Nästa</button>
--- code/src/components/WelcomePage.js
+++ code/src/components/WelcomePage.js
@@ -3,20 +3,21 @@ import Header from "./Header";
 
 const WelcomePage = ({ onStepChange }) => {
   return (
-    <div className="content-container">
+    <>
       <Header />
-
-      <section className="welcome-container">
-        <h2>Välkommen till Citymails kundundersökning</h2>
-        <p>
-          Vi jobbar ständigt för att förbättra oss och skickar därför ut en
-          enkät för att ta reda på hur vi kan jobba mot nöjdare kunder. Enkäten
-          tar ungefär tre minuter att svara på och svaren kan bidra till en
-          bättre utdelning.
-        </p>
-      </section>
-      <button onClick={onStepChange}>Starta</button>
-    </div>
+      <div className="content-container">
+        <section className="welcome-container">
+          <h2>Välkommen till Citymails kundundersökning</h2>
+          <p>
+            Vi jobbar ständigt för att förbättra oss och skickar därför ut en
+            enkät för att ta reda på hur vi kan jobba mot nöjdare kunder.
+            Enkäten tar ungefär tre minuter att svara på och svaren kan bidra
+            till en bättre utdelning.
+          </p>
+        </section>
+        <button onClick={onStepChange}>Starta</button>
+      </div>
+    </>
   );
 };
 
