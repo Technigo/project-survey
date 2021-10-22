f4cfa442a733d6c23a3afa7b8cb9c4be1f2177ad
Darya <darya.lapata@gmail.com>
2021-10-22 09:09:10 +0200

    change text overview

--- code/src/components/Overview.js
+++ code/src/components/Overview.js
@@ -10,9 +10,9 @@ const Overview = ({ nameInput, countryInput, ageInput, emailInput, moodInput }) 
 				</div>
 				<div className="speech-bubble">
 					<h4>Thank you for your application, <span className="bold">{nameInput}</span>!</h4>
-					<p>Our office in <span className="bold">{countryInput}</span> will contact you at the specified email address <span className="bold">{emailInput}</span>.
+					<p>Our office in <span className="bold">{countryInput}</span> will contact you at this <span className="bold">{emailInput} adress</span>.
 					Your age between <span className="bold">{ageInput}</span> is common in our games. 
-					While you wait read more about us on our webpage <a href="https://www.netflix.com/se/title/81040344" 
+					While you wait us to reach out to you, read more about us on our webpage <a href="https://www.netflix.com/se/title/81040344" 
 					target="_blank" rel="noopener noreferrer">here</a>.	<br></br><br></br>
 
 					<span className="bold">Best regards,</span><br></br>Pink Soldier</p>
