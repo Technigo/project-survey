86754ecf5011bc2625903137738edba5bc35de2f
MarySnopok <marysnopok8@gmail.com>
2021-10-23 13:48:33 +0200

    ----

--- code/src/components/Confetti.js
+++ code/src/components/Confetti.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useLayoutEffect } from "react";
 import Confetti from "react-confetti";
 import useWindowSize from "react-use/lib/useWindowSize";
 
@@ -8,10 +8,10 @@ export default () => {
   const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(window.innerHeight);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     setWidth(window.innerWidth);
     setHeight(window.innerHeight);
-  });
+  }, []);
 
   console.log("width, height", width, height, window.innerWidth, window.innerHeight);
   return <Confetti width={width} height={height} numberOfPieces={300} />;
