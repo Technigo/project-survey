d6d65e2293c2c0e4d6d06cc97ae8c691c89a78e5
MT-dotse <madelene.trang333@gmail.com>
2021-10-23 22:04:08 +0300

    updated READme

--- code/src/components/IntroPage.js
+++ code/src/components/IntroPage.js
@@ -3,7 +3,9 @@ import React from 'react';
 const IntroPage = ({ onStepChange }) => {
 	return (
 		<div className="welcome-message-container" tabIndex="0" id="first-question">
-			<h1 className="welcome-message">Want to watch a horror movie? </h1>
+			<h1 className="welcome-message">
+				Want to watch a horror movie?....................{' '}
+			</h1>
 
 			<button className="button" type="button" onClick={onStepChange}>
 				YES
