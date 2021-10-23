f33d66398f960761133985657a6a49b82badf897
MarySnopok <marysnopok8@gmail.com>
2021-10-23 13:51:00 +0200

    ----

--- code/src/components/Confetti.js
+++ code/src/components/Confetti.js
@@ -1,6 +1,6 @@
 import React, { useState, useLayoutEffect } from "react";
 import Confetti from "react-confetti";
-import useWindowSize from "react-use/lib/useWindowSize";
+//import useWindowSize from "react-use/lib/useWindowSize";
 
 export default () => {
   //const { width, height } = useWindowSize();
