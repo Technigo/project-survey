f5ddb26b1b5d1cf67e78d491bf3cbc2e3b2071c8
intehon <emelie.soderhielm@gmail.com>
2021-10-26 18:10:17 +0200

    change agespan2

--- code/src/components/SecondQuestion.js
+++ code/src/components/SecondQuestion.js
@@ -15,15 +15,15 @@ const SecondQuestion = ({ ageInput, onAgeChange, onStepChange }) => {
                     <option value="">Select your age</option>
                     <option value="0-12">0-12 yrs</option>
                     <option value="13-18">13-18 yrs</option>
-                    <option value="19-25">19-29 yrs</option>
-                    <option value="26-35">30-39 yrs</option>
-                    <option value="36-45">40-49 yrs</option>
-                    <option value="46-55">50-59 yrs</option>
-                    <option value="56-65">60-69 yrs</option>
-                    <option value="66-75">70-79 yrs</option>
-                    <option value="76-85">80-89 yrs</option>
-                    <option value="86-95">90-99 yrs</option>
-                    <option value="96+">100+ yrs</option>
+                    <option value="19-25">19-25 yrs</option>
+                    <option value="26-35">26-35 yrs</option>
+                    <option value="36-45">36-45 yrs</option>
+                    <option value="46-55">46-55 yrs</option>
+                    <option value="56-65">56-65 yrs</option>
+                    <option value="66-75">66-75 yrs</option>
+                    <option value="76-85">76-85 yrs</option>
+                    <option value="86-95">86-95 yrs</option>
+                    <option value="96+">96+ yrs</option>
             </select>
                 <button
                 className="btn" 
