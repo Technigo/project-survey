f0bcfd3ab1c029dd392182de481609ebd543fc37
esteficodes <estefaniaquevedolusby@gmail.com>
2021-11-01 15:49:11 +0100

    changes to display

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         
       <div className="header-wrapper">
         <h1 tabIndex='0' className='header-text'>Dinner plans generator</h1>
-        <p tabIndex='0' className='header-paragraph'>Hungry but out of ideas? Let's see what you can have for dinner today!</p>   
+        <p tabIndex='0' className='header-paragraph'>Hungry but out of ideas?</p>   
       </div>
     
     );
--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -6,12 +6,9 @@ const Summary = ({name, mainDish, sideDish, condiment} ) => {
   return (
     <>
     <div className="summary">
-    <h1 tabIndex="0" className='summary'>Dinner will be ready soon {name}!</h1>
+    <h1 tabIndex="0" className='summary-title'>Dinner will be ready soon {name}!</h1>
     <img className="dinner-image" src="assets/dinnerimage.jpg" alt="Friends toasting at dinner table" ></img>
-    <p tabIndex="0" className='summary'>You can have {mainDish} with a side of {sideDish}. Don't forget to spice it up a little bit with {condiment} .</p>
-    
-
-    
+    <p tabIndex="0" className='summary-text'>You can have {mainDish} with a side of {sideDish}. Don't forget to spice it up a little bit with {condiment}.</p>
     <p>Enjoy your dinner!</p>
 
     </div>
