a17a9005a89175841f300cbc64a6f520a91be49e
Kara Howes <karahowes@Karas-MacBook-Air.local>
2021-11-07 22:18:25 +0100

    added confetti effect for background

--- code/src/components/Overview.js
+++ code/src/components/Overview.js
@@ -1,41 +1,48 @@
 import React from 'react'
 import AnimalChoices from './AnimalChoices'
 import './overview.css'
+import useWindowSize from 'react-use-window-size'
+import Confetti from 'react-confetti'
 
- export const Overview = ({ nameInput, locationInput, animalInput, colourInput, aeroplaneInput, olderInput, sellotapeInput }) => {
+export const Overview = ({ nameInput, locationInput, animalInput, colourInput, aeroplaneInput, olderInput, sellotapeInput }) => {
+  const { width, height } = useWindowSize()
+  return (
 
-    return (
-      <section className = "overview-all">
-        <fieldset className = "fieldset">
-          <legend className = "legend">The Overview</legend>
-            <form className = "form-container">
-              <section className = "overview">
-                <p> Your name: <span>{ nameInput }</span></p>
-                <p> Your location: <span>{ locationInput }</span></p>
-                <p> Your favourite chosen animal: <span>{ animalInput} </span></p>
-                <p> Your favourite colour: <span class="colour">{ colourInput }</span></p>
-                <p> Can you make an aeroplane out of paper?: <span>{ aeroplaneInput }</span></p>
-                <p> Future profession: <span>{ olderInput }</span></p>
-                <p class="sellotape-overview"> Sellotape solution: <span>{ sellotapeInput }</span></p>
-            </section>
-            </form>
-        
-        </fieldset>
-        
-      
+    <section className="overview-all">
+      <Confetti
+        width={width}
+        height={3000}
+      />
+      <fieldset className="fieldset">
+        <legend className="legend">The Overview</legend>
+        <form className="form-container">
+          <section className="overview">
+            <p> Your name: <span>{nameInput}</span></p>
+            <p> Your location: <span>{locationInput}</span></p>
+            <p> Your favourite chosen animal: <span>{animalInput} </span></p>
+            <p> Your favourite colour: <span class="colour">{colourInput}</span></p>
+            <p> Can you make an aeroplane out of paper?: <span>{aeroplaneInput}</span></p>
+            <p> Future profession: <span>{olderInput}</span></p>
+            <p class="sellotape-overview"> Sellotape solution: <span>{sellotapeInput}</span></p>
+          </section>
+        </form>
 
-        <div className = "animal">
-          <AnimalChoices
-          animalInput = {animalInput}/>
-          </div>  
-        <div className = "thank-you">
-          <h1> THANK YOU!</h1>
-          <p>Thank you so much for taking time to answer our questions! We always have so many
-            questions and it is great that you answered some for us. And here is a picture of your favourite animal!
+      </fieldset>
+
+
+
+      <div className="animal">
+        <AnimalChoices
+          animalInput={animalInput} />
+      </div>
+      <div className="thank-you">
+        <h1> THANK YOU!</h1>
+        <p>Thank you so much for taking time to answer our questions! We always have so many
+        questions and it is great that you answered some for us. And here is a picture of your favourite animal!
           </p>
-         
-        </div> 
-      </section>
-    )
+
+      </div>
+    </section>
+  )
 }
 
