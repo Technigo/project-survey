03cfd16519699a06a56a8054498c52f5c752ae06
MarySnopok <marysnopok8@gmail.com>
2021-12-03 18:06:35 +0100

    confetty window resize

--- code/src/components/Confetti.js
+++ code/src/components/Confetti.js
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect } from "react";
+import React, { useState, useLayoutEffect, useEffect } from "react";
 import Confetti from "react-confetti";
 
 export default () => {
@@ -9,6 +9,19 @@ export default () => {
     setWidth(window.innerWidth);
     setHeight(window.innerHeight);
   }, []);
+
+  useEffect(() => {
+    const onResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", onResize);
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  });
 
   // console.log("width, height", width, height, window.innerWidth, window.innerHeight);
   return <Confetti width={width} height={height} numberOfPieces={300} />;
