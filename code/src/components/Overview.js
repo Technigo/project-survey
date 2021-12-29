a11ce433dfd15774e019ea194be24cfb9f33fa88
vrill <52007698+vrill@users.noreply.github.com>
2021-12-29 13:56:44 +0100

    Fixed up a few minor things such as cleaner presentation of the results and button effect.

--- code/src/components/Overview.js
+++ code/src/components/Overview.js
@@ -3,10 +3,11 @@ import React from 'react'
 const Overview = ({ nameInput, samuelInput, favInput }) => {
     return (
         <section>
-            <h2>Here's what we got for you:</h2>
-            <p>You chose to go by the handle of <strong>{nameInput}</strong>,
-            that Samuel L Jackson quote was from <strong>{samuelInput}</strong> and
-            your favourite type of cinematic experience is a <strong>{favInput}</strong>!</p>
+            <h2>Here's what we've got for you:</h2><br />
+            <p>You chose to go by the handle of <strong>{nameInput}</strong>. Cute.<br />
+            That Samuel L Jackson quote was from <strong>{samuelInput}</strong>, and<br />
+            your favourite type of cinematic experience is a <strong>{favInput}</strong>!</p><br />
+            <p>If you want to go again, just refresh the page.</p>
         </section>
     )
 }
