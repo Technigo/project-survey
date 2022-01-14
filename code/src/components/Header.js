e17a9a73b647d66dce40be943bf9cd00d46b1154
pdetli <74938586+pdetli@users.noreply.github.com>
2022-01-14 15:53:30 +0100

    updated emoji header comp

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -4,7 +4,12 @@ export const Header = () => {
   return (
     <header>
       <h4>MOON</h4>
-      <h4>TOUR 🌙</h4>
+      <h4>
+        TOUR
+        <span role="img" aria-label="moon-emoji">
+          🌙
+        </span>
+      </h4>
     </header>
   )
 }
