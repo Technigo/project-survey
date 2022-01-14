259a407e9593cba71596839a1e12f2ea5afbea4c
pdetli <74938586+pdetli@users.noreply.github.com>
2022-01-14 15:47:40 +0100

    updating developed year

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer>
       <h4>
-        Developed and Designed by Pinar Demirel 2021
+        Developed and Designed by Pinar Demirel 2022
         <a href="https://github.com/pdetli" target="blank">
           <i className="fa fa-github"></i>
         </a>
