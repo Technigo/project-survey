27a9213ae7992ce7601fdc0fb064c66e1026b320
Arianna Allahyar <anallahyar@gmail.com>
2022-03-14 23:43:04 +0100

    Conditional changed to switch statemenet that renders each question

--- code/src/App.js
+++ code/src/App.js
@@ -15,39 +15,62 @@ export const App = () => {
     },
   ];
 
-  const [answers, updateAnswerArray] = useState([]);
+  const [answers, updateAnswers] = useState([]);
 
-  let question = "";
-
-  if (answers.length === 0) {
-    question = questions[0].question;
-  } else if (answers.length === 1) {
-    question = questions[1].question;
-  } else if (answers.length === 2) {
-    question = questions[2].question;
-  } else {
-    question = "Thank you";
+  switch (answers.length) {
+    case 0:
+      return (
+        <>
+          <div className="questionCard">
+            <h2>{questions[0].question}</h2>
+            <input id="question1" type="text"></input>
+          </div>
+          <button
+            onClick={() =>
+              updateAnswers((arr) => [...arr, document.getElementById("question1").value])
+            }
+          >
+            Next question
+          </button>
+        </>
+      );
+    case 1:
+      return (
+        <>
+          <div className="questionCard">
+            <h2>{questions[1].question}</h2>
+            <label for="radio1">{questions[1].options[0]}</label>
+            <input className="radio" type="radio" value={questions[1].options[0]} />
+            <label for="radio2">{questions[1].options[1]}</label>
+            <input className="radio" type="radio" value={questions[1].options[1]} />
+          </div>
+          <button
+            onClick={() =>
+              //this isn't working...
+              updateAnswers((arr) => [...arr, document.getElementsByClassName("radio").value])
+            }
+          >
+            Next question
+          </button>
+        </>
+      );
+    case 2:
+      return (
+        <>
+          <div className="questionCard">
+            <h2>{questions[2].question}</h2>
+            <input id="questionColor" type="text"></input>
+          </div>
+          <button
+            onClick={() =>
+              updateAnswers((arr) => [...arr, document.getElementById("questionColor").value])
+            }
+          >
+            Submit questions
+          </button>
+        </>
+      );
+    default:
+      return <h1>Thank you! You answered: {answers}</h1>;
   }
-
-  return (
-    <main>
-      <h2>Arianna wonders...</h2>
-      <form>
-        <div className="questionCard">
-          <label>{question}</label>
-          <input id="questionColor" type="text"></input>
-        </div>
-      </form>
-
-      <button
-        onClick={() =>
-          updateAnswerArray((arr) => [...arr, document.getElementById("questionColor").value])
-        }
-      >
-        Next question
-      </button>
-
-      <h2>You answered: {answers}</h2>
-    </main>
-  );
 };
