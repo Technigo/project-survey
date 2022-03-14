023a7e29951c3c4ba07ebc05276dedd9c5a422f1
EmmaaBerg <emmaberg0819@gmail.com>
2022-03-14 20:58:53 +0100

    added a component for the survey introduction

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import {Introduction} from './components/IntroductionPage'
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <Introduction/>
     </div>
   )
 }
---
+++ code/src/components/IntroductionPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Introduction = () => {
+    return (
+        <main className='main-container'>
+            <h1>Ready for a weekend trip?</h1>
+            <form>
+                <button className='next-btn'>
+                    Lets find destination!
+                </button>
+            </form>
+        </main>
+    )
+}
