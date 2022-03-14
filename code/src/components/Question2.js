938b257547d95c682c5d5cb5d3124fa13842faa8
joannaringqvist <joanna.ringqvist@gmail.com>
2022-03-14 15:14:01 +0100

    Added the second question

--- code/src/components/Question2.js
+++ code/src/components/Question2.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Question2 = () => {
+
+    const ageGroups = [
+        '18-25',
+        '26-35',
+        '36-45',
+        '46-55',
+        '56 and above'
+    ];
+
+    const [ageGroup, setAgeGroup] = useState();
     
     return (
         <div>
             <p className="question">
                 How old are you?
             </p>
+            <form>
+                {ageGroups.map((group) => (
+                    <label key={group}>
+                    <input 
+                        type='radio'
+                        value={group}
+                        onChange={(event) => setAgeGroup(event.target.value)}
+                        checked={ageGroup === group}
+                    />
+                    {group}
+                </label>
+                ))}
+            </form>
         </div>
     );
   };
