ba1f9939a7626b9c231b76861b8f665e49c8e3d6
mathildakarlsson <86979661+mathildakarlsson@users.noreply.github.com>
2022-03-15 13:24:53 +0100

    basic structure with components

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Counter } from 'Components/Counter';
+import { InfoBanner } from 'Components/InfoBanner'
 
 export const App = () => {
+
+  const [counter, setCounter] = useState(0)
   return (
     <div>
-      Find me in src/app.js!
+      <h2>SUPER COUNTER</h2>
+      <Counter counter = {counter} setCounter = {setCounter}/>
+      <InfoBanner counter = {counter} />
     </div>
   )
 }
+
+// export const App = () => {
+
+//   const [counter, setCounter] = useState(0)
+//   const [surveyAnswers, setAnswer] = useState({
+//     'question1': ''
+//   })
+//   if (counter === 0) {
+//     return (
+//       <div>
+//         <Counter counter = {counter} setCounter = {setCounter}/>
+//         <h2>First Page</h2>
+//         {/* ANVÄND FORM ISTÄLLET FÖR COUNTER */}
+//         <InfoBanner counter = {counter} />
+//       </div>
+//     )
+//   } else if (counter === 1 { 
+//   } else {  
+//   }
+//   }
+//   return (
+//   )
+// }
---
+++ code/src/Components/DivisibleBy3.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const DivisibleByThree = (props) => {
+    const counter = props.counter;
+
+    return (
+        <div>
+            <p>Divisible by Three</p>
+        </div>
+    )
+}
