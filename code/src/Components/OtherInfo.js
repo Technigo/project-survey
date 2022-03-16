55da0dfa494bca10630639d3ee9f8d74ae68f00d
Jacob <jacob@engd.se>
2022-03-16 23:02:35 +0100

    Added radio buttons

--- code/src/Components/OtherInfo.js
+++ code/src/Components/OtherInfo.js
@@ -4,7 +4,7 @@ export const OtherInfo = ({ formData, setFormData }) => {
     return(
       <div className="other-info-container">
         <input type="text" placeholder="Nationalty..." value={formData.nationality} onChange={(event) => setFormData({...formData, nationality: event.target.value})} />
-        <input type="text" placeholder="Other..." value={formData.other} onChange={(event) => setFormData({...formData, other: event.target.value})} />
+        <input id="text" placeholder="Other..." value={formData.other} onChange={(event) => setFormData({...formData, other: event.target.value})} />
       </div>
     );
   }
--- code/src/Components/PersonalInfo.js
+++ code/src/Components/PersonalInfo.js
@@ -5,10 +5,10 @@ export const PersonalInfo = ({ formData, setFormData }) => {
       <div className="personal-info-container">
         <div>
           <lable>Male:</lable>
-        <input type="radio" name="gender" text="First Name..." value={formData.firstName} onChange={(event) => setFormData({...formData, firstName: event.target.value})}/>
+        <input type="radio" name="gender" text="First Name..." checked={formData.firstName} onChange={(event) => setFormData({...formData, firstName: event.target.checked})}/>
         </div>
         <div><lable>Female:</lable>
-        <input type="radio" name="gender" placeholder="Last Name..." value={formData.lastName} onChange={(event) => setFormData({...formData, lastName: event.target.value})} />
+        <input type="radio" name="gender" placeholder="Last Name..." checked={formData.lastName} onChange={(event) => setFormData({...formData, lastName: event.target.checked})} />
         </div>
         <input type="text" placeholder="User Name..." value={formData.username} onChange={(event) => setFormData({...formData, username: event.target.value})} />
       </div>
