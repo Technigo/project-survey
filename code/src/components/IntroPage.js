a629b2c366ee0eff31fbd4f3bab6b90fa450b889
Emma Högberg <emmahogberg88@gmail.com>
2022-03-17 20:40:27 +0100

    adjust colors

--- code/src/components/IntroPage.js
+++ code/src/components/IntroPage.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-import Balloon from "../assets/balloons.png";
 import "../intropage.css";
 
 export const IntroPage = () => {
 	return (
 		<>
-			<img className="balloons" src={Balloon} alt="balloons"></img>
 			<h2 className="intro-text">Someone is planning a birthday party, huh?</h2>
 			<h3 className="intro-text">
 				Create a customized invitation card for your kids{" "}
