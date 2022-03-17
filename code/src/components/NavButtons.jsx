18502524ee177ed78aaaaa8626fa40c014ebc998
Michael Chang <michaelchang@MacBook-Pro.lan>
2022-03-17 23:09:35 +0100

    trying to fix some things with the submit button

--- code/src/components/NavButtons.jsx
+++ code/src/components/NavButtons.jsx
@@ -57,7 +57,46 @@ const NavButtons = ({pageNumber, setPageNumber, error, setError, name, gender, a
             <div className="nav-buttons">
             {pageNumber > 0 && pageNumber <= 9 && <button onClick={pageBackward}><img className="navbutton-icon" src={backButton} alt="back button" /></button>}
             {pageNumber > 0 && pageNumber < 8 && <button onClick={pageForward}><img className="navbutton-icon" src={forwardButton} alt="forward button" /></button>}
-            {pageNumber === 8 && <SubmitButton />}
+            {pageNumber === 8 && 
+            <SubmitButton 
+                pageNumber = {pageNumber}
+                setPageNumber = {setPageNumber}
+                error = {error}
+                setError = {setError}
+                name = {name}
+                gender = {gender}
+                age = {age}
+                tipiq1Answer = {tipiq1Answer}
+                tipiq2Answer = {tipiq2Answer}
+                tipiq3Answer = {tipiq3Answer}
+                tipiq4Answer = {tipiq4Answer}
+                tipiq5Answer = {tipiq5Answer}
+                tipiq6Answer = {tipiq6Answer}
+                tipiq7Answer = {tipiq7Answer}
+                tipiq8Answer = {tipiq8Answer}
+                tipiq9Answer = {tipiq9Answer}
+                tipiq10Answer = {tipiq10Answer}
+                tiviq1Answer = {tiviq1Answer}
+                tiviq2Answer = {tiviq2Answer}
+                tiviq3Answer = {tiviq3Answer}
+                tiviq4Answer = {tiviq4Answer}
+                tiviq5Answer = {tiviq5Answer}
+                tiviq6Answer = {tiviq6Answer}
+                tiviq7Answer = {tiviq7Answer}
+                tiviq8Answer = {tiviq8Answer}
+                tiviq9Answer = {tiviq9Answer}
+                tiviq10Answer = {tiviq10Answer}
+                tiviq11Answer = {tiviq11Answer}
+                tiviq12Answer = {tiviq12Answer}
+                tiviq13Answer = {tiviq13Answer}
+                tiviq14Answer = {tiviq14Answer}
+                tiviq15Answer = {tiviq15Answer}
+                tiviq16Answer = {tiviq16Answer}
+                tiviq17Answer = {tiviq17Answer}
+                tiviq18Answer = {tiviq18Answer}
+                tiviq19Answer = {tiviq19Answer}
+                tiviq20Answer = {tiviq20Answer}
+            />}
             {pageNumber === 9 && <button onClick={refreshPage}><img className="navbutton-icon" src={restartButton} alt="restart button" /></button>}
         </div>
         )
--- code/src/components/SubmitButton.jsx
+++ code/src/components/SubmitButton.jsx
@@ -1,79 +1,94 @@
-import React, {useState} from "react";
+import React from "react";
+import forwardButton from '../assets/forward-purple.png'
 
-import { initializeApp } from "firebase/app";
-import { database } from 'firebase/database'
-import firebase from 'firebase/app';
+// import { initializeApp } from "firebase/app";
+// import firebase from 'firebase/app';
+// import { database } from 'firebase/database'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDGbArzFfRKxXoM7v1E9jhVQqS7HfXYtlk",
-    authDomain: "project-survey-week10.firebaseapp.com",
-    databaseURL: 'https://project-survey-week10-default-rtdb.europe-west1.firebasedatabase.app/',
-    projectId: "project-survey-week10",
-    storageBucket: "project-survey-week10.appspot.com",
-    messagingSenderId: "245167263996",
-    appId: "1:245167263996:web:ade40b556cd8a503b00779",
-    measurementId: "G-EKP1BVXVN8"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDGbArzFfRKxXoM7v1E9jhVQqS7HfXYtlk",
+//     authDomain: "project-survey-week10.firebaseapp.com",
+//     databaseURL: 'https://project-survey-week10-default-rtdb.europe-west1.firebasedatabase.app/',
+//     projectId: "project-survey-week10",
+//     storageBucket: "project-survey-week10.appspot.com",
+//     messagingSenderId: "245167263996",
+//     appId: "1:245167263996:web:ade40b556cd8a503b00779",
+//     measurementId: "G-EKP1BVXVN8"
+//   };
   
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+// const app = initializeApp(firebaseConfig);
+// const database = getDatabase(app);
 
 
 
-const SubmitButton = ({name, gender, age, tipiq1Answer, tipiq2Answer, tipiq3Answer, tipiq4Answer, tipiq5Answer, tipiq6Answer, tipiq7Answer, tipiq8Answer, tipiq9Answer, tipiq10Answer, tiviq1Answer, tiviq2Answer, tiviq3Answer, tiviq4Answer, tiviq5Answer, tiviq6Answer, tiviq7Answer, tiviq8Answer, tiviq9Answer, tiviq10Answer, tiviq11Answer, tiviq12Answer, tiviq13Answer, tiviq14Answer, tiviq15Answer, tiviq16Answer, tiviq17Answer, tiviq18Answer, tiviq19Answer, tiviq20Answer}) => {
+const SubmitButton = ({pageNumber, setPageNumber, name, gender, age, tipiq1Answer, tipiq2Answer, tipiq3Answer, tipiq4Answer, tipiq5Answer, tipiq6Answer, tipiq7Answer, tipiq8Answer, tipiq9Answer, tipiq10Answer, tiviq1Answer, tiviq2Answer, tiviq3Answer, tiviq4Answer, tiviq5Answer, tiviq6Answer, tiviq7Answer, tiviq8Answer, tiviq9Answer, tiviq10Answer, tiviq11Answer, tiviq12Answer, tiviq13Answer, tiviq14Answer, tiviq15Answer, tiviq16Answer, tiviq17Answer, tiviq18Answer, tiviq19Answer, tiviq20Answer}) => {
+    console.log(name, gender, age, tipiq1Answer, tipiq2Answer, tipiq3Answer, tipiq4Answer, tipiq5Answer, tipiq6Answer, tipiq7Answer, tipiq8Answer, tipiq9Answer, tipiq10Answer, tiviq1Answer, tiviq2Answer, tiviq3Answer, tiviq4Answer, tiviq5Answer, tiviq6Answer, tiviq7Answer, tiviq8Answer, tiviq9Answer, tiviq10Answer, tiviq11Answer, tiviq12Answer, tiviq13Answer, tiviq14Answer, tiviq15Answer, tiviq16Answer, tiviq17Answer, tiviq18Answer, tiviq19Answer, tiviq20Answer)
+
+    // $.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
+    // console.log(JSON.stringify(data, null, 2));
+    // });
+    let ipData
+    const IP_API = 'https://api.db-ip.com/v2/free/self'
+    fetch(IP_API)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+        ipData = data
+    })
 
     const handleSubmit = () => {
         window.scrollTo(0, 0)
         setPageNumber(pageNumber + 1)
-        saveData(name, gender, age, tipiq1Answer, tipiq2Answer, tipiq3Answer, tipiq4Answer, tipiq5Answer, tipiq6Answer, tipiq7Answer, tipiq8Answer, tipiq9Answer, tipiq10Answer, tiviq1Answer, tiviq2Answer, tiviq3Answer, tiviq4Answer, tiviq5Answer, tiviq6Answer, tiviq7Answer, tiviq8Answer, tiviq9Answer, tiviq10Answer, tiviq11Answer, tiviq12Answer, tiviq13Answer, tiviq14Answer, tiviq15Answer, tiviq16Answer, tiviq17Answer, tiviq18Answer, tiviq19Answer, tiviq20Answer)
+        // saveData(name, gender, age, tipiq1Answer, tipiq2Answer, tipiq3Answer, tipiq4Answer, tipiq5Answer, tipiq6Answer, tipiq7Answer, tipiq8Answer, tipiq9Answer, tipiq10Answer, tiviq1Answer, tiviq2Answer, tiviq3Answer, tiviq4Answer, tiviq5Answer, tiviq6Answer, tiviq7Answer, tiviq8Answer, tiviq9Answer, tiviq10Answer, tiviq11Answer, tiviq12Answer, tiviq13Answer, tiviq14Answer, tiviq15Answer, tiviq16Answer, tiviq17Answer, tiviq18Answer, tiviq19Answer, tiviq20Answer, ipData)
     }
 
     // Submission collection
-    const submissionRef = firebase.database().ref('submissions')
-    const saveData = (name, gender, age, tipiq1Answer, tipiq2Answer, tipiq3Answer, tipiq4Answer, tipiq5Answer, tipiq6Answer, tipiq7Answer, tipiq8Answer, tipiq9Answer, tipiq10Answer, tiviq1Answer, tiviq2Answer, tiviq3Answer, tiviq4Answer, tiviq5Answer, tiviq6Answer, tiviq7Answer, tiviq8Answer, tiviq9Answer, tiviq10Answer, tiviq11Answer, tiviq12Answer, tiviq13Answer, tiviq14Answer, tiviq15Answer, tiviq16Answer, tiviq17Answer, tiviq18Answer, tiviq19Answer, tiviq20Answer) => {
-        const newSubmissionRef = submissionRef.push();
-        newSubmissionRef.set({
-            Name: name,
-            Gender: gender,
-            Age: age,
-            TipiOne: tipiq1Answer,
-            TipiTwo: tipiq2Answer,
-            TipiThree: tipiq3Answer,
-            TipiFour: tipiq4Answer,
-            TipiFive: tipiq5Answer,
-            TipiSix: tipiq6Answer,
-            TipiSeven: tipiq7Answer,
-            TipiEight: tipiq8Answer,
-            TipiNine: tipiq9Answer,
-            TipiTen: tipiq10Answer,
-            TiviOne: tiviq1Answer,
-            TiviTwo: tiviq2Answer,
-            TiviThree: tiviq3Answer,
-            TiviFour: tiviq4Answer,
-            TiviFive: tiviq5Answer,
-            TiviSix: tiviq6Answer,
-            TiviSeven: tiviq7Answer,
-            TiviEight: tiviq8Answer,
-            TiviNine: tiviq9Answer,
-            TiviTen: tiviq10Answer,
-            TiviEleven: tiviq11Answer,
-            TiviTwelve: tiviq12Answer,
-            TiviThirteen: tiviq13Answer,
-            TiviFourteen: tiviq14Answer,
-            TiviFifteen: tiviq15Answer,
-            TiviSixteen: tiviq16Answer,
-            TiviSeventeen: tiviq17Answer,
-            TiviEighteen: tiviq18Answer,
-            TiviNineteen: tiviq19Answer,
-            TiviTwenty: tiviq20Answer,
-        })
-    }
+    // const submissionRef = database().ref('submissions')
+    // const saveData = (name, gender, age, tipiq1Answer, tipiq2Answer, tipiq3Answer, tipiq4Answer, tipiq5Answer, tipiq6Answer, tipiq7Answer, tipiq8Answer, tipiq9Answer, tipiq10Answer, tiviq1Answer, tiviq2Answer, tiviq3Answer, tiviq4Answer, tiviq5Answer, tiviq6Answer, tiviq7Answer, tiviq8Answer, tiviq9Answer, tiviq10Answer, tiviq11Answer, tiviq12Answer, tiviq13Answer, tiviq14Answer, tiviq15Answer, tiviq16Answer, tiviq17Answer, tiviq18Answer, tiviq19Answer, tiviq20Answer) => {
+    //     const newSubmissionRef = submissionRef.push();
+    //     newSubmissionRef.set({
+    //         Name: name,
+    //         Gender: gender,
+    //         Age: age,
+    //         TipiOne: tipiq1Answer,
+    //         TipiTwo: tipiq2Answer,
+    //         TipiThree: tipiq3Answer,
+    //         TipiFour: tipiq4Answer,
+    //         TipiFive: tipiq5Answer,
+    //         TipiSix: tipiq6Answer,
+    //         TipiSeven: tipiq7Answer,
+    //         TipiEight: tipiq8Answer,
+    //         TipiNine: tipiq9Answer,
+    //         TipiTen: tipiq10Answer,
+    //         TiviOne: tiviq1Answer,
+    //         TiviTwo: tiviq2Answer,
+    //         TiviThree: tiviq3Answer,
+    //         TiviFour: tiviq4Answer,
+    //         TiviFive: tiviq5Answer,
+    //         TiviSix: tiviq6Answer,
+    //         TiviSeven: tiviq7Answer,
+    //         TiviEight: tiviq8Answer,
+    //         TiviNine: tiviq9Answer,
+    //         TiviTen: tiviq10Answer,
+    //         TiviEleven: tiviq11Answer,
+    //         TiviTwelve: tiviq12Answer,
+    //         TiviThirteen: tiviq13Answer,
+    //         TiviFourteen: tiviq14Answer,
+    //         TiviFifteen: tiviq15Answer,
+    //         TiviSixteen: tiviq16Answer,
+    //         TiviSeventeen: tiviq17Answer,
+    //         TiviEighteen: tiviq18Answer,
+    //         TiviNineteen: tiviq19Answer,
+    //         TiviTwenty: tiviq20Answer,
+    //         IPData: ipData,
+    //     })
+    // }
  
     return (
         <button type="submit" value="Submit" onClick={handleSubmit} className="submit-btn"><img className="navbutton-icon" src={forwardButton} alt="forward button" /></button>
