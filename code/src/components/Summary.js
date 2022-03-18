b5825a6fef6da4bd1884fe1a3769be9c3667a7af
Lisen Lundgren <lisen@Lisens-MacBook-Pro.local>
2022-03-18 13:26:48 +0100

    changed background

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -3,16 +3,14 @@ import React from 'react';
 const Summary = ({ wordInput, selectInput, radioInput }) => {
 	return (
 		<div className='container'>
-			<div className=''>
+			<div className='question-container'>
 				<h2>Summary of answers</h2>
 				<p>Lmao "{wordInput}", good one xd</p>
 				<p>
-					Oh so you choose {selectInput}s? I hate {selectInput}s, {wordInput}{' '}
-					{selectInput}s!{' '}
+					Oh, so you chose {selectInput}s? I hate {selectInput}s, {wordInput}{' '}
+					{selectInput}s!
 				</p>
-				<p>
-					You strongly believe that jellyfishes are {radioInput}. Same bro.{' '}
-				</p>
+				<p>You strongly believe that jellyfishes are {radioInput}. Same bro.</p>
 				<h3>Thanks and good bye!</h3>
 			</div>
 		</div>
