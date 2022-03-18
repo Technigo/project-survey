5509416ab184507ff68fb47124fe100f44b98984
Kristiina Kolu <kristiinakolu@Maxs-MacBook-Air.local>
2022-03-18 23:03:08 +0200

    changed submission text

--- code/src/Components/ThanksPage.js
+++ code/src/Components/ThanksPage.js
@@ -4,15 +4,14 @@ export const ThanksPage = () => {
     return(
         <div className="container thanksContainer">
             <h1 className="thanksText">
-                <span>S</span>
+                <span>T</span>
+                <span>h</span>
+                <span>a</span>
+                <span>n</span>
+                <span>k</span>
+                <span>Y</span>
+                <span>o</span>
                 <span>u</span>
-                <span>b</span>
-                <span>m</span>
-                <span>i</span>
-                <span>t</span>
-                <span>t</span>
-                <span>e</span>
-                <span>d</span>
                 <span>!</span>
             </h1>
         </div>
