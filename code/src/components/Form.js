abb5e2a1fcb521ba8f4e92c928c56c9c4123337d
jenniferwvng <83246937+jenniferwvng@users.noreply.github.com>
2022-03-18 21:23:48 +0100

    Added validation to all input forms

--- code/src/components/Form.js
+++ code/src/components/Form.js
@@ -8,26 +8,6 @@ const Form = ({name, setName, location, setLocation, willTravel, setWillTravel, 
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const submitForm = (e) => {
-        if (currentQuestion !== componentsArray.length - 1) {
-            e.preventDefault();            
-        } else {
-            setSubmit(true);
-        }        
-     }
-
-
-    const handleNextQuestion = () => {
-        if (name !== '') {
-            setCurrentQuestion(currentQuestion + 1);
-            setErrorMessage('');
-        } else {
-            console.log('you must type your name');
-            setErrorMessage('You cannot leave this field empty');
-        }
-    }
-     //add validation that checks that values are not empty before clicking on to next question
-
      const componentsArray = [
         <InputForm 
             name={name}
@@ -49,18 +29,44 @@ const Form = ({name, setName, location, setLocation, willTravel, setWillTravel, 
             setRoles={setRoles}
             key={3}
         />
-     ]
+     ];
+
+    const submitForm = (e) => {
+        if (currentQuestion !== componentsArray.length - 1) {
+            e.preventDefault();            
+        } else {
+            setSubmit(true);
+        }        
+     }
+
+    const handleNextQuestionValidation = () => {
+        if (currentQuestion === 0 && name !== '') {
+            setCurrentQuestion(currentQuestion + 1);
+            setErrorMessage('');  
+        } else if (currentQuestion === 1 && location !== '') {
+            setCurrentQuestion(currentQuestion + 1);
+            setErrorMessage('');
+        } else if (currentQuestion === 2) {
+            setCurrentQuestion(currentQuestion + 1);
+            setErrorMessage('');
+        } else {
+            setErrorMessage('You cannot leave this field empty');
+        }
+    }
+
+    const totalNumOfQuestions = componentsArray.length;
 
     if (currentQuestion < componentsArray.length - 1) {
         return (
             <form onSubmit={submitForm}>
                 <h1>Application form</h1>
                 <h2>Developer and software engineering</h2>
+                <p>Question {currentQuestion + 1} out of {totalNumOfQuestions}</p>
                 {errorMessage}
                 {componentsArray[currentQuestion]}     
 
                 <div className="next-and-submit-button-div">
-                    <button className="next-button" type="button" onClick={handleNextQuestion}>Next question &#8618;</button>
+                    <button className="next-button" type="button" onClick={handleNextQuestionValidation}>Next question &#8618;</button>
                     <button className="submit-button" type="submit">Submit</button>
                 </div>
             </form>
@@ -70,6 +76,7 @@ const Form = ({name, setName, location, setLocation, willTravel, setWillTravel, 
             <form onSubmit={submitForm}>
             <h1>Application form</h1>
             <h2>Developer and software engineering</h2>
+            <p>Question {currentQuestion + 1} out of {totalNumOfQuestions}</p>
             {componentsArray[currentQuestion]}
     
             <button className="submit-button" type="submit">Submit</button>
