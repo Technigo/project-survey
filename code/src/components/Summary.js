bc04b504a8382edc6f556e5c35b756f027d8ac6c
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-03-18 12:27:44 +0100

    updated css

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -3,10 +3,12 @@ import React from "react";
 const Summary = ({ nameInput, FavoriteInput, HolidayInput }) => {
   return (
     <section className="summary">
-      <h2>Thank you for taking the survey</h2>
-      <h3>Name: {nameInput}</h3>
-      <h3>My Favorite season: {FavoriteInput}</h3>
-      <h3>My idea of a perfect holiday: I love {HolidayInput}</h3>
+      <div class="overview">
+        <h2>Thank you for taking the survey</h2>
+        <h3>Name: {nameInput}</h3>
+        <h3>My Favorite season: {FavoriteInput}</h3>
+        <h3>My idea of a perfect holiday: I love {HolidayInput}</h3>
+      </div>
     </section>
   );
 };
