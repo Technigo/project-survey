7743d538c50820edd76f0f8ff28707d62e16a3b9
Arianna Allahyar <anallahyar@gmail.com>
2022-03-18 18:20:37 +0100

    SubmittedView text revised

--- code/src/components/SubmittedView.js
+++ code/src/components/SubmittedView.js
@@ -1,13 +1,10 @@
 import React from "react";
-import './SubmittedView.css'
+import "./SubmittedView.css";
 
 const SubmittedView = ({ answers, reset }) => {
   return (
     <section className="container">
-      <h2>
-        Awesome! For our next dinner party, we'll make sure to design a menu around the following
-        preferences:{" "}
-      </h2>
+      <h2>Awesome! Our next dinner party will be designed around your preferences: </h2>
       <ul>
         {answers.map((answer, i) => (
           <li key={i}>{answer}</li>
