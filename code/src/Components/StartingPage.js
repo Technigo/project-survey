c922dc852f4349d26e5733303ecf53077b0bb6f3
Kristiina Kolu <kristiinakolu@Maxs-MacBook-Air.local>
2022-03-19 11:19:39 +0200

    fixed typing error

--- code/src/Components/StartingPage.js
+++ code/src/Components/StartingPage.js
@@ -5,7 +5,7 @@ export const StartingPage = (props) => {
     return(
             <div className="container">
                 <h1>Open application</h1>
-                <h2>We are also looking for creative developers. 
+                <h2>We are always looking for creative developers. 
                     Please fill the open application form and we will get back to you. 
                 </h2>
                 <button className="button" onClick={onStepChange}>Start</button>
