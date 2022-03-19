c5f5f8d14a6ca546f0c57b5a26c85c0a3f497e53
Arianna Allahyar <anallahyar@gmail.com>
2022-03-19 13:23:19 +0100

    Class naming conventions changes to kebab case

--- code/src/components/SubmittedView.js
+++ code/src/components/SubmittedView.js
@@ -4,10 +4,10 @@ import "./SubmittedView.css";
 const SubmittedView = ({ answers, reset }) => {
   return (
     <section className="container">
-      <h2 className="submitMessage">Awesome! Our next dinner party will be designed around your preferences: </h2>
-      <ul>
+      <h2 className="submit-message">Awesome! Our next dinner party will be designed around your preferences: </h2>
+      <ul className="submit-list">
         {answers.map((answer, i) => (
-          <li key={i}>{answer}</li>
+          <li className="submit-list-item" key={i}>{answer}</li>
         ))}
       </ul>
       <button onClick={reset}>Reset</button>
