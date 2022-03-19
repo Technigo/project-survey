f6770a07061ab4e1d8679c11b458b61950275fd8
Nhung Phan <nhungphan@Joel-Lov.local>
2022-03-19 14:23:53 +0200

    styling hero section

--- code/src/components/Heading.js
+++ code/src/components/Heading.js
@@ -5,13 +5,17 @@ import { TiStarFullOutline } from "react-icons/ti";
 
 const Heading = ( {nextQuestion} ) => {
    return <div className='hero-section'>
+   <div>
+
    <h1>Your feedback means the world to us</h1>
    <button className="start-btn" type="button" onClick={nextQuestion}>Give feedback</button>
+   </div>
    <div className="hero-img-container">
-      <img src={form} alt='a happy woman'/>
+      <img className='hero-img' src={form} alt='a happy woman'/>
       <TiStarFullOutline className="star-icon" />
       <TiStarFullOutline className="star-icon star-icon-two" />
       </div>
+   
 
    </div>
 }
