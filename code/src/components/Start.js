d5f937ab480e6e739b3667960e164fbd49a2c0be
joannaringqvist <joanna.ringqvist@gmail.com>
2022-03-20 21:00:48 +0100

    Added a start page component

---
+++ code/src/components/Start.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Start = () => {
+
+  return (
+    <div className="start">
+        
+      <div className="container">
+        <h1>This is a small survey about just a random product</h1>
+        <p>
+        Thank you for filling out this form. It will take about 1 minute.
+        </p>
+      </div>
+      
+
+    </div>
+  );
+};
+
+export default Start;
