05f63066d617ba052d0b60696e8ce58242b75c83
Therese Nyman <theresenyman@Thereses-MacBook-Air.local>
2022-03-20 19:22:29 +0100

    code and styling done

--- code/src/components/Location.js
+++ code/src/components/Location.js
@@ -8,14 +8,12 @@ import React from "react";
           <label htmlFor="city">
               <h2>In which city did you join our event?</h2>
             </label>
-          <div className="custom-select">
-            <select onChange={onCityChange} value={city}>
+            <select className="location-select" onChange={onCityChange} value={city}>
             <option value="">Select location:</option>
             <option value="Stockholm">Stockholm</option>
             <option value="Gothenburg">Gothenburg</option>
             <option value="Copenhagen">Copenhagen</option>
             </select>
-            </div>
           <div className="locationButtons">
         <button onClick={onBackstepChange}>Back</button>
         <button onClick={onStepChange} disabled={city===""}>Go!</button>
