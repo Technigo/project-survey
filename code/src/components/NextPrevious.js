dd3b4f45c6c507500a39560244712697f65d5360
joannaringqvist <joanna.ringqvist@gmail.com>
2022-03-20 22:09:30 +0100

    Fixed a bug with the previous button

--- code/src/components/NextPrevious.js
+++ code/src/components/NextPrevious.js
@@ -5,7 +5,7 @@ const NextPrevious = (props) => {
   return (
     <div className="container forward-next">
         {/*  Only show the previous button if you are not on the first page */}
-        { props.counter > 0 && <button onClick={() =>props.setCounter(props.counter - 1)}>Previous question</button> }
+        { props.counter > 1 && <button onClick={() =>props.setCounter(props.counter - 1)}>Previous question</button> }
         { !props.hideNextQuestionButton &&
         <button onClick={() =>props.setCounter(props.counter + 1)}>{ props.counter === 0 ? 'Start!' : 'Next question' } </button>
         }
