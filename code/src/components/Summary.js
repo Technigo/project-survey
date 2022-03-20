5fb744c880d5b36aeebb491b8cca75763183ed6c
Katarina Elgerus <katarina.elgerus@liv.se>
2022-03-20 12:37:24 +0100

    mobileversion adjusted

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -6,9 +6,9 @@ export const Summary=({nameInput, interestInput, locationInput, emailInput, rest
     <section>
       <h1> Thank you <span className="purpletext">{nameInput}</span>!</h1>
       <h2> We will send updates to 
-        <span className="purpletext">{emailInput}</span> about 
-        <span className="purpletext">{interestInput}
-        </span> in <span className="purpletext">{locationInput}</span>
+        <span className="purpletext"> {emailInput}</span> about 
+        <span className="purpletext"> {interestInput}
+        </span> in <span className="purpletext"> {locationInput}</span>
        </h2>
        <button className="btn" type="submit" onClick={()=>{restart()}}><span>Start again!</span></button>
     </section>
