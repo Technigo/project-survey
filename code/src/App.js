56ead009d6e062a924c038e62bf11cac8036a8df
Jenny Fiskaare <jennyfiskaare@Jennys-MacBook-Air.local>
2022-03-21 16:22:56 +0100

    renoved unused consts

--- code/src/App.js
+++ code/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import Footer from "components/Footer";
-import StartPage from "components/Start";
-import FirstQuestion from 'components/FirstQuestion';
-import SecondQuestion from 'components/SecondQuestion';
-import ThirdQuestion from 'components/ThirdQuestion';
 import Form from 'components/Form';
-import Summary from 'components/Summary';
+
 
 
 export const App = () => {
