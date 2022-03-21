e5a0282e462949bd8ca5aa83cb1c724f77aacf21
Jenny Fiskaare <jennyfiskaare@Jennys-MacBook-Air.local>
2022-03-21 16:31:57 +0100

    removed one more icon

--- code/src/components/Start.js
+++ code/src/components/Start.js
@@ -5,7 +5,7 @@ const StartPage = ( {onStepChange} ) => {
     return(
            <section className="intro-container">
             <h1 className="start-text">CRAVING SOME MACARONS?!</h1>
-               <p className="intro-text">Order your personal macaron box below! <span>🎁  🎁 🎁</span> </p>
+               <p className="intro-text">Order your personal macaron box below! </p>
               <div className="button-container">
                <button className="start-btn" onClick={onStepChange}>OK, let's go!!! </button>
                </div>
