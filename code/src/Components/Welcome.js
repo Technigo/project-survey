8a65329b5be91c7ac7514fc83da4b8f0fc93c97c
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-03-21 10:00:13 +0100

    testing autodeploy netlify

--- code/src/Components/Welcome.js
+++ code/src/Components/Welcome.js
@@ -5,7 +5,7 @@ export const Welcome = () => {
   return (
     <>
     <h1>Welcome</h1>
-    <h2>To the annual art lottery sign up!</h2>
+    <h2>To the  annual art lottery sign up!</h2>
     </>
   )
 }
