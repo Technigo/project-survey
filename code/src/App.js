ba4ee82dbd41deda760edfe4c1561df04240d30d
Sneezan <ceciliafraanlund@gmail.com>
2022-09-26 12:04:20 +0200

    forgot this is a react app, html built directly in appjs

--- code/src/App.js
+++ code/src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/Cecilia.js!
-    </div>
+    <main>
+      <div className="main-site" id="main-site">
+        <h1>Welcome to the survery</h1>
+        <button type="button">Start survey</button>
+      </div>
+
+      <div className="survey">
+        <form />
+      </div>
+    </main>
   );
 }
