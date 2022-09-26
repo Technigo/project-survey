a4d9a277ea4c263ec45d88c9f6992478fef341d2
ViktorSvenssonN <viktor.tommy.svensson@gmail.com>
2022-09-26 19:01:33 +0200

    added components and images

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
+
+import HeroImg from "./components/HeroImg";
 
 export const App = () => {
+
+  const [currentNum, setQuestionNum] = useState(0);
+
+  {questionNum === 0 &&  <HeroSection nextQuestion = {nextQuestion}/>}
+
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+      <form className="mainForm" onSubmit={submit}>
+        <div className="formContainer">
+          <p>hello </p>
+        </div>
+
+      </form>
+    </>
   );
 }
