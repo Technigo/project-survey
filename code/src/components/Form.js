f97a4acbcd93432f1a323786bd4d630cb9c37393
Sailornina <sylcardozo.sc@gmail.com>
2022-09-26 18:02:04 +0200

    components folder was created. Form in progress.

---
+++ code/src/components/Form.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import WelcomeQuestion from './WelcomeQuestion';
+
+const Form = () => {
+  <WelcomeQuestion />
+};
+
+export default Form;
---
+++ code/src/components/WelcomeQuestion.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const WelcomeQuestion = () => {
+  return (
+    <section className="welcome-section">
+      <div className="welcome-wrap">
+        <h1>Hey! This is a Survey Time.</h1>
+        <p> This survey is about the change of custom or decisions to protect the environment.
+            Come participate with us!
+        </p>
+      </div>
+
+    </section>
+  )
+}
+
+export default WelcomeQuestion;
