ed19e1547fd887a05ff80d5cfd17e9ab34fff10f
Sailornina <sylcardozo.sc@gmail.com>
2022-09-27 11:32:39 +0200

    WelcomeQuestion component is done.

--- code/src/components/WelcomeQuestion.js
+++ code/src/components/WelcomeQuestion.js
@@ -1,14 +1,28 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 
-const WelcomeQuestion = () => {
+const WelcomeQuestion = ({ onNameChange, userName, nextQuestion }) => {
   return (
     <section className="welcome-section">
       <div className="welcome-wrap">
-        <h1>Hey! This is a Survey Time.</h1>
+        <h1>Hey! This is Survey Time.</h1>
         <p> This survey is about the change of custom or decisions to protect the environment.
-            Come participate with us!
+          Come participate with us!
         </p>
       </div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="name" placeholder="Your name">
+          <input
+            type="text"
+            onChange={onNameChange}
+            value={userName}
+            id="name"
+            name="name" />
+        </label>
+      </form>
+      <button type="button" onClick={nextQuestion} className="button">
+       Let's go!
+      </button>
     </section>
   )
 }
