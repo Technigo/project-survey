b5ede8909288371720a060eb11f4c5c081202f7b
Jessika Lind <jessika@MacBook-Air-som-tillhor-Jessika.local>
2022-09-27 17:23:33 +0200

    radio buttons working

--- code/src/Components/AgeQuestion.js
+++ code/src/Components/AgeQuestion.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 
 const ageGroups = [
-  "15-20",
-  "20-29",
-  "30-39",
-  "40-49",
-  "50-59",
-  "60-69",
-  "70-79",
-  "80-89",
-  "90+"]
+  '15-20',
+  '20-29',
+  '30-39',
+  '40-49',
+  '50-59',
+  '60-69',
+  '70-79',
+  '80-89',
+  '90+']
 console.log(ageGroups)
 
 const AgeQuestion = ({age, setAge}) => {
@@ -20,21 +20,21 @@ const AgeQuestion = ({age, setAge}) => {
 
     return (
       <>
-      <h2>How old are you?</h2>
-        <form>
-          {ageGroups.map((group => {
-            <label key ={group}>
-              <input
-                type="radio"
-                value={group}
-                onChange={event => handleAgeChange}
-                checked={ageGroups === group}
-                />
-                {age}
-              </label>
-          }))}
-        </form>
+        <h2>How old are you?</h2>
+          <form className="radio-buttons">
+              {ageGroups.map(group => (
+                <label key ={group}>
+                  <input
+                    type="radio"
+                    value={group}
+                    onChange={handleAgeChange}
+                    checked={age === group}
+                    />
+                    {group}
+                  </label>
+              ))}
+          </form>
       </>
- )
+    )
 }
   export default AgeQuestion
--- code/src/Components/MemoryQuestion.js
+++ code/src/Components/MemoryQuestion.js
@@ -10,6 +10,7 @@ const MemoryQuestion = ({memory, setMemory}) => {
       <div>
       <h2>A memorable visit?</h2>
         <form>
+          {/* Style the input so that it line-breaks and the text starts at the very top */}
           <p>We are interested in hearing if you have a special memory from a visit at 
             one of Stockholm's museums that you wish to share. Perhaps you remember a certain 
             exhibit fondly, or have a mildly traumatic experience related to a boring visit? Please share!</p>
