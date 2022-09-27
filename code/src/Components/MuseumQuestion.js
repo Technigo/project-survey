1503be04c161d980645cfd82fc2bbf926d3e768e
Jessika Lind <jessika@MacBook-Air-som-tillhor-Jessika.local>
2022-09-27 17:07:06 +0200

    select working in MuseumQuestion-component

--- code/src/Components/MuseumQuestion.js
+++ code/src/Components/MuseumQuestion.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-const MuseumQuestion = () => {
-    return (
-      <div>
-        <h2>Which of these is your favoriture museum?</h2>
+const MuseumQuestion = ({museum, setMuseum}) => {
+    
+  const handleMuseumChange = (event) => {
+    setMuseum(event.target.value)
+  }
+
+  return (
         <form>
-            {/* Which of the following museums is your favorite? - Select dropdown */}
+           <h2>Which of these is your favoriture museum?</h2>
+          <select
+            onChange={handleMuseumChange}
+            value={museum}>
+              <option value="Moderna museet">Select museum</option> 
+              {/* Make the option above not possible to choose */}
+              <option value="Moderna museet">Moderna museet</option>
+              <option value="Vasamuseet">Vasamuseet</option>
+              <option value="Noriska museet">Noriska museet</option>
+              <option value="Naturhistoriska museet">Naturhistoriska museet</option>
+              <option value="Sjöhistoriska museet">Sjöhistoriska museet</option>
+              <option value="Historiska museet">Historiska museet</option>
+            </select>
         </form>
-      </div>
+
     );
   }
 
