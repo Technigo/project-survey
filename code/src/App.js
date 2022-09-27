2f53c08eda9a10cf49f18e1c1e54588cc5e202f9
Petra Söderström <petrasoderstrom1612@gmail.com>
2022-09-27 11:36:56 +0200

    jajaja

--- code/src/App.js
+++ code/src/App.js
@@ -1,5 +1,55 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 
 export const App = () => {
-  return <div>Find me in src/app.js!</div>;
+  const [counter, setCounter] = useState(0);
+  const [userInput, setUserInput] = useState('');
+  const [userName, setUserName] = useState('');
+  const handleCounterButtonClick = (shouldIncrease) => {
+    if (shouldIncrease && counter < 10) {
+      setCounter(counter + 1);
+    } else if (!shouldIncrease && counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
+  const handleUserInputChange = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const handleClearButtonClick = () => {
+    setUserInput('');
+  };
+
+  const handleUserNameInputChange = (event) => {
+    setUserName(event.target.value);
+  };
+
+  return (
+    <div>
+      My counter {counter}
+      <button type="button" onClick={() => handleCounterButtonClick(true)}>
+        Increase 10 max
+      </button>
+      <button type="button" onClick={() => handleCounterButtonClick(false)}>
+        Decrease 0 min
+      </button>
+      <input type="text" value={userInput} onChange={handleUserInputChange} />
+      <p> {userInput}</p>
+      <p>
+        <button type="button" onClick={handleClearButtonClick}>
+          clear input
+        </button>
+      </p>
+      {counter === 0 && (
+        <div>
+          <p>Hello, please type in your name</p>
+          <input
+            type="text"
+            value={userName}
+            onChange={handleUserNameInputChange} />
+        </div>
+      )}
+    </div>
+  );
 };
