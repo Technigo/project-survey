2ac347c19f2cd6491c66a94edddb52c1877a52c0
Linus Eriksson <erilinus@gmail.com>
2022-09-28 12:23:29 +0200

    added some questions

--- code/src/App.js
+++ code/src/App.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 
 export const App = () => {
   const [counter, setCounter] = useState(0);
+  const [recieverName, setRecieverName] = useState('');
   const [userInput, setUserInput] = useState('');
   const [userName, setUserName] = useState('');
 
   const handleCounterButtonClick = (shouldIncrease) => {
-    if (shouldIncrease && counter < 10) {
+    if (shouldIncrease && counter < 1) {
       setCounter(counter + 1)
     } else if (!shouldIncrease && counter > 0) {
       setCounter(counter - 1)
@@ -21,26 +22,51 @@ export const App = () => {
     setUserInput('');
   }
 
+  const handleRecieverNameInputChange = (event) => {
+    setRecieverName(event.target.value);
+  }
+
   const handleUserNameInputChange = (event) => {
     setUserName(event.target.value);
   }
 
   return (
     <div>
-      My counter {counter}
-      <button type="button" onClick={() => handleCounterButtonClick(true)}> Increase  - 10 max</button>
-      <button type="button" onClick={() => handleCounterButtonClick(false)}> Decrease  - 0 min</button>
-      <input type="text" value={userInput} onChange={handleUserInputChange} />
-      <p> {userInput} </p>
-      <p>
-        <button type="button" onClick={handleClearButtonClick}>Clear input</button>
-      </p>
+      <h1>Passive-Aggressive Email</h1>
+
+      {/* Where we gather the message */}
       {counter === 0 && (
         <div>
-          <p>Hello, please type in your name.</p>
-          <input type="text" value={userName} onChange={handleUserNameInputChange} />
+          <p>
+            Dear
+            <input type="text" value={recieverName} onChange={handleRecieverNameInputChange} placeHolder="Type name" />
+            ,
+          </p>
+          <p>
+            <p>Lately I`ve noticed that you:</p>
+            <textarea rows="5" cols="30" name="Message" type="text" value={userInput} onChange={handleUserInputChange} placeHolder="Write a message" />
+          </p>
+          <p>
+            Sincerely,
+            <input type="text" value={userName} onChange={handleUserNameInputChange} placeHolder="Your name" />
+          </p>
+          <button type="button" onClick={() => handleCounterButtonClick(true)}> Summarize</button>
         </div>
       )}
+
+      {/* Where we summarize the message */}
+      {counter === 1 && (
+        <div>
+          {/* Use this in summarize for message input */}
+          <p> {userInput} </p>
+          <button type="button" onClick={() => handleCounterButtonClick(false)}> Cancel</button>
+          <p>
+            <button type="button" onClick={handleClearButtonClick}>Clear input</button>
+          </p>
+          <p>Hello, please type in your name.</p>
+        </div>
+      )}
+      My counter {counter}
     </div>
   );
 }
