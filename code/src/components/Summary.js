b43cf7cc7d631394dba876f4170b73fbdfa42127
uohman <ulrikalovisa@gmail.com>
2022-09-28 14:06:16 +0100

    Trying to make the change background color to work

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -5,13 +5,26 @@ const Summary = ({
   selectInput,
   radioInput
 }) => {
-  
+
+  const changeBackgroundColor = () => {
+    let color;
+    if (selectInput === "Orange") {
+      document.body.style.backgroundColor = "#e57151"
+    } else if (selectInput === "Brown") {
+        color = "#a5895d"
+    } else if (selectInput === "Yellow") {
+        color = '#f09937';
+    }
+    return color;
+};
+
   return (
+
     <div>
-      
       <div className="Greeting">{radioInput}, {nameInput}!</div>
     </div>
   )
-}
+
+};
 
 export default Summary;
