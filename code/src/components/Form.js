8eba49973fc3d9d1fe6dab67fbaab8caa17e4b5b
Elin Segelöv <elin.segelov@gmail.com>
2022-09-28 10:15:06 +0200

    add thre dummy questions

---
+++ code/src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FirstQuestion from './FirstQuestion';
+import SecondQuestion from './SecondQuestion';
+import ThirdQuestion from './ThirdQuestion';
+
+const Form = () => {
+  return (
+    <form>
+      <FirstQuestion />
+      <SecondQuestion />
+      <ThirdQuestion />
+    </form>
+  )
+}
+export default Form;
