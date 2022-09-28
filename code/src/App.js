e72b9da30a5e2824a5f852ca51cb2d1a86945fcb
Archkrull <102250838+Archkrull@users.noreply.github.com>
2022-09-28 15:00:37 +0200

    testing out features

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+
+import React, { useState } from 'react';
+import { Result } from 'components/Result';
+import { Name } from './components/Name';
+import { Food } from './components/Food';
+import { Drink } from './components/Drink';
+/* const [userInput, setUserInput] = useState(''); */
 
 export const App = () => {
+  const [step, setStep] = useState(1);
+  const [name, setName] = useState('');
+  const [food, setFood] = useState('');
+  const [drink, setDrink] = useState('');
+
+  const handleStepIncrease = () => {
+    setStep(step + 1);
+  }
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+      {step === 1 && (<Name name={name} setName={setName} />
+      )}
+      {step === 2 && (<Food food={food} setFood={setFood} />
+      )}
+      {step === 3 && (<Drink drink={drink} setDrink={setDrink} />
+      )}
+      {step >= 4 && (<Result name={name} food={food} drink={drink} />
+      )}
+
+      {step < 4 && (
+        <>
+          <p>current question: {step}/4</p>
+          <button type="button" value={step} onClick={handleStepIncrease}>Next Question</button>
+        </>
+      )}
+    </>
   );
 }
