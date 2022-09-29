3b82bfb3d85f8bc3c6420a3f5ba325873684ba8f
Petra Söderström <petrasoderstrom1612@gmail.com>
2022-09-29 15:16:09 +0200

    Buttons when applicable

--- code/src/App.js
+++ code/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import FirstPage from 'components/FirstPage';
 import UserName from 'components/UserName';
 import UserLastname from 'components/UserLastname';
 import UserMail from 'components/UserMail';
@@ -7,7 +8,7 @@ import UserAge from 'components/UserAge';
 import Frequency from 'components/Frequency';
 
 export const App = () => {
-  const [counter, setCounter] = useState(1);
+  const [counter, setCounter] = useState(0);
   const [name, setName] = useState('');
   const [lastname, setLastname] = useState('');
   const [mail, setMail] = useState('');
@@ -18,7 +19,7 @@ export const App = () => {
   const handleCounterButtonClick = (shouldIncrease) => {
     if (shouldIncrease && counter < 7) {
       setCounter(counter + 1);
-    } else if (!shouldIncrease && counter > 1) {
+    } else if (!shouldIncrease && counter > 0) {
       setCounter(counter - 1);
     }
   };
@@ -34,89 +35,86 @@ export const App = () => {
 
   return (
     <form/*  onSubmit={(event) => event.preventDefault()} */>
-      <div>
-        <div className="newsletter">
-          <h1>Join our ⭐ newsletter</h1>
-          <p>Get early access to new products & exclusive promos! </p>
-          <p>Do not miss your chance! Only for members of our newsletter!</p>
-        </div>
 
-        <div className="questionsCounter">
-        Question number {counter}
-          <div className="questionsButtons">
-            <button type="button" aria-label="Click to see previous question" onClick={() => handleCounterButtonClick(false)}>
+      {counter < 8 && counter > 0 && (
+        <button type="button" aria-label="Click to see previous question" onClick={() => handleCounterButtonClick(false)}>
         Previous question
-            </button>
-            <button type="button" aria-label="Click to see next question" onClick={() => handleCounterButtonClick(true)}>
+        </button>
+      )}
+
+      {counter < 7 && counter > -1 && (
+        <button type="button" aria-label="Click to see next question" onClick={() => handleCounterButtonClick(true)}>
         Next question
-            </button>
-          </div>
+        </button>
+      )}
+
+      {counter === 0 && (
+        <FirstPage />
+      )}
+
+      {counter === 1 && (
+        <div>
+          <UserName username={name} setUsername={setName} />
+          <p>Questions answered: {counter}/7 </p>
         </div>
+      )}
 
-        {counter === 1 && (
-          <div>
-            <UserName username={name} setUsername={setName} />
-            <p>Questions answered: {counter}/7 </p>
-          </div>
-        )}
+      {counter === 2 && (
+        <div>
+          <UserLastname userlastname={lastname} setUserlastname={setLastname} username={name} />
+          <p>Questions answered: {counter}/7 </p>
+        </div>
+      )}
 
-        {counter === 2 && (
-          <div>
-            <UserLastname userlastname={lastname} setUserlastname={setLastname} username={name} />
-            <p>Questions answered: {counter}/7 </p>
-          </div>
-        )}
+      {counter === 3 && (
+        <div>
+          <UserMail usermail={mail} setUsermail={setMail} />
+          <p>Questions answered: {counter}/7 </p>
+        </div>
+      )}
 
-        {counter === 3 && (
-          <div>
-            <UserMail usermail={mail} setUsermail={setMail} />
-            <p>Questions answered: {counter}/7 </p>
-          </div>
-        )}
+      {counter === 4 && (
+        <div>
+          <UserLocation userlocation={location} setUserlocation={setLocation} />
+          <p>Questions answered: {counter}/7 </p>
+        </div>
+      )}
 
-        {counter === 4 && (
-          <div>
-            <UserLocation userlocation={location} setUserlocation={setLocation} />
-            <p>Questions answered: {counter}/7 </p>
-          </div>
-        )}
+      {counter === 5 && (
+        <div>
+          <UserAge userage={age} setUserage={setAge} />
+          <p>Questions answered: {counter}/7 </p>
+        </div>
+      )}
 
-        {counter === 5 && (
-          <div>
-            <UserAge userage={age} setUserage={setAge} />
-            <p>Questions answered: {counter}/7 </p>
-          </div>
-        )}
+      {counter === 6 && (
+        <div>
+          <Frequency userfrequency={frequency} setUserfrequency={setFrequency} />
+          <p>Questions answered: {counter}/7 </p>
+        </div>
+      )}
 
-        {counter === 6 && (
-          <div>
-            <Frequency userfrequency={frequency} setUserfrequency={setFrequency} />
-            <p>Questions answered: {counter}/7 </p>
-          </div>
-        )}
-
+      {counter < 7 && counter > 0 && (
         <p>
           <button type="button" onClick={handleClearButtonClick}>
           clear input
           </button>
         </p>
+      )}
 
-        {counter === 7 && (
-          <div>
-            <p>Survey completed!</p>
-            <p>Questions answered: {counter}/7 </p>
-            <p>Summary of you answers:</p>
-            <p>Name: {name} </p>
-            <p>Lastname: {lastname} </p>
-            <p>E-mail: {mail} </p>
-            <p>Physical shop location: {location} </p>
-            <p>Age group: {age} </p>
-            <p>Frequency for newsletter: {frequency}</p>
-            <button aria-label="Submit answers" type="submit" onSubmit value="Send">Send</button>
-          </div>
-        )}
-      </div>
+      {counter === 7 && (
+        <div>
+          <p>Survey completed!</p>
+          <p>Summary of you answers:</p>
+          <p>Name: {name} </p>
+          <p>Lastname: {lastname} </p>
+          <p>E-mail: {mail} </p>
+          <p>Physical shop location: {location} </p>
+          <p>Age group: {age} </p>
+          <p>Frequency for newsletter: {frequency}</p>
+          <button aria-label="Submit answers" type="submit" onSubmit value="Send">Send</button>
+        </div>
+      )}
     </form>
-
   );
 };
---
+++ code/src/components/FirstPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FirstPage = () => {
+  return (
+    <div className="newsletter">
+      <h1>Join our ⭐ newsletter</h1>
+      <p>Get early access to new products & exclusive promos! </p>
+      <p>Do not miss your chance! Only for members of our newsletter!</p>
+    </div>
+  )
+}
+
+export default FirstPage;
