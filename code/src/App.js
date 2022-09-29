01658975f086c8a32f219532014354c783226100
Caroline Söderström <carolinesoderstrom@Carolines-MacBook-Air.local>
2022-09-29 15:23:34 +0200

    renamed elements and variables

--- code/src/App.js
+++ code/src/App.js
@@ -6,7 +6,7 @@ export const App = () => {
   const [preference, setPreference] = useState('');
   const [condiment, setCondiment] = useState('');
   const [spicy, setSpicy] = useState('');
-  const [age, setAge] = useState('');
+  const [sprinkle, setSprinkle] = useState('');
 
   const handleNextButtonClick = () => {
     setStepCounter(stepCounter + 1);
@@ -31,104 +31,118 @@ export const App = () => {
     console.log(spicy);
   }
 
-  const handleAgeChange = (event) => {
-    setAge(event.target.value);
+  const handleSprinkleChange = (event) => {
+    setSprinkle(event.target.value);
   }
 
   return (
-    <div className="container">
-      {stepCounter === 1 && (
-        <>
-          <h1>Hi! What is your name?</h1>
-          <input type="text" value={userName} onChange={handleUserNameInputChange} />
-          <button type="button" onClick={handleNextButtonClick}>Next</button>
-        </>
-      )}
+    <section className="container">
+      <div className="stepOne">
+        {stepCounter === 1 && (
+          <>
+            <h1>Welcome to the Pomodoro generator!</h1>
+            <p>Gone are the days of tasteless carbonara and dull pesto jars.
+              We have created this pomodoro generator to help you take the first steps into
+              the Queendom of the greatest sauce on earth, tomato sauce. There are endless
+              variations to explore and to avoid choice overload you will answer
+              a few questions in this survey to help narrow down the options.
+              Start by entering your name below to find your perfect pomodoro match!
+            </p>
+            <div className="start">
+              <input type="text" placeholder="Your Name" value={userName} onChange={handleUserNameInputChange} />
+              <button type="button" onClick={handleNextButtonClick}>Start!</button>
+            </div>
+          </>
+        )}
+      </div>
 
-      {stepCounter === 2 && (
-        <>
-          <h1>Hi {userName}!</h1>
-          <p>Let&apos;s find your pomodoro match &#127813;</p>
-          <p>Would you like your sauce to be?</p>
-          <label htmlFor="meat">
-            <input type="radio" id="meat" name="preference" value="meat" onChange={handlePreferenceInputChange} />
-            Meat
-          </label>
-          <label htmlFor="veg">
-            <input type="radio" id="veg" name="preference" value="veg" onChange={handlePreferenceInputChange} />
-            Veg
-          </label>
-          <label htmlFor="np">
-            <input type="radio" id="np" name="preference" value="No Preference" onChange={handlePreferenceInputChange} />
-            No preference
-          </label>
-          <button type="button" onClick={handleNextButtonClick}>Next</button>
-        </>
-      )}
+      <div className="questions">
+        {stepCounter === 2 && (
+          <>
+            <h1>Hi {userName}!</h1>
+            <h2>Would you like your sauce..?</h2>
+            <label htmlFor="meat">
+              <input type="radio" id="meat" name="preference" value="Meat" onChange={handlePreferenceInputChange} checked={preference === 'Meat'} />
+              Filled with pancetta!
+            </label>
+            <label htmlFor="veg">
+              <input type="radio" id="veg" name="preference" value="Veg" onChange={handlePreferenceInputChange} checked={preference === 'Veg'} />
+              Vegetarian please!
+            </label>
+            <button type="button" onClick={handleNextButtonClick}>Next</button>
+          </>
+        )}
 
-      {stepCounter === 3 && (
-        <>
-          <h1>Which is your favorite tomato pairing?</h1>
-          <label htmlFor="cheese">
-            <input type="radio" id="cheese" name="condiment" value="cheese" onChange={handleCondimentChange} />
-            Cheeeeese!!
-          </label>
-          <label htmlFor="aubergine">
-            <input type="radio" id="aubergine" name="condiment" value="aubergine" onChange={handleCondimentChange} />
-            Aubergine
-          </label>
-          <label htmlFor="chili">
-            <input type="radio" id="chili" name="condiment" value="chili" onChange={handleCondimentChange} />
-            Chili &#127798;
-          </label>
-          <button type="button" onClick={handleNextButtonClick}>Next</button>
-        </>
-      )}
+        {stepCounter === 3 && (
+          <>
+            <h2>Which is your favorite tomato pairing?</h2>
+            <label htmlFor="cheese">
+              <input type="radio" id="cheese" name="condiment" value="cheese" onChange={handleCondimentChange} checked={condiment === 'cheese'} />
+              Cheeeeese!!
+            </label>
+            <label htmlFor="aubergine">
+              <input type="radio" id="aubergine" name="condiment" value="aubergine" onChange={handleCondimentChange} checked={condiment === 'aubergine'} />
+              Aubergine
+            </label>
+            <label htmlFor="chili">
+              <input type="radio" id="chili" name="condiment" value="chili" onChange={handleCondimentChange} checked={condiment === 'chili'} />
+              Chili &#127798;
+            </label>
+            <button type="button" onClick={handleNextButtonClick}>Next</button>
+          </>
+        )}
 
-      {stepCounter === 4 && (
-        <>
-          <h1>How spicy would you like your sauce?</h1>
-          <label htmlFor="A">
-            <input type="radio" id="A" name="spicy" value="notSpicy" onChange={handleSpicyChange} />
-            Not spicy
-          </label>
-          <label htmlFor="B">
-            <input type="radio" id="B" name="spicy" value="lagom" onChange={handleSpicyChange} />
-            Lagom
-          </label>
-          <label htmlFor="C">
-            <input type="radio" id="C" name="spicy" value="spicy" onChange={handleSpicyChange} />
-            Spicy!
-          </label>
-          <button type="button" onClick={handleNextButtonClick}>Next</button>
-        </>
-      )}
+        {stepCounter === 4 && (
+          <>
+            <h2>How spicy would you like your sauce?</h2>
+            <label htmlFor="A">
+              <input type="radio" id="A" name="spicy" value="not spicy" onChange={handleSpicyChange} checked={spicy === 'not spicy'} />
+              Not spicy
+            </label>
+            <label htmlFor="B">
+              <input type="radio" id="B" name="spicy" value="lagom" onChange={handleSpicyChange} checked={spicy === 'lagom'} />
+              Lagom
+            </label>
+            <label htmlFor="C">
+              <input type="radio" id="C" name="spicy" value="spicy" onChange={handleSpicyChange} checked={spicy === 'spicy'} />
+              Spicy!
+            </label>
+            <button type="button" onClick={handleNextButtonClick}>Next</button>
+          </>
+        )}
 
-      {stepCounter === 5 && (
-        <>
-          <label htmlFor="age">
-            Choose your age:
-            <select value={age} id="age" onChange={handleAgeChange}>
-              <option value="0-17">0-17</option>
-              <option value="18+">18+</option>
+        {stepCounter === 5 && (
+          <>
+            <h1>What would you like to sprinkle on top of your pomodoro?</h1>
+            {/* <label htmlFor="sprinkle">
+              Choose sprinkle */}
+            <select value={sprinkle} id="sprinkle" onChange={handleSprinkleChange}>
+              <option value="">Select Sprinkle</option>
+              <option value="Parmesan">Parmesan</option>
+              <option value="Basil">Basil</option>
+              <option value="Both">Both</option>
             </select>
-          </label>
-          <button type="button" onClick={handleNextButtonClick}>Next</button>
-        </>
-      )}
+            {/* </label> */}
+            <button type="button" onClick={handleNextButtonClick}>Next</button>
+          </>
+        )}
+      </div>
 
-      {stepCounter === 6 && (
-        <>
-          <div>
-            <h1>{userName}&apos;s Summary</h1>
-            <p>Preference: {preference}</p>
-            <p>Condiment: {condiment}</p>
-            <p>How spicy: {spicy}</p>
-            <p>Age: {age}</p>
-          </div>
-          <button type="button" onClick={handleNextButtonClick}>Generate Recipe!</button>
-        </>
-      )}
-    </div>
+      <div className="summary">
+        {stepCounter === 6 && (
+          <>
+            <div>
+              <h2>{userName}&apos;s Pomodoro summary </h2>
+              <p>Preference: {preference}</p>
+              <p>Condiment: {condiment}</p>
+              <p>How spicy: {spicy}</p>
+              <p>Sprinkle: {sprinkle}</p>
+            </div>
+            <h3>Let&apos;s get cookin&apos;! </h3>
+            <button type="button" onClick={handleNextButtonClick}>Generate Recipe!</button>
+          </>
+        )}
+      </div>
+    </section>
   );
 }
