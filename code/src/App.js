a3b7b0adda8e189790240a6e073120bfec17750d
YoTesfam <102465267+YoTesfam@users.noreply.github.com>
2022-09-29 22:37:36 +0200

    Greated a title, question, text-box, and a button

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FirstPage } from 'components/FirstPage';
+import { SecondPage } from 'components/SecondPage';
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <FirstPage />
+      <SecondPage />
     </div>
   );
-}
+};
