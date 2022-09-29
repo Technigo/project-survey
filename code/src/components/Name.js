a9d89dd500aadfd372d36abc13dc45f47cf5fcac
ViktorSvenssonN <viktor.tommy.svensson@gmail.com>
2022-09-29 19:56:24 +0200

    small change from h1 to label

--- code/src/components/Name.js
+++ code/src/components/Name.js
@@ -7,7 +7,6 @@ export const Name = ({ name, setName }) => {
   return (
     <div className="question-wrapper">
       <div className="question">
-        <h1>Welcome to the New York times recycle survey of the year ♻️</h1>
         <label htmlFor='Age'>What is your name?</label>
         <input type="text" value={name} onChange={handleNameChange} />
       </div>
