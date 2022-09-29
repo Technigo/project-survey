0db5c15742c9969f3451fe9bf3e1aa542bebce12
Caroline Söderström <carolinesoderstrom@Carolines-Air.home>
2022-09-30 01:29:21 +0200

    basic styling done started responsiveness

--- code/src/App.js
+++ code/src/App.js
@@ -44,13 +44,14 @@ export const App = () => {
             <p>Gone are the days of tasteless carbonara and dull pesto jars.
               We have created this pomodoro generator to help you take the first steps into
               the Queendom of the greatest sauce on earth, tomato sauce. There are endless
-              variations to explore and to avoid choice overload you will answer
+              variations to explore and to avoid choice overload, you will answer
               a few questions in this survey to help narrow down the options.
-              Start by entering your name below to find your perfect pomodoro match!
+            </p>
+            <p>Start by entering your name below and find your perfect pomodoro match! &#127813;
             </p>
             <div className="start">
               <input type="text" placeholder="Your Name" value={userName} onChange={handleUserNameInputChange} />
-              <button type="button" onClick={handleNextButtonClick}>Start!</button>
+              <button type="button" className="button" onClick={handleNextButtonClick}>Start!</button>
             </div>
           </>
         )}
@@ -60,18 +61,18 @@ export const App = () => {
         {stepCounter === 2 && (
           <>
             <h1>Hi {userName}!</h1>
-            <h2>Would you like your sauce..?</h2>
+            <h2>How would you like your sauce?</h2>
             <div className="radioContainer">
               <label htmlFor="meat">
-                <input type="radio" id="meat" name="preference" value="Meat" onChange={handlePreferenceInputChange} checked={preference === 'Meat'} />
+                <input type="radio" className="radio" id="meat" name="preference" value="Filled with pancetta!" onChange={handlePreferenceInputChange} checked={preference === 'Filled with pancetta!'} />
                 Filled with pancetta!
               </label>
               <label htmlFor="veg">
-                <input type="radio" id="veg" name="preference" value="Veg" onChange={handlePreferenceInputChange} checked={preference === 'Veg'} />
+                <input type="radio" className="radio" id="veg" name="preference" value="Vegetarian" onChange={handlePreferenceInputChange} checked={preference === 'Vegetarian'} />
                 Vegetarian please!
               </label>
             </div>
-            <button type="button" onClick={handleNextButtonClick}>Next</button>
+            <button type="button" className="button" onClick={handleNextButtonClick}>Next</button>
           </>
         )}
 
@@ -80,19 +81,19 @@ export const App = () => {
             <h2>Which is your favorite tomato pairing?</h2>
             <div className="radioContainer">
               <label htmlFor="cheese">
-                <input type="radio" id="cheese" name="condiment" value="cheese" onChange={handleCondimentChange} checked={condiment === 'cheese'} />
-                Cheeeeese!!
+                <input type="radio" className="radio" id="cheese" name="condiment" value="Cheese" onChange={handleCondimentChange} checked={condiment === 'Cheese'} />
+                 Cheeeeese!!
               </label>
               <label htmlFor="aubergine">
-                <input type="radio" id="aubergine" name="condiment" value="aubergine" onChange={handleCondimentChange} checked={condiment === 'aubergine'} />
-                Aubergine
+                <input type="radio" className="radio" id="aubergine" name="condiment" value="Aubergine" onChange={handleCondimentChange} checked={condiment === 'Aubergine'} />
+                 Aubergine
               </label>
               <label htmlFor="chili">
-                <input type="radio" id="chili" name="condiment" value="chili" onChange={handleCondimentChange} checked={condiment === 'chili'} />
-                Chili &#127798;
+                <input type="radio" className="radio" id="chili" name="condiment" value="Chili" onChange={handleCondimentChange} checked={condiment === 'Chili'} />
+                 Chili &#127798;
               </label>
             </div>
-            <button type="button" onClick={handleNextButtonClick}>Next</button>
+            <button type="button" className="button" onClick={handleNextButtonClick}>Next</button>
           </>
         )}
 
@@ -101,35 +102,37 @@ export const App = () => {
             <h2>How spicy would you like your sauce?</h2>
             <div className="radioContainer">
               <label htmlFor="A">
-                <input type="radio" id="A" name="spicy" value="not spicy" onChange={handleSpicyChange} checked={spicy === 'not spicy'} />
+                <input type="radio" className="radio" id="A" name="spicy" value="Not spicy" onChange={handleSpicyChange} checked={spicy === 'Not spicy'} />
                 Not spicy
               </label>
               <label htmlFor="B">
-                <input type="radio" id="B" name="spicy" value="lagom" onChange={handleSpicyChange} checked={spicy === 'lagom'} />
+                <input type="radio" className="radio" id="B" name="spicy" value="Lagom" onChange={handleSpicyChange} checked={spicy === 'Lagom'} />
                 Lagom
               </label>
               <label htmlFor="C">
-                <input type="radio" id="C" name="spicy" value="spicy" onChange={handleSpicyChange} checked={spicy === 'spicy'} />
-                Spicy!
+                <input type="radio" className="radio" id="C" name="spicy" value="Spicy" onChange={handleSpicyChange} checked={spicy === 'Spicy'} />
+                Spicy! &#128293;
               </label>
             </div>
-            <button type="button" onClick={handleNextButtonClick}>Next</button>
+            <button type="button" className="button" onClick={handleNextButtonClick}>Next</button>
           </>
         )}
 
         {stepCounter === 5 && (
           <>
-            <h1>What would you like to sprinkle on top of your pomodoro?</h1>
+            <h2>What would you like to sprinkle on top of your pomodoro?</h2>
+            <p>A meal is not complete until you add the topping! &#129472;</p>
             {/* <label htmlFor="sprinkle">
               Choose sprinkle */}
-            <select value={sprinkle} id="sprinkle" onChange={handleSprinkleChange}>
-              <option value="">Select Sprinkle</option>
+            {/* <div className="custom-select" style={{ width: '200px' }}> */}
+            <select value={sprinkle} className="sprinkle" id="sprinkle" onChange={handleSprinkleChange}>
+              <option value="">Select Topping</option>
               <option value="Parmesan">Parmesan</option>
               <option value="Basil">Basil</option>
-              <option value="Both">Both</option>
+              <option value="All the toppings!">All the toppings!</option>
             </select>
             {/* </label> */}
-            <button type="button" onClick={handleNextButtonClick}>Next</button>
+            <button type="button" className="button" onClick={handleNextButtonClick}>Next</button>
           </>
         )}
       </div>
@@ -144,8 +147,8 @@ export const App = () => {
               <p>How spicy: {spicy}</p>
               <p>Sprinkle: {sprinkle}</p>
             </div>
-            <h3>Let&apos;s get cookin&apos;! </h3>
-            <button type="button" onClick={handleNextButtonClick}>Generate Recipe!</button>
+            <h3>Let&apos;s get cookin&apos;! &#127813;</h3>
+            <button type="button" className="button" onClick={handleNextButtonClick}>Generate Recipe!</button>
           </>
         )}
       </div>
