da5ccd832c615e49b5ddc8857638164e1893845f
Scarylie <saralie.bognandi@gmail.com>
2022-09-29 10:01:21 +0200

    paragraph intro

--- code/src/components/Intro.js
+++ code/src/components/Intro.js
@@ -7,10 +7,8 @@ const Intro = ({ handleSubmit }) => {
     <div className="intro-container">
       <h1>Story Time</h1>
       <h2>The Dragon Sneeze</h2>
-      <p>
-        get carried away by an incredible mad lib story. But first, you need to
-        fill out the form.
-      </p>
+      <p>get carried away by an incredible mad lib story. </p>
+      <p>But first, you need to fill out the form.</p>
       <form onSubmit={handleSubmit}>
         <input type="submit" className="continue-btn" value="Start" />
       </form>
