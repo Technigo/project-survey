83467397f88e2f84a40128d8c2471bcc7a9712ae
MariaWellander <maria.wellander@gmail.com>
2022-09-30 15:36:30 +0200

    the base, example week 6

---
+++ code/src/components/Name.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Name = ({ name, setName }) => {
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  }
+  return (
+    <>
+      <p>What is your name?</p>
+      <input type="text" value={name} onChange={handleNameChange} />
+    </>
+  );
+}
