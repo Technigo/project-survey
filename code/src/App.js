6ff5720b507057dc091f579f6cb249b311034aaa
Jessika Lind <jessika@MacBook-Air-som-tillhor-Jessika.local>
2022-10-02 11:47:59 +0200

    generic component created and working

--- code/src/App.js
+++ code/src/App.js
@@ -4,7 +4,7 @@ import AgeQuestion from './Components/AgeQuestion';
 import FrequencyQuestion from './Components/FrequencyQuestion';
 import MuseumQuestion from './Components/MuseumQuestion';
 import Result from './Components/Result'
-import MemoryQuestion from './Components/MemoryQuestion'
+import TextAreaInput from './Components/TextAreaInput'
 import ProgressBar from './Components/ProgressBar'
 import Footer from 'Components/Footer'
 
@@ -15,6 +15,7 @@ export const App = () => {
   const [frequency, setFrequency] = useState('')
   const [museum, setMuseum] = useState('')
   const [memory, setMemory] = useState('')
+  const [improvements, setImprovements] = useState('')
 
 
   const handleStepIncrease = () => {
@@ -52,18 +53,44 @@ export const App = () => {
 
           {step === 5 && (
             <div>
-            <MemoryQuestion memory={memory} setMemory={setMemory} />
-            <button className="submit-btn" type="button" onClick={handleStepIncrease}>Finish survey</button>
+            <TextAreaInput 
+              headline="A memorable visit?"
+              input={memory}
+              setInput={setMemory}
+              inputLabel="We are interested in hearing if you have a special memory from a visit at one of Stockholm's
+              museums that you wish to share. Perhaps you remember a certain exhibit fondly, or have a mildly traumatic
+              experiece related to a boring visit?"
+              id="memorable-visit"
+              placeholder="A memorable visit..."
+              htmlFor="memorable-visit"
+              />
+              
             </div>
           )}
 
           {step === 6 && (
             <div>
-              <Result age={age} frequency={frequency} museum={museum} memory={memory} />
+            <TextAreaInput 
+              headline="Room for improvements!"
+              input={improvements} 
+              setInput={setImprovements} 
+              inputLabel="Lastly, we would highly appriciate if you would want to share your thoughts on what
+              could be improved when it comes to the museums of Stockholm"
+              id="improvements"
+              placeholder="My thoughts on measures that could be taken to improve Stockholm's museums..."
+              htmlFor="improvements"
+              />
+            <button className="submit-btn" type="button" onClick={handleStepIncrease}>Finish survey</button>
+            </div>
+          )} 
+
+          {step === 7 && (
+            <div>
+              <Result age={age} frequency={frequency} museum={museum} memory={memory} improvements={improvements}/>
             </div>
           )}
 
-          {step >= 2 && step < 6 && (
+          {step >= 2 && step < 7 && (
             <div>
               <ProgressBar 
                 step={step}
@@ -71,19 +98,19 @@ export const App = () => {
             </div>
           )}
 
-          {step < 5 && step > 1 && (
+          {step < 6 && step > 1 && (
             <div>
               <button className="continue-btn" type="button" onClick={handleStepIncrease}>Continue</button>
             </div>
           )}
 
-          {step >1 && step < 6 && (
+          {step >1 && step < 7 && (
             <div>
               <button className="back-btn" type="button" onClick={handleStepDecrease}>Go back</button>
             </div>
           )}   
 
-          {step >=6 && (
+          {step >=7 && (
             <div>
               <button className="reset-btn" type="button" onClick={handleStepReset}>Restart survey</button>
             </div>
---
+++ code/src/Components/TextAreaInput.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const TextAreaInput = ({headline, input, setInput, inputLabel, id, placeholder, htmlFor}) => {
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value)
+  }
+
+    return (
+      <>
+      <h2>{headline}</h2>
+        <form>
+          <label htmlFor={htmlFor}>
+            <p>{inputLabel}</p>
+            </label>
+            <textarea
+            type ="text"
+            id={id}
+            className="text-input"
+            placeholder={placeholder}
+            aria-live="polite"
+            onChange={handleInputChange}
+            value={input}
+            />
+        </form>
+      </>
+    );
+  }
+
+  export default TextAreaInput
