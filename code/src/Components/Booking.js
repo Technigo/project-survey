ae493820a0d9236484a42a47ed137e1577ab8155
jessnmo <jessicamo92@gmail.com>
2022-11-06 16:47:53 +0100

    fixed radio button

--- code/src/Components/Booking.js
+++ code/src/Components/Booking.js
@@ -1,86 +1,80 @@
+const Booking = ({ setWorkout, setLocation }) => {
+	const workouts = [
+		{
+			name: 'workout',
+			value: 'Holy Shred',
+			label: 'Holy Shred',
+		},
+		{
+			name: 'workout',
+			value: 'Holy Build',
+			label: 'Holy Build',
+		},
+		{
+			name: 'workout',
+			value: 'Holy Ride',
+			label: 'Holy Ride',
+		},
+		{
+			name: 'workout',
+			value: 'Holy Booty',
+			label: 'Holy Booty',
+		},
+		{
+			name: 'workout',
+			value: 'Open Gym',
+			label: 'Open Gym',
+		},
+	];
 
-const Booking = ({setWorkout, setLocation}) => {
+	const locations = [
+		{
+			name: 'location',
+			value: 'Oud Zuid',
+			label: 'Oud Zuid',
+		},
+		{
+			name: 'location',
+			value: 'Oud West',
+			label: 'Oud West',
+		},
+	];
 
-   const workouts = [
-    {
-        name:"holy-shred",
-        value:"Holy Shred",
-        label: "Holy Shred"
-    },
-    {
-        name:"holy-build",
-        value:"Holy Build",
-        label: "Holy Build"
-    },
-    {
-        name:"holy-ride",
-        value:"Holy Ride",
-        label: "Holy Ride"
-        },
-    {
-        name:"holy-shred",
-        value:"Holy Booty",
-        label: "Holy Booty"
-        }, 
-    {
-        name:"open-gym",
-        value:"Open Gym",
-        label: "Open Gym"
-        }
-     ]
+	return (
+		<>
+			<div className="booking">
+				{workouts.map((workout) => {
+					return (
+						<>
+							<input
+								type="radio"
+								name={workout.name}
+								value={workout.value}
+								onChange={(e) => setWorkout(e.target.value)}
+							/>
+							<label>{workout.label}</label>
+						</>
+					);
+				})}
+			</div>
 
-     const locations = [
-        {
-            name:"oud-zuid",
-            value:"Oud Zuid",
-            label:"Oud Zuid"
-        },
-        {
-            name:"oud-west",
-            value:"Oud West",
-            label:"Oud West"
-        }
-     ]
+			<div className="location">
+				{locations.map((location) => {
+					return (
+						<>
+							<input
+								type="radio"
+								name={location.name}
+								value={location.value}
+								onChange={(e) => setLocation(e.target.value)}
+							/>
+							<label>{location.label}</label>
+						</>
+					);
+				})}
+			</div>
+		</>
+	);
+};
 
-    return (
-        <>
-        <div className="booking">
-        {workouts.map(workout => {
-        return (
-            <> 
-            <input 
-            type="radio" 
-            name= {workout.name}
-            value= {workout.value}
-            onChange={e=>setWorkout(e.target.value)} />
-            <label>{workout.label}</label>
-            </>
-        )
-    })} 
-    </div>
-    
-    <div className="location">
-        {locations.map(location => {
-            return(
-                <>
-                <input type="checkbox" 
-                name={location.name}
-                value={location.value}
-                onChange={e=>setLocation(e.target.value)} />
-                <label>{location.label}</label>
-                </>
-            )
-        })}
-    </div>
-    </>
-
-    )
-}
- 
 export default Booking;
-
-
-
-
-
-
