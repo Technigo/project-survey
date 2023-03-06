b9b3f4236997c50632019a13be86571026713e15
malmen237 <linda.malm237@gmail.com>
2023-03-06 17:09:52 +0100

    Updated the footer.

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import piImg from '../pi.png';
+import linuxImg from '../linux.png';
 
 const Footer = () => {
   return (
     <>
-      <p className="footer-hide">This is a project made made by Linda Malm for educational purposes</p>
-      <p><a href="https://github.com/malmen237/project-survey">Watch the code in github</a></p>
+      <p className="footer-hide">This is a project made by Linda Malm for educational purposes</p>
+      <p><a href="https://github.com/malmen237/project-survey">Check out my code in GitHub</a></p>
       <p><a href="https://www.freepik.com/author/catalyststuff">Images by catalyststuff on Freepik</a></p>
+      <p>This page is hosted on a server that was built by me.</p>
+      <div className="image-wrapper">
+        <img src={piImg} className="pi-img" alt="raspberry pi logo" />
+        <img src={linuxImg} className="linux-img" alt="linux penguin logo" />
+      </div>
     </>
   );
 }
