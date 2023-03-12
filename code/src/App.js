e49d44965ee4168305e96ed663bee1a0362ba0bc
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-03-12 18:20:17 +0100

    erased testings

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Start } from 'Start';
 
 export const App = () => {
   return (
-    <div className="container">
-      <div className="survey-box">
-        <Start />
-      </div>
+    <div>
+      Find me in src/app.js!
     </div>
-  )
-}
+  );
+};
--- code/src/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Button = ({ btnText, btnClickFunction }) => {
-  return (
-    <button className="button" type="button" onClick={btnClickFunction}>{btnText}</button>
-  )
-}
