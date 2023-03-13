340199dc5312e8dcead405d1ca8901f001a6e7ef
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-03-13 21:06:10 +0100

    added button

--- code/src/App.js
+++ code/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from './components/Header'
+import SubmitButton from './components/Button'
 
 export const App = () => {
   return (
     <div>
       <Header />
+      <SubmitButton name="Submit" />
     </div>
   );
 }
--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import YesButton from './Button'
+import BeginButton from './Button'
 
 export const Header = () => {
   return (
     <section className="header">
       <h1 className="section-title">Would you dare to dream?</h1>
-      <YesButton name="OKey" />
+      <BeginButton name="Begin Button" />
     </section>
   );
 }
