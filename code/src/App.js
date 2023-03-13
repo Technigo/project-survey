b0e0ac6a474ae1621ae73be3185982c0b3c7b562
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-03-13 14:13:56 +0100

    adding images and form doc

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from 'react'
+
+import Form from 'components/Form';
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  );
-}
+  return <Form />;
+};
