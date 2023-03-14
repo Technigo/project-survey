3ac8e59adc2e6cf1bd79171fd592bf7a655dd7e1
SandraMadeleine <sandramohlsson@yahoo.com>
2023-03-14 22:36:07 +0100

    minor changes

--- code/src/App.js
+++ code/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 export const App = () => {
-  const [step, setStep] = useState(1)
+  const [step, setStep] = useState(1);
   return (
     <div className="outer-wrapper">
       <div className="inner-wrapper">
--- code/src/Components/purchase.js
+++ code/src/Components/purchase.js
@@ -1,20 +1,16 @@
 import React from 'react'
 
-const Purchases = [
-  '1-2',
-  '3-5',
-  '6-10',
-  '10+']
+const Purchases = ['1-2', '3-5', '6-10', '10+']
 console.log(Purchases)
 
-const PurchaseQuestion = ({purchase, setPurchase}) => {
+const PurchaseQuestion = ({ purchase, setPurchase }) => {
   const handleNumberPurchases = (event) => {
     setPurchase(event.target.value)
   }
 
   return (
     <>
-      <h2>How often per week do you purchase something to drink in a café?</h2>
+      <h2>How often per week do you purchase something to drink in a cafei?</h2>
       <form className="radio-buttons" id="radio-buttons">
         {Purchases.map(group => (
           <label key ={group}>
