ca17e4d96e02edf93649833f0ee2652f86b37ac8
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-03-14 21:03:09 +0100

    Deleted redundant files

--- code/src/Components/ProgressBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ProgressBar from 'react-bootstrap/ProgressBar'
-
-export const ProgressBar = (props) => {
-return <ProgressBar now={60} />;
-}
-      
-      
-      
