2b0564f7e3ed410f159576878538342b7223bcc1
Frid Matilda <matilda.x.frid@scania.com>
2023-03-14 12:05:41 +0100

    basic page

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,20 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable max-len */
+
 import React from 'react';
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="container">
+      <h1>Matildas TV support</h1>
+      <span>📺🍿</span>
+      <hr />
+      <p>Do you ever feel like your in the mood to cuddle down in the sofa with a bowl of snacks and put on a really nice tv-show?</p>
+      <p>But, when scrolling on Netflix you are overwhelmed by all the options available. You quickly call your friend and trusted tv-nerd Matilda to get help, but she is busy taking a nap and cannot respond.</p>
+      <p>Your bowl of snacks is looking at you, dripping with disappointment.</p>
+      <p>Fear no more! Matildas TV support is here to help you find a good show to put on. We are filled with only shows that Matilda has seen and would recommend. Answer a few questions and we will give you a suggestion.</p>
+      <p>Let's get started!</p>
+      <button type="button">Start</button>
     </div>
   );
 }
