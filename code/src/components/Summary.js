d0d1dcbe65812528f44352208566ae80eeb16a49
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-03-14 20:37:06 +0100

    fixed functionality of bot

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -7,7 +7,7 @@ export const Summary = (props) => {
       <p>What a unique personality you have {props.formData.name}!!</p>
       <p>
         <span> So you love to listen to {props.formData.artistName}</span>
-        <span>while you {props.formData.selectedHorseOption}</span>
+        <span>while you {props.formData.horseOptions}</span>
         <span> and you would like to {props.formData.selectedElephantOptions},</span>
         <span> all while making your life easier with your ability to
           {props.formData.SuperpowerOptions}.
