444da089ed73d501b69cd971ee503800461e4a41
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-03-15 09:40:13 +0100

    added header in name.js

--- code/src/components/Name.js
+++ code/src/components/Name.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Header } from './Header';
 
 export const Name = ({ name, setName }) => {
   const handleNameChange = (event) => {
     setName(event.target.value);
   }
   return (
-    <div className="inputbox">
-      <label htmlFor="name">
-        <p>Dare to dream? First type your name</p>
-        <input type="text" id="name" value={name} onChange={handleNameChange} required />
-      </label>
-    </div>)
+    <><Header />
+      <div className="inputbox">
+        <label htmlFor="name">
+          <p>Dare to dream? First type your name</p>
+          <input type="text" id="name" value={name} onChange={handleNameChange} required />
+        </label>
+      </div>
+    </>)
 }
 
