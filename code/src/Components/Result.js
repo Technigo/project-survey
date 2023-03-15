2b45c1f33229c3163a64027f2795e3aed12e9425
Andreas Axelsson <pererikandreas@gmail.com>
2023-03-15 08:20:48 +0100

    added new background and sizeing

--- code/src/Components/Result.js
+++ code/src/Components/Result.js
@@ -3,10 +3,11 @@ import React from 'react';
 export const Result = ({ name, food, drink, group, taste }) => {
   return (
     <>
+      <h1 className="nametext">Summery:</h1>
       <h1 className="resulttext">My favorite wine is {name}</h1>
-      <h1 className="resulttext">My favorite grape is {food} 🍇</h1>
-      <h1 className="resulttext">I want to spend {group} 💸</h1>
-      <h1 className="resulttext">I will eat {drink} to this wine 🍲</h1>
+      <h1 className="resulttext">My favorite grape is {food}</h1>
+      <h1 className="resulttext">I want to spend {group}€</h1>
+      <h1 className="resulttext">I will eat {drink} to this wine</h1>
       <h1 className="resulttext">My favorite tones {taste}</h1>
     </>
 
