d2c36c974976418da6ed25e72a787a40a1c25314
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-03-15 21:50:30 +0100

    Completed results component

--- code/src/Components/Results.js
+++ code/src/Components/Results.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import data from '../data';
+import { NextButton } from './NextButton';
 
 export const Results = (props) => {
-  data.listings.map((listing) => {
-    console.log(listing)
-    return (
-      <div className="results-container">
-        <h1>{props.resultsTitle}</h1>
-        <div className="result-card">
-          <img
-            className="listing-image"
-            src={listing.image}
-            alt="property listed in Stockholm" />
-          <p className="listing-info">{listing.listPrice} </p>
-          <p className="listing-info">{listing.namedAreas} </p>
-        </div>
-      </div>
-    );
-  });
+  return (
+    <div className="results-container">
+      <h1>{props.resultsTitle}</h1>
+      {
+        data.listings.map((listing) => {
+          return (
+            <div
+              className="result-card"
+              onClick={() => window.open(listing.url, '_blank')}
+              onKeyDown={() => window.open(listing.url, '_blank')}
+              role="link"
+              tabIndex={0}>
+              <img
+                className="listing-image"
+                src={listing.image}
+                alt="property listed in Stockholm" />
+              <p className="listing-info">{listing.listPrice} kr </p>
+              <p className="listing-info">{listing.livingArea} m<sup>2</sup> </p>
+              <p className="listing-info">{listing.location.namedAreas} </p>
+            </div>
+          );
+        })
+      }
+      <NextButton
+        onNext={props.onNext}
+        buttonLabel={props.restartButtonLabel} />
+    </div>
+  );
 };
