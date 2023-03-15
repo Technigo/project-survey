ea81975aea6c20bc6157bfc3a271579ae796de83
SandraMadeleine <sandramohlsson@yahoo.com>
2023-03-15 16:43:51 +0100

    buildning the page

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import Start from './Components/start'
-import Purchase from './Components/purchase'
 
 export const App = () => {
   const [step, setStep] = useState(1)
