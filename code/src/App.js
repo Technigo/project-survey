e707c44fef67eff564e5489701bce526d08dc50d
Emma Engvall <emma.engvall@ica.se>
2023-03-15 20:09:15 +0100

    updated app.js

--- code/src/App.js
+++ code/src/App.js
@@ -1,22 +1,55 @@
 import React, { useState } from 'react';
+import { Name } from 'components/Name';
+import { Size } from 'components/Size';
+import { Flavor } from 'components/Flavor';
+import { Topping } from 'components/Topping';
+import { Result } from 'components/Result';
 
 export const App = () => {
   const [step, setStep] = useState(1);
   const [name, setName] = useState('');
   const [size, setSize] = useState('');
   const [flavor, setFlavor] = useState('');
+  const [topping, setTopping] = useState('');
 
   const handleStepIncrease = () => {
     setStep(step + 1)
   }
 
   return (
-    <div>
-      The Popcorn picker 🍿!
+    <>
+      <h1>The Popcorn picker 🍿!</h1>
       <p>"You had me at popcorn..."</p>
-    </div>
+    
+    {step === 1 && (
+      <Name name={name} setName={setName} />
+    )}
+
+    {step === 2 && (
+      <Size size={size} setSize={setSize} />
+    )}
+
+    {step === 3 && (
+      <Flavor flavor={flavor} setFlavor={setFlavor} />
+    )}
+
+    {step === 4 && (
+      <Topping topping={topping} setTopping={setTopping} />
+    )}
+
+    {step >= 5 && (
+      <Result name={name} size={size} flavor={flavor} topping={topping} />
+    )}
+
+    {step < 5 && (
+      <>
+        <button type="button" onClick={handleStepIncrease}>OK</button>
+      </>
+    )}
+  </>
   );
 }
 
 //Submit button
 //Event.preventDefault
+//Add if/else if topping is chosen. 
--- code/src/components/Result.js
+++ code/src/components/Result.js
@@ -1,1 +1,10 @@
 import React from 'react';
+
+export const Result = ({ name, size, flavor, topping }) => {
+    return (
+        <>
+            <p>Your name is {name}.</p>
+            <p>You ordered a {size} {flavor} popcorn with {topping}.</p>
+        </>
+    ); 
+}
