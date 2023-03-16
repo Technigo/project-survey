fc4931e8eb1e4c042549122e27e2ab6cc2d1fea6
Emilia Saberski <emilia@saberski.com>
2023-03-16 15:18:01 +0100

    Adjusted the transistion slightly

--- code/src/components/AnimatedPage.jsx
+++ code/src/components/AnimatedPage.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 const animations = {
   initial: { opacity: 0, y: 100 },
-  animate: { opacity: 1, y: 20 },
+  animate: { opacity: 1, y: 50 },
   exit: { opacity: 0, y: -100 }
 };
 
