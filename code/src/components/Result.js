573509ee0e262df85c999f77a0617b13f1ce1994
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-03-16 15:24:52 +0100

    added code result

--- code/src/components/Result.js
+++ code/src/components/Result.js
@@ -3,8 +3,9 @@ import React from 'react';
 export const Result = ({ power, hero, name }) => {
   return (
     <div className="results">
-      <h3>What a dream <b>{name}</b></h3>
+      <h3>What a dream <b>{name}</b>!</h3>
       <p>Psst, let your soul <b>{hero}</b>, you have <b>{power}</b> powers.
+        <span role="img" className="emoji" aria-label="img">🫶</span>
       </p>
     </div>
   )
