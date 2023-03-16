163962fead313742b8958e257978016175937b9f
Frid Matilda <matilda.x.frid@scania.com>
2023-03-16 20:52:48 +0100

    grid

--- code/src/components/Starter.js
+++ code/src/components/Starter.js
@@ -9,20 +9,24 @@ const Starter = (props) => {
   }
 
   return (
-    <div className="container start">
-      <h1>Matildas TV support</h1>
-      <div className="emojibox">
-        <span role="img" className="emoji" aria-label="img">📺</span>
-        <span role="img" className="emoji" aria-label="img">🍿</span>
+    <div className="start-container">
+      <div className="start-heading">
+        <h1>Matildas TV support</h1>
+        <div className="emojibox">
+          <span role="img" className="emoji" aria-label="img">📺</span>
+          <span role="img" className="emoji" aria-label="img">🍿</span>
+        </div>
+        <hr />
       </div>
-      <hr />
-      <p>Do you ever feel like you're in the mood to cuddle down on the sofa with a bowl of snacks and put on a really nice tv-show?</p>
-      <p>But, when scrolling on Netflix, you are overwhelmed by all the options available. You quickly call your friend and trusted tv-nerd Matilda to get help, but she is busy taking a nap and cannot respond.</p>
-      <p>Your bowl of snacks is looking at you, dripping with disappointment.</p>
-      <p>Fear no more! Matilda's TV support is here to help you find a good show to put on. We are filled with only shows that Matilda has seen and would recommend. Answer a few questions, and we will give you a suggestion.</p>
-      <p>Let's get started!</p>
+      <div className="start-text">
+        <p>Do you ever feel like you're in the mood to cuddle down on the sofa with a bowl of snacks and put on a really nice tv-show?</p>
+        <p>But, when scrolling on Netflix, you are overwhelmed by all the options available. You quickly call your friend and trusted tv-nerd Matilda to get help, but she is busy taking a nap and cannot respond.</p>
+        <p>Your bowl of snacks is looking at you, dripping with disappointment.</p>
+        <p>Fear no more! Matilda's TV support is here to help you find a good show to put on. We are filled with only shows that Matilda has seen and would recommend. Answer a few questions, and we will give you a suggestion.</p>
+        <p>Let's get started!</p>
+      </div>
       <button className="startbtn" type="button" onClick={handleClick}>Start</button>
-      <img src="https://i.postimg.cc/gjF9yC0z/pexels-jeshootscom-1040159.jpg" alt="picy" />
+      <img src="https://i.postimg.cc/gjF9yC0z/pexels-jeshootscom-1040159.jpg" className="start-img" alt="woman watching tv" />
     </div>
   )
 };
