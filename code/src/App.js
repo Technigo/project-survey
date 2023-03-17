ee34ba22b8be205e5596a9926240a9f2ce586cfa
Emma Holtegaard <emmaholtegaard@gmail.com>
2023-03-17 19:28:54 +0100

    first commit. Created component-files and did the basic HTML in the App component.

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,25 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
+import Name from './components/Name'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <section className="main-container">
+      <header>
+        <h1>Survey</h1>
+      </header>
+      <div className="form-container">
+        <form className="form">
+          This is where the form will go. Mount components here.
+          <input type="text" />
+        </form>
+        <div className="button-container">
+          <button type="button">Next</button>
+        </div>
+      </div>
+      <footer>
+        <p>x/x</p>
+      </footer>
+    </section>
   );
 }
---
+++ code/src/components/Name.js
@@ -0,0 +1,3 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+
