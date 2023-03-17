bc621cdc90ef972a8f19a446ac8cec28979c4f00
EmiliaGA <emilia-lundmark@hotmail.com>
2023-03-17 13:57:53 +0100

    new try

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import myImage from './linkedin.png';
+import myOtherImage from './github.png';
 
 export const Summary = ({ name, points, bestThing, impression, answer }) => {
   return (
@@ -8,14 +10,7 @@ export const Summary = ({ name, points, bestThing, impression, answer }) => {
       <h3>The best thing about our site: {bestThing}</h3>
       <h3>The looks of our site is: {impression}</h3>
       <h3>Would you recommend it to a friend? {answer}</h3>
-    </div>
-  )
-}
-
-/*
-import myImage from './linkedin.png';
-import myOtherImage from './github.png';
-<div className="aboutme">
+      <div className="aboutme">
         <p>Made by Emilia G. Anundi @Technigo</p>
       </div>
       <div className="contactme">
@@ -23,4 +18,8 @@ import myOtherImage from './github.png';
           <a href="https://www.linkedin.com/in/emiliaga/"><img src={myImage} className="contact-logo" alt="linkedin link" /></a>
           <a href="https://github.com/EmiliaGA"><img src={myOtherImage} className="contact-logo" alt="github link" /></a>
         </div>
-      </div> */
+      </div>
+    </div>
+  )
+}
+
