4566757a207415e5f2c10e4795853d980fd289b1
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-03-17 22:49:58 +0100

    worked on responsiveness

--- code/src/Components/Result.js
+++ code/src/Components/Result.js
@@ -20,32 +20,27 @@ export const Result = ({
         What you like most about your workplace
         are the {adjective} {foodNoun}.
         In fact, the {adjective} {foodNoun} are a big part of who you are.
-        </p>
-        <p> If you could pick something to bring with you to a desert island that would
+        If you could pick something to bring with you to a desert island that would
         definitely be {adjective} {foodNoun}.
-        </p>
-        <p> In your spare time you enjoy {ingVerb} with your {clothingNoun} on,
+          <br />
+        In your spare time you enjoy {ingVerb} with your {clothingNoun} on,
         preferably in the company of {celebrity}.
-        </p>
-        <p> But let&#39;s be real here.</p>
-        <p>It&#39;s likely that you will end up {ingVerb} alone, since {celebrity} probably has more
+        But let&#39;s be real here.
+        It&#39;s likely that you will end up {ingVerb} alone, since {celebrity} probably has more
         important stuff to do than {ingVerb} with you.
-        </p>
-        <p>That would be really cool though.</p>
-        <p> If you were to change your career,
+        That would be really cool though.
+          <br />
+        If you were to change your career,
         you would probably end up in the {jobSector} industry.
+        There is a high demand for particularily {jobSector} jobs,
+        so that will probably work out really well for you.
+        Fun fact: Rumor has it that {celebrity} used to work in the {jobSector} industry before
+        getting famous.
+          <br />
+        You are most proud of your {happyAdjective} smile, your great {bodyPart} and
+        your ability to always {infinitiveVerb}.
+        Your previous boss would probably describe you as {descriptiveAdjective}.
         </p>
-        <p> There is a high demand for particularily {jobSector} jobs,
-      so that will probably work out really well for you.
-        </p>
-        <p> Fun fact: Rumor has it that {celebrity} used to work in the {jobSector} industry before
-      getting famous.
-        </p>
-
-        <p> You are most proud of your {happyAdjective} smile, your great {bodyPart} and
-       your ability to always {infinitiveVerb}.
-        </p>
-        <p> Your previous boss would probably describe you as {descriptiveAdjective}.</p>
       </div>
       <button aria-label="restart-button" className="restart-button" type="button" onClick={() => window.location.reload()}>Try again!</button>
 
