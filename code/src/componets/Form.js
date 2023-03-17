5eb1fedfdf225c7414cef68119d6041674d2f8d0
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-03-17 23:25:46 +0100

    added functionality to form

---
+++ code/src/componets/Form.js
@@ -0,0 +1,133 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-closing-bracket-location */
+import React, { useState } from 'react';
+
+export const Form = () => {
+  const [inputName, setInputName] = useState('');
+  const [inputRole, setInputRole] = useState('');
+  const [inputEnvironment, setInputEnvironment] = useState('');
+  const [inputExperience, setInputExperience] = useState(null);
+  const [formStage, setFormStage] = useState(0);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setFormStage(formStage + 1);
+    if (formStage >= 6) {
+      setFormStage(0);
+      setInputName('');
+      setInputRole('');
+      setInputEnvironment('');
+      setInputExperience('');
+    }
+  }
+
+  return (
+    <section>
+      <p>
+        {formStage < 5 ? formStage : 5}
+        <span>→</span>
+      </p>
+      <h1>Mentorship Sign Up</h1>
+      <form onSubmit={handleSubmit} required>
+        {formStage === 0 && (
+          <div>
+            <h2>
+              Welcome to the mentor sign up form, thankyou for showing intresst
+              in becoming a mentor, pleas press ok to continu in the form.
+            </h2>
+          </div>
+        )}
+        {formStage === 1 && (
+          <div>
+            <h1>What is your name?</h1>
+            <input
+              type="text"
+              value={inputRole}
+              onChange={(e) => {
+                setInputRole(e.target.value);
+                console.log(e.target.value);
+              }}
+              required
+            />
+          </div>
+        )}
+        {formStage === 2 && (
+          <div>
+            <h1>What is your current roll?</h1>
+            <select
+              onChange={(e) => {
+                setInputRole(e.target.value);
+              }}
+              value={inputRole}
+              required
+            >
+              <option value="">Pick a roll</option>
+              <option value="Developer">Developer</option>
+              <option value="HR">HR / Recruter</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+        )}
+        {formStage === 3 && (
+          <div>
+            <h1>Whats your preferd meeting eviroment??</h1>
+            <input
+              type="text"
+              value={inputEnvironment}
+              onChange={(e) => {
+                setInputEnvironment(e.target.value);
+                console.log(e.target.value);
+              }}
+              required
+            />
+          </div>
+        )}
+        {formStage === 4 && (
+          <div>
+            <h1>Have you been a mentor before?</h1>
+            <label htmlFor="yes">
+              <input
+                name="experience"
+                type="radio"
+                value="Yes"
+                onChange={(e) => {
+                  setInputExperience(e.target.value);
+                  console.log(e.target.value);
+                }}
+                checked={inputExperience === 'Yes'}
+                required
+              />
+              Yes
+            </label>
+            <label htmlFor="no">
+              <input
+                name="experience"
+                type="radio"
+                value="No"
+                onChange={(e) => {
+                  setInputExperience(e.target.value);
+                  console.log(e.target.value);
+                }}
+                checked={inputExperience === 'No'}
+                required
+              />
+              No
+            </label>
+          </div>
+        )}
+        {formStage === 5 && (
+          <div>
+            <p>Thank you {inputName} for siging up on this mentor program</p>
+            <p>Role: {inputRole}</p>
+            <p>Exp: {inputExperience}</p>
+            <p>Eviroment: {inputEnvironment}</p>
+          </div>
+        )}
+        {formStage === 6 && <h2>Thanks!</h2>}
+        {formStage < 5 && <button type="submit">Next »</button>}
+        {formStage === 5 && <button type="submit">Submit</button>}
+        <button type="submit">Restart Form</button>
+      </form>
+    </section>
+  );
+};
