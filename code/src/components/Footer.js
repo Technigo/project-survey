f53c3158fb0788e4e917e20ef294ed8ed9d0dc81
Bridget Mailley <bmailley42@gmail.com>
2023-03-17 21:10:52 +0100

    updated footer and credits

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -3,8 +3,6 @@ import React from 'react';
 export const Footer = () => {
   return (
     <footer>
-      <p>All recipes and images are from <a href="https://houseofnasheats.com/" target="_blank" rel="noopener noreferrer">House of Nash Eats</a></p>
-      <p>Icons are by Freepik at <a href="https://www.flaticon.com/" target="_blank" rel="noopener noreferrer">Flaticon</a></p>
       <div className="contact-me">
         <a href="https://www.linkedin.com/in/bridget-mailley/"><img src="./assets/linkedinlogo.png" className="contact-logo" alt="link to LinkedIn" /></a>
         <a href="https://bridgetmailley.com/"><img src="./assets/bridgetmailley.jpg" className="contact-logo" alt="link to Bridget's portfolio" /></a>
--- code/src/components/ResultsWrapper.js
+++ code/src/components/ResultsWrapper.js
@@ -7,6 +7,10 @@ export const ResultsWrapper = ({ region, flavor, season }) => {
       <h2>What a snack!</h2>
       <p>Congrats! You are...</p>
       <Results region={region} flavor={flavor} season={season} />
+      <div className="result-wrapper">
+        <p>All recipes and images are from <a href="https://houseofnasheats.com/" target="_blank" rel="noopener noreferrer">House of Nash Eats</a></p>
+        <p>Icons are by Freepik at <a href="https://www.flaticon.com/" target="_blank" rel="noopener noreferrer">Flaticon</a></p>
+      </div>
     </>
   )
 }
