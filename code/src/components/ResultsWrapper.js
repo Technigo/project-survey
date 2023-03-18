e56297bebe5714ebeb230b47a3fe0a4f312ec0c7
Bridget Mailley <bmailley42@gmail.com>
2023-03-18 09:55:53 +0100

    updated sizing and fixed footer to sit at bottom of page

--- code/src/components/ResultsWrapper.js
+++ code/src/components/ResultsWrapper.js
@@ -3,14 +3,12 @@ import { Results } from './Results'
 
 export const ResultsWrapper = ({ region, flavor, season }) => {
   return (
-    <>
+    <div className="result-wrapper">
       <h2>What a snack!</h2>
       <p>Congrats! You are...</p>
       <Results region={region} flavor={flavor} season={season} />
-      <div className="result-wrapper">
-        <p>All recipes and images are from <a href="https://houseofnasheats.com/" target="_blank" rel="noopener noreferrer">House of Nash Eats</a></p>
-        <p>Icons are by Freepik at <a href="https://www.flaticon.com/" target="_blank" rel="noopener noreferrer">Flaticon</a></p>
-      </div>
-    </>
+      <p>All recipes and images are from <a href="https://houseofnasheats.com/" target="_blank" rel="noopener noreferrer">House of Nash Eats</a></p>
+      <p>Icons are by Freepik at <a href="https://www.flaticon.com/" target="_blank" rel="noopener noreferrer">Flaticon</a></p>
+    </div>
   )
 }
