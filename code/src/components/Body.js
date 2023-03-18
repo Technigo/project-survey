687353ca7d9cd21ff8a3c99213b09b97595b6655
Sara Battilotti <sbattigliocchi@MBPdiSbgliocchi.homenet.telecomitalia.it>
2023-03-18 23:09:20 +0100

    all input types fixed

--- code/src/components/Body.js
+++ code/src/components/Body.js
@@ -3,15 +3,25 @@ import React from 'react';
 export const Body = ({ body, setBody }) => {
   return (
     <section>
-
-      <h1>Which one do you think is your best body part? </h1>
       <form>
-        <label htmlFor="heart">
-          <input type="radio" id="heart" value={body} onChange={(event) => setBody(event.target.value)} /> Heart
+        <h1>Which one do you think is your best body part? </h1>
+        <label>
+          <input
+            type="radio"
+            id="Heart"
+            value="Heart"
+            onChange={(event) => setBody(event.target.value)}
+            checked={body === 'Heart'} />
+        Heart
         </label>
-
-        <label htmlFor="brain">
-          <input type="radio" id="brain" value={body} onChange={(event) => setBody(event.target.value)} /> Brain
+        <label>
+          <input
+            type="radio"
+            id="Brain"
+            value="Brain"
+            onChange={(event) => setBody(event.target.value)}
+            checked={body === 'Brain'} />
+        Brain
         </label>
       </form>
     </section>
--- code/src/components/Combact.js
+++ code/src/components/Combact.js
@@ -4,8 +4,25 @@ export const Combact = ({ combact, setCombact }) => {
   return (
     <form>
       <h1> Which do you think is the most effective form of combat? </h1>
-      <label htmlFor="asia">
-        <input type="radio" id="asia" value={combact} onChange={(event) => setCombact(event.target.value)} checked />
+      <label>
+        <input
+          type="radio"
+          id="Judo"
+          value="Judo"
+          onChange={(event) => setCombact(event.target.value)}
+          checked={combact === 'Judo'}
+        />
+        Judo
+      </label>
+      <label>
+        <input
+          type="radio"
+          id="Kung-Fu"
+          value="Kung-Fu"
+          onChange={(event) => setCombact(event.target.value)}
+          checked={combact === 'Kung-Fu'}
+        />
+        Kung-Fu
       </label>
     </form>
   );
