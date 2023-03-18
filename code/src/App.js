10851ab3170e6c6eff5a8edce1c7f157545be25e
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-03-18 08:57:25 +0100

    Changed alt description

--- code/src/App.js
+++ code/src/App.js
@@ -4,7 +4,7 @@ import { Survey } from 'Components/Survey';
 export const App = () => {
   return (
     <div className="survey-container">
-      <img className="banner-image" src="https://images.unsplash.com/photo-1561223369-6e4b5e790972?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80" alt="a red house in the snow" />
+      <img className="banner-image" src="https://images.unsplash.com/photo-1561223369-6e4b5e790972?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80" alt="a red door" />
       <form>
         <Survey />
       </form>
