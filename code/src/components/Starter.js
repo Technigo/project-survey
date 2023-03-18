2ec4b3c3a2c42e45a151660f4e791dd054fac18c
Frid Matilda <matilda.x.frid@scania.com>
2023-03-18 22:11:12 +0100

    removed aria

--- code/src/components/Starter.js
+++ code/src/components/Starter.js
@@ -22,7 +22,7 @@ const Starter = (props) => {
         <p>Do you ever feel like you're in the mood to cuddle down on the sofa with a bowl of snacks and put on a really nice tv-show?</p>
         <p>But, when scrolling on Netflix, you are overwhelmed by all the options available. You quickly call your friend and trusted tv-nerd Matilda to get help, but she is busy taking a nap and cannot respond.</p>
         <p>Your bowl of snacks is looking at you, dripping with disappointment.</p>
-        <p>Fear no more! Matilda's TV support is here to help you find a good show to put on. We are filled with only shows that Matilda has seen and would recommend. Answer a few questions, and we will give you a suggestion.</p>
+        <p><span className="emphasis">Fear no more!</span> Matilda's TV support is here to help you find a good show to put on. We are filled with only shows that Matilda has seen and would recommend. Answer a few questions, and we will give you a suggestion.</p>
         <p>Let's get started!</p>
       </div>
       <button className="startbtn" type="button" onClick={handleClick} aria-label="Button named Start">Start</button>
