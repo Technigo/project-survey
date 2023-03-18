280efe1c55dba7520859898a3a5b4162f5966995
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-03-18 23:27:25 +0100

    save

--- code/src/componets/Form.js
+++ code/src/componets/Form.js
@@ -1,24 +1,21 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-closing-bracket-location */
 import React, { useState } from 'react';
+import './Form.css';
 
 const ProgressBar = ({ progress }) => {
   const container = {
-    height: 20,
+    height: 10,
     width: '100%',
-    backgroundColor: '#fff',
-    borderRadius: 50,
-    margin: 50
+    backgroundColor: '#ededed'
   };
   const bar = {
     height: '100%',
     width: `${progress}%`,
-    backgroundColor: '#90CAF9',
+    backgroundColor: '#4fb0ae',
     borderRadius: 'inherit'
-  };
-  const label = {
-    padding: '1rem',
-    color: '#000000'
   };
   return (
     <div style={container}>
@@ -28,9 +25,7 @@ const ProgressBar = ({ progress }) => {
         aria-valuenow={progress}
         aria-valuemin={0}
         aria-valuemax={100}
-      >
-        <span style={label}>{`${progress}%`}</span>
-      </div>
+      />
     </div>
   );
 };
@@ -40,128 +35,173 @@ export const Form = () => {
   const [inputRole, setInputRole] = useState('');
   const [inputEnvironment, setInputEnvironment] = useState('');
   const [inputExperience, setInputExperience] = useState(null);
+  const [inputTime, setInputTime] = useState(0);
   const [formStage, setFormStage] = useState(0);
+  const [percent, setPercent] = useState(0);
 
-  function handleSubmit(event) {
+  const resetForm = () => {
+    setFormStage(0);
+    setInputName('');
+    setInputRole('');
+    setInputEnvironment('');
+    setInputExperience('');
+    setPercent(0);
+  };
+
+  const handleSubmit = (event) => {
     event.preventDefault();
     setFormStage(formStage + 1);
-    if (formStage >= 6) {
-      setFormStage(0);
-      setInputName('');
-      setInputRole('');
-      setInputEnvironment('');
-      setInputExperience('');
-    }
-  }
+    const correctPercent = (formStage * 100) / 6;
+    setPercent(Number(correctPercent).toFixed(1));
+  };
 
   return (
     <section>
-      <p>
-        {formStage < 5 ? formStage : 5}
-        <span>→</span>
-      </p>
-      <ProgressBar progress={60} />
-      <h1>Mentorship Sign Up</h1>
-      <form onSubmit={handleSubmit} required>
-        {formStage === 0 && (
-          <div>
-            <h2>
-              Welcome to the mentor sign up form, thankyou for showing intresst
-              in becoming a mentor, pleas press ok to continu in the form.
-            </h2>
-          </div>
+      {formStage > 0 && <ProgressBar progress={percent} />}
+
+      <div>
+        <div className="title">
+          <p>
+            {formStage < 5 ? formStage : 5}
+            <span>→</span>
+          </p>
+
+          <h1>Mentorship Sign Up</h1>
+        </div>
+        {formStage >= 1 && (
+          <button className="reset-button" type="button" onClick={resetForm}>
+            Restart Form
+          </button>
         )}
-        {formStage === 1 && (
-          <div>
-            <h1>What is your name?</h1>
-            <input
-              type="text"
-              value={inputRole}
-              onChange={(e) => {
-                setInputRole(e.target.value);
-                console.log(e.target.value);
-              }}
-              required
-            />
-          </div>
-        )}
-        {formStage === 2 && (
-          <div>
-            <h1>What is your current roll?</h1>
-            <select
-              onChange={(e) => {
-                setInputRole(e.target.value);
-              }}
-              value={inputRole}
-              required
-            >
-              <option value="">Pick a roll</option>
-              <option value="Developer">Developer</option>
-              <option value="HR">HR / Recruter</option>
-              <option value="Other">Other</option>
-            </select>
-          </div>
-        )}
-        {formStage === 3 && (
-          <div>
-            <h1>Whats your preferd meeting eviroment??</h1>
-            <input
-              type="text"
-              value={inputEnvironment}
-              onChange={(e) => {
-                setInputEnvironment(e.target.value);
-                console.log(e.target.value);
-              }}
-              required
-            />
-          </div>
-        )}
-        {formStage === 4 && (
-          <div>
-            <h1>Have you been a mentor before?</h1>
-            <label htmlFor="yes">
+        <form onSubmit={handleSubmit} required>
+          {formStage === 0 && (
+            <div>
+              <h2>
+                Welcome to the mentor sign up survay, thankyou for showing
+                intresst in becoming a mentor, pleas press ok to continu in the
+                form. Please take a fwe minutes to fill in this survay to start
+                your journey in becoming a mentor
+              </h2>
+            </div>
+          )}
+          {formStage === 1 && (
+            <div>
+              <h1>What is your name? *</h1>
               <input
-                name="experience"
-                type="radio"
-                value="Yes"
+                type="text"
+                value={inputName}
                 onChange={(e) => {
-                  setInputExperience(e.target.value);
-                  console.log(e.target.value);
+                  setInputName(e.target.value);
                 }}
-                checked={inputExperience === 'Yes'}
                 required
               />
-              Yes
-            </label>
-            <label htmlFor="no">
-              <input
-                name="experience"
-                type="radio"
-                value="No"
+            </div>
+          )}
+          {formStage === 2 && (
+            <div>
+              <h1>What is your current roll? *</h1>
+              <select
                 onChange={(e) => {
-                  setInputExperience(e.target.value);
-                  console.log(e.target.value);
+                  setInputRole(e.target.value);
                 }}
-                checked={inputExperience === 'No'}
+                value={inputRole}
+                required
+              >
+                <option value="">Pick a roll</option>
+                <option value="Developer">Developer</option>
+                <option value="HR">HR / Recruter</option>
+                <option value="Other">Other</option>
+              </select>
+            </div>
+          )}
+          {formStage === 3 && (
+            <div>
+              <h1>What time commitment can I make? /Hours a month/ *</h1>
+              <input
+                type="range"
+                min="1"
+                max="100"
+                value={inputTime}
+                onChange={(e) => {
+                  setInputTime(e.target.value);
+                }}
                 required
               />
-              No
-            </label>
-          </div>
-        )}
-        {formStage === 5 && (
-          <div>
-            <p>Thank you {inputName} for siging up on this mentor program</p>
-            <p>Role: {inputRole}</p>
-            <p>Exp: {inputExperience}</p>
-            <p>Eviroment: {inputEnvironment}</p>
-          </div>
-        )}
-        {formStage === 6 && <h2>Thanks!</h2>}
-        {formStage < 5 && <button type="submit">Next »</button>}
-        {formStage === 5 && <button type="submit">Submit</button>}
-        {formStage >= 1 && <button type="submit">Restart Form</button>}
-      </form>
+              <p>Time I can commit: {inputTime} h/month</p>
+            </div>
+          )}
+          {formStage === 4 && (
+            <div>
+              <h1>Whats your preferd meeting eviroment? *</h1>
+              <input
+                type="text"
+                value={inputEnvironment}
+                onChange={(e) => {
+                  setInputEnvironment(e.target.value);
+                }}
+                required
+              />
+            </div>
+          )}
+          {formStage === 5 && (
+            <div>
+              <h1>Have you been a mentor before? *</h1>
+              <label htmlFor="yes">
+                <input
+                  name="experience"
+                  type="radio"
+                  value="Yes"
+                  onChange={(e) => {
+                    setInputExperience(e.target.value);
+                    console.log(e.target.value);
+                  }}
+                  checked={inputExperience === 'Yes'}
+                  required
+                />
+                Yes
+              </label>
+              <label htmlFor="no">
+                <input
+                  name="experience"
+                  type="radio"
+                  value="No"
+                  onChange={(e) => {
+                    setInputExperience(e.target.value);
+                    console.log(e.target.value);
+                  }}
+                  checked={inputExperience === 'No'}
+                  required
+                />
+                No
+              </label>
+            </div>
+          )}
+          {formStage === 6 && (
+            <div>
+              <p>Thank you {inputName} for siging up on this mentor program</p>
+              <p>Role: {inputRole}</p>
+              <p>Commited time: {inputTime} h/month</p>
+              <p>Exp: {inputExperience}</p>
+              <p>Eviroment: {inputEnvironment}</p>
+            </div>
+          )}
+          {formStage === 7 && (
+            <h2>Thank you, we will contact you when a match is found ❤️</h2>
+          )}
+          {formStage < 6 && (
+            <div>
+              <button className="submit-button next" type="submit">
+                Ok ✓
+              </button>
+            </div>
+          )}
+          {formStage === 6 && (
+            <button className="submit-button submit" type="submit">
+              Submit
+            </button>
+          )}
+        </form>
+      </div>
     </section>
   );
 };
