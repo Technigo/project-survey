d7f3f902f014c22e77a84b0ce83eec2320104821
fannystenberg <ffannystenberg@gmail.com>
2023-03-19 14:05:45 +0100

    added vaildation, footer and updated readme

---
+++ code/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="contact">
+      <p>CREATED BY FANNY STENBERG<br />STUDENT @ TECHNIGO 2023</p>
+      <button
+        type="button"
+        className="contactMe"
+        onClick={() => window.open('https://fannystenberg.netlify.app/')}>
+          Contact
+      </button>
+    </div>
+  )
+};
+
+export default Footer;
