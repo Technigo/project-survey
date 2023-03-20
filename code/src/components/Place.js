03f8d603c89b5d271d2f478162799c705a1e020b
Andrea <andrea@Andreas-MacBook-Air.local>
2023-03-20 12:48:32 +0100

    changed place-text

--- code/src/components/Place.js
+++ code/src/components/Place.js
@@ -7,7 +7,7 @@ export const Place = ({ place, setPlace }) => {
   return (
     <>
       <div className="question">
-        <h2>Were in the world would you like to be in your dream?</h2>
+        <h2>Where in the world would you like to be in your dream?</h2>
       </div>
       <div className="answer-input">
         <input type="text" value={place} onChange={handlePlaceChange} />
