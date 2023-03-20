88d6ee59b50f27de8d09566215068ff7c859ca12
Peki <112956568+akipeki@users.noreply.github.com>
2023-03-20 15:23:48 +0200

    Add files via upload

---
+++ code/src/components/Name.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+export const Name = ({ name, setName }) => {
+  const handleNameChange = (event) => {
+    const limit = 10;
+    setName(event.target.value.slice(0, limit));
+  };
+  return (
+    <div className="page-one-container">
+      <h4 className="page-one-title">Bench Press</h4>
+      <p className="page-one-sub-title">1 RM</p>
+      <p className="page-two-question">What is your name?</p>
+      <input className="input-field" type="text" value={name} onChange={handleNameChange} />
+    </div>
+  );
+}
+
+Name.propTypes = {
+  name: PropTypes.string.isRequired
+
+}
+
+export default Name
