0d6eee371aef556641f9cbe6fb34ba883928e0b3
AntoniaGranit <antonia.granit@gmail.com>
2023-03-20 18:07:03 +0100

    final commit

--- code/src/Components/Header.js
+++ code/src/Components/Header.js
@@ -5,7 +5,7 @@ import './Header.css';
 export const Header = () => {
   return (
     <div className="survey-header">
-      <h1>THE AFROBEAT<strong className="green"><a className="title-link" href="https://gilox.co/difference-between-afrobeats-and-afrobeat/" target="_blank" rel="noreferrer">S</a></strong> SURVEY</h1>
+      <h1>THE AFROBEAT<strong className="green"><a className="title-link" href="https://gilox.co/difference-between-afrobeats-and-afrobeat/" target="_blank" rel="noreferrer">S</a></strong> QUIZ</h1>
     </div>
   )
 }
--- code/src/Components/Name.js
+++ code/src/Components/Name.js
@@ -12,7 +12,16 @@ export const Name = ({ step, name, setName, handleStepIncrease }) => {
   return (
     <div>
       <form>
-        <p>Let's begin! What is your name?</p>
+        <h2>Hello there! </h2>
+        <p>
+          Are you on the lookout for new music? Are you perhaps interested in learning more
+          about West African (mainly Nigerian and Ghanaian) popular music?
+          If so, then you have come to the right place! This short quiz will match you with
+          one of four playlists that have been curated according to your musical preferences.
+        </p>
+        <p>
+          Let's begin! What is your name?
+        </p>
         <span className="questionTitle">Name: </span><input
           type="text"
           value={name}
