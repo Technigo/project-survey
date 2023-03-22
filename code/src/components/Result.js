2e0e77e2fd3c2ed735b3968b663a1819767dd8ae
tess.aquilon <tess.aquilon@gmail.com>
2023-03-22 13:11:09 +0100

    did some more styling

--- code/src/components/Result.js
+++ code/src/components/Result.js
@@ -6,8 +6,9 @@ import React from 'react';
 export const Result = ({ name, animal, job, color, verb, adjective }) => {
   return (
     <>
-      <p className="typewriter">Here you go! </p>
-      <p>The {animal} wanted to be a {job}, but turned a funny shade of {color} after trying to {verb}! What a {adjective} shame.
+      <p className="typewriter">Here we go! </p>
+      <p>Your own personal MADLIBS is:</p>
+      <p className="result-txt">The {animal} wanted to be a {job}, but turned a funny shade of {color} after trying to {verb}! What a {adjective} shame.
       </p>
       <p>Thank you for playing, {name}.</p>
     </>
