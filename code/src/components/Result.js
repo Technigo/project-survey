5a4f3404ca6e6b61b658f686333cf74813b4ce61
tess.aquilon <tess.aquilon@gmail.com>
2023-03-22 14:54:46 +0100

    started on the media queries

--- code/src/components/Result.js
+++ code/src/components/Result.js
@@ -4,13 +4,17 @@
 import React from 'react';
 
 export const Result = ({ name, animal, job, color, verb, adjective }) => {
+  const handleReloadClick = () => {
+    window.location.reload();
+  }
   return (
     <>
       <p className="typewriter">Here we go! </p>
-      <p>Your own personal MADLIBS is:</p>
+      <p>Your own personal <span className="madlibs-txt">MADLIBS</span> is:</p>
       <p className="result-txt">The {animal} wanted to be a {job}, but turned a funny shade of {color} after trying to {verb}! What a {adjective} shame.
       </p>
-      <p>Thank you for playing, {name}.</p>
+      <p>Thank you for playing, {name}. Want to play again?</p>
+      <button id="restart-btn" type="button" onClick={handleReloadClick}>Start over</button>
     </>
   )
 }
