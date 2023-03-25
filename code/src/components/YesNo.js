b1a7b3a175c8d28ad6e2006ff226d7e05c144153
Lina Adamsson <studio@adamsson.com>
2023-03-25 14:32:40 +0100

    Added labels.

--- code/src/components/YesNo.js
+++ code/src/components/YesNo.js
@@ -9,20 +9,22 @@ export const YesNo = ({ yesno, setYesNo }) => {
       <div className="question">Have you heard of the Bouba Kiki Effect?</div>
       <div className="radio-buttons">
         <form>
-          <input
-            type="radio"
-            id="answer-yes"
-            name="answer"
-            value="Yes"
-            onChange={handleYesNo}
-            checked={yesno === 'Yes'} />Yes
-          <input
-            type="radio"
-            id="answer-no"
-            name="answer"
-            value="No"
-            onChange={handleYesNo}
-            checked={yesno === 'No'} />No
+          <label for="answer-yes">Yes
+            <input type="radio"
+              id="answer-yes"
+              name="answer"
+              value="Yes"
+              onChange={handleYesNo}
+              checked={yesno === 'Yes'} />
+          </label>
+          <label for="answer-no">No
+            <input type="radio"
+              id="answer-no"
+              name="answer"
+              value="No"
+              onChange={handleYesNo}
+              checked={yesno === 'No'} />
+          </label>
         </form>
       </div>
     </div>
