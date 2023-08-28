1ed9b073e4e636567f884e0fb04a581815de9ca0
augustaquilon <august.aquilon@gmail.com>
2023-08-28 16:38:37 +0200

    started reworking the whole project

--- code/src/App.js
+++ code/src/App.js
@@ -1,62 +1,19 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable max-len */
-import React, { useState } from 'react';
-import { Header } from 'components/Header';
-import { Name } from './components/Name';
-import { Animal } from './components/Animal';
-import { Color } from './components/Color';
-import { Result } from './components/Result';
-import { Job } from './components/Job';
-import { Verb } from './components/Verb';
-import { Adjective } from './components/Adjective';
+import React from 'react';
+import { Button } from 'components/Button';
 
 export const App = () => {
   const [step, setStep] = useState(1);
-  const [name, setName] = useState('');
-  const [animal, setAnimal] = useState('');
-  const [job, setJob] = useState('');
-  const [color, setColor] = useState('');
-  const [verb, setVerb] = useState('');
-  const [adjective, setAdjective] = useState('');
-  const handleStepIncrease = () => {
-    setStep(step + 1);
-  }
+    const handleStepIncrease = () => {
+        setStep(step + 1);
+      }
   return (
     <section className="outer-wrapper">
       <section className="inner-wrapper">
-        <Header />
         {(step === 1) && (
-          <>
-            <Name name={name} setName={setName} />
-            <button type="button" className="next-btn" onClick={handleStepIncrease}> </button>
-          </>
+          <Button onClick={handleStepIncrease}/>
         )}
-        {(step === 2) && (
-          <Animal animal={animal} setAnimal={setAnimal} />
-        )}
-        {step === 3 && (
-          <Job job={job} setJob={setJob} />
-        )}
-        {step === 4 && (
-          <Color color={color} setColor={setColor} />
-        )}
-        {step === 5 && (
-          <Verb verb={verb} setVerb={setVerb} />
-        )}
-        {step === 6 && (
-          <Adjective adjective={adjective} setAdjective={setAdjective} />
-        )}
-        {step === 7 && (
-          <Result name={name} animal={animal} job={job} color={color} verb={verb} adjective={adjective} />
-        )}
-        {step >= 2 && step < 7 ? (
-          <section className="counter">
-            <button className="next-btn" type="button" onClick={handleStepIncrease}> </button>
-            <p>
-              Current step: {step}/6
-            </p>
-          </section>
-        ) : null}
       </section>
     </section>
   );
---
+++ code/src/components/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+export const Button = () => {
+    return(
+        <button type="button" className="advance-btn">Next</button>
+    )  
+}
+
