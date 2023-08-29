606c12edd8377588534be2bea5bc17248d6e0da1
augustaquilon <august.aquilon@gmail.com>
2023-08-29 13:59:58 +0200

    submit button functionality

--- code/src/components/SubmitButton.js
+++ code/src/components/SubmitButton.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Popup from './Popup';
 
-const SubmitButton = () => {
-  return <button type="submit" className="submit-button">Submit form</button>
+const SubmitButton = ({ currentState, defaultState, setSection, message }) => {
+  const [popup, setPopup] = useState(false);
+  const handleSubmitButton = () => {
+    if (currentState !== defaultState) {
+      setSection('summary');
+    } else {
+      setPopup(true);
+    }
+  };
+
+  return (
+    <>
+      {popup && <Popup message={message} />}
+      <button
+        type="submit"
+        className="submit-button"
+        onClick={handleSubmitButton}>Submit form
+      </button>
+    </>
+  )
 }
 
 export default SubmitButton;
