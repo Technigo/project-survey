0097cec64cc86923b174cab4a07ea9bbc09a631b
augustaquilon <august.aquilon@gmail.com>
2023-08-29 13:21:50 +0200

    working on the questions in the form

--- code/src/components/SurveyForm.js
+++ code/src/components/SurveyForm.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 export const SurveyForm = () => {
   const [firstName, setFirstName] = useState(''); // Declare a state variable...
   const [selectedOption, setSelectedOption] = useState('');
+  const [otherPlatforms, setOtherPlatforms] = useState('');
   const [comment, setComment] = useState('');
   const handleSubmit = (e) => {
     // Prevent the browser from reloading the page
@@ -67,6 +68,13 @@ export const SurveyForm = () => {
               value="56-65"
               id="56-65" /> 56-65
           </label>
+          <label htmlFor="secret">
+            <input
+              type="radio"
+              name="age"
+              value="secret"
+              id="secret" /> I prefer not to say
+          </label>
         </p>
         <label htmlFor="dropdownMenu">
           How often do you listen to music?
@@ -114,12 +122,28 @@ export const SurveyForm = () => {
           <label htmlFor="checkbox5">
             <input
               type="checkbox"
-              name="other"
+              name="otherPlatforms"
               id="checkbox5"
               defaultChecked={false} />Other:
           </label>
+          <label htmlFor="otherPlatformsTextInput">
+            <input
+              type="text"
+              value={otherPlatforms}
+              onChange={(e) => setOtherPlatforms(e.target.value)}
+              placeholder="Type other platforms here please..."
+              id="otherPlatformsTextInput"
+              autoComplete="off" />
+          </label>
+          <label htmlFor="checkbox6">
+            <input
+              type="checkbox"
+              name="noPlatforms"
+              id="checkbox6"
+              defaultChecked={false} />None
+          </label>
         </p>
-        <textarea name="comment" placeholder="Comment" value={comment} onChange={(e) => setComment(e.target.value)} />
+        <textarea name="comment" placeholder="Comment" value={comment} onChange={(e) => setComment(e.target.value)} autoComplete="off" />
         {comment !== '' && <p>Here is what you think: {comment}</p>}
         <button type="submit" className="submit-button">Submit form</button>
       </form>
