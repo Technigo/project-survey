7d55b7edaab5e2702047f7f03499bd3c477d3d2f
augustaquilon <august.aquilon@gmail.com>
2023-08-29 11:22:57 +0200

    imported some jazz music

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import backgroundMusic from 'assets/music/Jazz Night - Golden Age Radio.mp3';
 
 export const Header = () => {
+  // A variable importing the music file outside of the App function
+  // because the toggling didn't work inside it for some reason:
+  const bgMusic = new Audio(backgroundMusic);
+  bgMusic.loop = true;
+
+  const audioRef = useRef(bgMusic);
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  // This little function plays the music when pressing the music button,
+  // and turns it off when pressing again.
+  const togglePlay = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(true);
+    }
+  }
   return (
     <div className="header">
+      <button type="submit" className="music-btn" onClick={togglePlay}><span className="music-emoji">&#127925;</span></button>
       <h1>Survey Form</h1>
       <p>Description</p>
     </div>
