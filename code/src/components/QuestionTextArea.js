cf37d0d00c5ee093f4eb0c6e02c0b62466166dbf
augustaquilon <august.aquilon@gmail.com>
2023-08-30 11:41:44 +0200

    created a summary file

--- code/src/components/QuestionTextArea.js
+++ code/src/components/QuestionTextArea.js
@@ -7,10 +7,15 @@ import PreviousButton from './PreviousButton';
 const QuestionTextArea = ({ favoriteSong, setFavoriteSong, section, setSection }) => {
   return (
     <>
+      <p>What&apos;s your favorite song?</p>
       <textarea
         name="favoriteSong"
         placeholder="What is it called? What genre(s) does it belong to? Why do you like it so much? Do you have any special memories connected to it?"
         value={favoriteSong}
+        rows={10}
+        cols={42}
+        minLength={10}
+        maxLength={200}
         onChange={(e) => setFavoriteSong(e.target.value)}
         autoComplete="off" />
       <PreviousButton
@@ -19,8 +24,8 @@ const QuestionTextArea = ({ favoriteSong, setFavoriteSong, section, setSection }
         setSection={setSection} />
       <NextButton
         whatQuestionNext="summary"
-        defaultState=""
-        currentState={favoriteSong}
+        defaultState={0}
+        currentState={favoriteSong.length}
         message="Just...something?"
         section={section}
         setSection={setSection} />
