1dec754745f2848365d75f04d91a330f314b6d11
Malin <malin@MacBook-Pro-4.local>
2023-08-30 10:23:08 +0200

    removed overflow:hidden on body, think this was the problem

--- code/src/components/ThankYouPage.js
+++ code/src/components/ThankYouPage.js
@@ -6,7 +6,7 @@ export const ThankYouPage = () => {
     <div className="thank-you-page">
       <h1>Thank you!</h1>
       <h2>Have a nice day!</h2>
-      <button className="start-over-btn-last-page" type="button" onClick={() => window.location.reload()}>Leave quiz</button>
+      <button className="start-over-btn-last-page" type="button" onClick={() => window.location.reload()}>Start over</button>
     </div>
   )
 }
