750657b7f89f4a0f3e440092619edd2b20bdc8ce
augustaquilon <august.aquilon@gmail.com>
2023-09-01 12:50:09 +0200

    custom select styles start

--- code/src/components/QuestionDropdownGenre.js
+++ code/src/components/QuestionDropdownGenre.js
@@ -11,37 +11,35 @@ const QuestionDropdownGenre = ({ section, setSection, genre, setGenre }) => {
     <>
       <label htmlFor="selectGenre" className="form__question">
         What is a genre you like to listen to?
-        <div className="question__content-wrapper">
-          <select
-            value={genre}
-            onChange={(e) => setGenre(e.target.value)}
-            name="selectedGenre"
-            id="selectGenre">
-            <option value="Pop">Pop</option>
-            <option value="Hiphop">Hiphop</option>
-            <option value="Rock">Rock</option>
-            <option value="Rhythm and blues">Rhythm and blues</option>
-            <option value="Soul">Soul</option>
-            <option value="Funk">Funk</option>
-            <option value="Reggae">Reggae</option>
-            <option value="Ska">Ska</option>
-            <option value="Country">Country</option>
-            <option value="Folk">Folk</option>
-            <option value="Middle eastern">Middle Eastern</option>
-            <option value="Jazz">Jazz</option>
-            <option value="Disco">Disco</option>
-            <option value="Classical">Classical</option>
-            <option value="Electronic">Electronic</option>
-            <option value="Latin American">Latin American</option>
-            <option value="Blues">Blues</option>
-            <option value="African">African</option>
-            <option value="Asian">Asian</option>
-            <option value="Traditional">Traditional</option>
-            <option value="Christian">Christian</option>
-            <option value="Indie">Indie</option>
-            <option value="New-age">New-age</option>
-          </select>
-        </div>
+        <select
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+          name="selectedGenre"
+          id="selectGenre">
+          <option value="Pop">Pop</option>
+          <option value="Hiphop">Hiphop</option>
+          <option value="Rock">Rock</option>
+          <option value="Rhythm and blues">Rhythm and blues</option>
+          <option value="Soul">Soul</option>
+          <option value="Funk">Funk</option>
+          <option value="Reggae">Reggae</option>
+          <option value="Ska">Ska</option>
+          <option value="Country">Country</option>
+          <option value="Folk">Folk</option>
+          <option value="Middle eastern">Middle Eastern</option>
+          <option value="Jazz">Jazz</option>
+          <option value="Disco">Disco</option>
+          <option value="Classical">Classical</option>
+          <option value="Electronic">Electronic</option>
+          <option value="Latin American">Latin American</option>
+          <option value="Blues">Blues</option>
+          <option value="African">African</option>
+          <option value="Asian">Asian</option>
+          <option value="Traditional">Traditional</option>
+          <option value="Christian">Christian</option>
+          <option value="Indie">Indie</option>
+          <option value="New-age">New-age</option>
+        </select>
       </label>
       <div className="buttons__wrapper">
         <PreviousButton
